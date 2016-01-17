c155418f4b4adc99c7a8405d2c28b21731614fb6
James Hensman <james.hensman@gmail.com>
2016-01-17 18:44:31 +0000

    improved kern tests (slicing still an issue)

--- GPflow/tf_hacks.py
+++ GPflow/tf_hacks.py
@@ -7,5 +7,5 @@ hopefully we can remove these as the library matures
 import tensorflow as tf
 
 def eye(N):
-    return tf.diag(tf.ones((N,), dtype='float64') )
+    return tf.diag(tf.ones(tf.pack([N,]), dtype='float64') )
 
