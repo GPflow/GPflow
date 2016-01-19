bug in KL computation for svgp

GPflow/svgp.py
@@ -63,7 +63,7 @@ class SVGP(GPModel):
                     KL += 0.5*tf.reduce_sum(tf.square(Ldiag))
             fmean, fvar = conditionals.gaussian_gp_predict_whitened(self.X, self.Z, self.kern, self.q_mu, self.q_sqrt, self.num_latent)
         else:
-            L = tf.cholesky(self.kern.K(self.Z) + eye(self.num_inducing) * 1e-4)
+            L = tf.cholesky(self.kern.K(self.Z) + eye(self.num_inducing) * 1e-6)
             alpha = tf.user_ops.triangular_solve(L, self.q_mu, 'lower')
             KL = 0.5*tf.reduce_sum(tf.square(alpha)) - 0.5*self.num_inducing*self.num_latent +\
                  self.num_latent * tf.reduce_sum(tf.log(tf.user_ops.get_diag(L)))
@@ -76,7 +76,7 @@ class SVGP(GPModel):
                 for d in range(self.num_latent):
                     L = tf.user_ops.triangle(self.q_sqrt[:,:,d], 'lower')
                     S = tf.matmul(L, tf.transpose(L))
-                    KL -= -tf.reduce_sum(tf.log(tf.user_ops.get_diag(L)))
+                    KL -= tf.reduce_sum(tf.log(tf.user_ops.get_diag(L)))
                     KL += 0.5*tf.reduce_sum(S * K_inv)
             fmean, fvar = conditionals.gaussian_gp_predict(self.X, self.Z, self.kern, self.q_mu, self.q_sqrt, self.num_latent)
 

testing/test_methods.py
@@ -86,10 +86,22 @@ class TestSVGP(unittest.TestCase):
         self.failUnless(np.allclose(m1._objective(m1.get_free_state())[0], m2._objective(m2.get_free_state())[0]))
 
     def test_notwhite(self):
-        m1 = GPflow.svgp.SVGP(self.X, self.Y, kern=GPflow.kernels.RBF(1)+GPflow.kernels.White(1), likelihood=GPflow.likelihoods.Exponential(),
-                           Z=self.Z, q_diag=True, whiten=False)
-        m2 = GPflow.svgp.SVGP(self.X, self.Y, kern=GPflow.kernels.RBF(1)+GPflow.kernels.White(1), likelihood=GPflow.likelihoods.Exponential(),
-                           Z=self.Z, q_diag=False, whiten=False)
+        m1 = GPflow.svgp.SVGP(self.X,
+                              self.Y,
+                              kern=GPflow.kernels.RBF(1) + \
+                                   GPflow.kernels.White(1),
+                              likelihood=GPflow.likelihoods.Exponential(),
+                              Z=self.Z,
+                              q_diag=True,
+                              whiten=False)
+        m2 = GPflow.svgp.SVGP(self.X,
+                              self.Y,
+                              kern=GPflow.kernels.RBF(1) + \
+                                   GPflow.kernels.White(1),
+                              likelihood=GPflow.likelihoods.Exponential(),
+                              Z=self.Z,
+                              q_diag=False,
+                              whiten=False)
         m1._compile()
         m2._compile()
 