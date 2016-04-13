b9d42d6154f5f98696a67097cc93dac027398d76
James Hensman <james.hensman@gmail.com>
2016-04-13 21:57:53 +0100

    sgpr.py is now pep8 compliant

--- GPflow/sgpr.py
+++ GPflow/sgpr.py
@@ -2,21 +2,22 @@ import tensorflow as tf
 import numpy as np
 from .model import GPModel
 from .param import Param
-from .densities import multivariate_normal
 from .mean_functions import Zero
 from . import likelihoods
 from .tf_hacks import eye
 
-class SGPR(GPModel):
 
+class SGPR(GPModel):
     def __init__(self, X, Y, kern, Z, mean_function=Zero()):
         """
         This Sparse Variational GP regression. The key reference is
 
         @inproceedings{titsias2009variational,
-          title={Variational learning of inducing variables in sparse Gaussian processes},
+          title={Variational learning of inducing variables in
+                 sparse Gaussian processes},
           author={Titsias, Michalis K},
-          booktitle={International Conference on Artificial Intelligence and Statistics},
+          booktitle={International Conference on
+                     Artificial Intelligence and Statistics},
           pages={567--574},
           year={2009}
         }
@@ -40,33 +41,36 @@ class SGPR(GPModel):
         """
         Constuct a tensorflow function to compute the bound on the marginal
         likelihood. For a derivation of the terms in here, see the associated
-        SGPR notebook. 
+        SGPR notebook.
         """
 
         num_inducing = tf.shape(self.Z)[0]
-        num_data = tf.shape(self.Y)[0]
-        output_dim = tf.shape(self.Y)[1]
+        num_data = tf.cast(tf.shape(self.Y)[0], tf.float64)
+        output_dim = tf.cast(tf.shape(self.Y)[1], tf.float64)
 
-        err =  self.Y - self.mean_function(self.X)
+        err = self.Y - self.mean_function(self.X)
         Kdiag = self.kern.Kdiag(self.X)
         Kuf = self.kern.K(self.Z, self.X)
         Kuu = self.kern.K(self.Z) + eye(num_inducing) * 1e-6
         L = tf.cholesky(Kuu)
+        sigma = tf.sqrt(self.likelihood.variance)
 
         # Compute intermediate matrices
-        A = tf.matrix_triangular_solve(L, Kuf, lower=True)*tf.sqrt(1./self.likelihood.variance)
+        A = tf.matrix_triangular_solve(L, Kuf, lower=True) / sigma
         AAT = tf.matmul(A, tf.transpose(A))
         B = AAT + eye(num_inducing)
         LB = tf.cholesky(B)
-        c = tf.matrix_triangular_solve(LB, tf.matmul(A, err), lower=True) * tf.sqrt(1./self.likelihood.variance)
+        Aerr = tf.matmul(A, err)
+        c = tf.matrix_triangular_solve(LB, Aerr, lower=True) / sigma
 
-        #compute log marginal bound
+        # compute log marginal bound
         bound = -0.5*tf.cast(num_data*output_dim, tf.float64)*np.log(2*np.pi)
-        bound += -tf.cast(output_dim, tf.float64)*tf.reduce_sum(tf.log(tf.user_ops.get_diag(LB)))
-        bound += -0.5*tf.cast(num_data*output_dim, tf.float64)*tf.log(self.likelihood.variance)
+        bound += -output_dim * tf.reduce_sum(tf.log(tf.user_ops.get_diag(LB)))
+        bound -= 0.5 * num_data * output_dim * tf.log(self.likelihood.variance)
         bound += -0.5*tf.reduce_sum(tf.square(err))/self.likelihood.variance
         bound += 0.5*tf.reduce_sum(tf.square(c))
-        bound += -0.5*(tf.reduce_sum(Kdiag)/self.likelihood.variance - tf.reduce_sum(tf.user_ops.get_diag(AAT)))
+        bound += -0.5 * tf.reduce_sum(Kdiag) / self.likelihood.variance
+        bound += 0.5 * tf.reduce_sum(tf.user_ops.get_diag(AAT))
 
         return bound
 
@@ -74,26 +78,31 @@ class SGPR(GPModel):
         """
         Compute the mean and variance of the latent function at some new points
         Xnew. For a derivation of the terms in here, see the associated SGPR
-        notebook. 
+        notebook.
         """
         num_inducing = tf.shape(self.Z)[0]
-        err =  self.Y - self.mean_function(self.X)
+        err = self.Y - self.mean_function(self.X)
         Kuf = self.kern.K(self.Z, self.X)
         Kuu = self.kern.K(self.Z) + eye(num_inducing) * 1e-6
         Kus = self.kern.K(self.Z, Xnew)
+        sigma = tf.sqrt(self.likelihood.variance)
         L = tf.cholesky(Kuu)
-        A = tf.matrix_triangular_solve(L, Kuf, lower=True)*tf.sqrt(1./self.likelihood.variance)
+        A = tf.matrix_triangular_solve(L, Kuf, lower=True) / sigma
         B = tf.matmul(A, tf.transpose(A)) + eye(num_inducing)
         LB = tf.cholesky(B)
-        c = tf.matrix_triangular_solve(LB, tf.matmul(A, err), lower=True) * tf.sqrt(1./self.likelihood.variance)
+        Aerr = tf.matmul(A, err)
+        c = tf.matrix_triangular_solve(LB, Aerr, lower=True) / sigma
         tmp1 = tf.matrix_triangular_solve(L, Kus, lower=True)
         tmp2 = tf.matrix_triangular_solve(LB, tmp1, lower=True)
         mean = tf.matmul(tf.transpose(tmp2), c)
         if full_cov:
-            var = self.kern.K(Xnew) + tf.matmul(tf.transpose(tmp2), tmp2) - tf.matmul(tf.transpose(tmp1), tmp1)
-            var = tf.tile(tf.expand_dims(var, 2), tf.pack([1,1, tf.shape(self.Y)[1]]))
+            var = self.kern.K(Xnew) + tf.matmul(tf.transpose(tmp2), tmp2)\
+                - tf.matmul(tf.transpose(tmp1), tmp1)
+            shape = tf.pack([1, 1, tf.shape(self.Y)[1]])
+            var = tf.tile(tf.expand_dims(var, 2), shape)
         else:
-            var = self.kern.Kdiag(Xnew) + tf.reduce_sum(tf.square(tmp2), 0) - tf.reduce_sum(tf.square(tmp1), 0)
-            var = tf.tile(tf.expand_dims(var, 1), tf.pack([1, tf.shape(self.Y)[1]]))
+            var = self.kern.Kdiag(Xnew) + tf.reduce_sum(tf.square(tmp2), 0)\
+                - tf.reduce_sum(tf.square(tmp1), 0)
+            shape = tf.pack([1, tf.shape(self.Y)[1]])
+            var = tf.tile(tf.expand_dims(var, 1), shape)
         return mean + self.mean_function(Xnew), var
-
