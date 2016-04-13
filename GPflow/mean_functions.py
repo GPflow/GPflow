4f55afcb5db10ad65f95fd35f3de0c0f1bd70038
James Hensman <james.hensman@gmail.com>
2016-04-13 21:09:53 +0100

    mean_functions.py is noe pep8 compliant

--- GPflow/mean_functions.py
+++ GPflow/mean_functions.py
@@ -1,22 +1,25 @@
 import tensorflow as tf
 import numpy as np
 from param import Param, Parameterized
-import transforms
+
 
 class MeanFunction(Parameterized):
     """
     The base mean function class.
 
-    To implement a mean funcion, write the __call__ method. This takes a 
+    To implement a mean funcion, write the __call__ method. This takes a
     tensor X and returns a tensor m(X). In accordance with the GPflow
     standard, each row of X represents one datum, and each row of Y is computed
-    independently for each row of X. 
+    independently for each row of X.
 
-    MeanFunction classes can have parameters, see the Linear class for an example.
+    MeanFunction classes can have parameters, see the Linear class for an
+    example.
     """
     def __call__(self, X):
-        raise NotImplementedError, "Implement the __call__ method for this mean function"
-    
+        raise NotImplementedError("Implement the __call__\
+                                  method for this mean function")
+
+
 class Zero(MeanFunction):
     def __call__(self, X):
         return tf.zeros(tf.pack([tf.shape(X)[0], 1]), dtype='float64')
@@ -26,16 +29,18 @@ class Linear(MeanFunction):
     """
     y_i = A x_i + b
     """
-    def __init__(self, A=np.ones((1,1)), b=np.zeros(1)):
+    def __init__(self, A=np.ones((1, 1)), b=np.zeros(1)):
         """
-        A is a matrix which maps each element of X to Y, b is an additive constant.
+        A is a matrix which maps each element of X to Y, b is an additive
+        constant.
 
         If X has N rows and D columns, and Y is intended to have Q columns,
-        then A must be D x Q, b must be a vector of length Q. 
+        then A must be D x Q, b must be a vector of length Q.
         """
         MeanFunction.__init__(self)
         self.A = Param(np.atleast_2d(A))
         self.b = Param(b)
+
     def __call__(self, X):
         return tf.matmul(X, self.A) + self.b
 
@@ -47,6 +52,7 @@ class Constant(MeanFunction):
     def __init__(self, c=np.zeros(1)):
         MeanFunction.__init__(self)
         self.c = Param(c)
-    def __call__(self, X):
-        return tf.tile(tf.reshape(self.c, (1,-1)), tf.pack([tf.shape(X)[0], 1]))
 
+    def __call__(self, X):
+        shape = tf.pack([tf.shape(X)[0], 1])
+        return tf.tile(tf.reshape(self.c, (1, -1)), shape)
