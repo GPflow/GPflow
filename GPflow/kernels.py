122f4ae6f400f0c43f3fc6a85481264809e678f5
James Hensman <james.hensman@gmail.com>
2016-05-27 16:51:37 +0100

    minor tidying in periodicKernel

--- GPflow/kernels.py
+++ GPflow/kernels.py
@@ -52,7 +52,7 @@ class Static(Kern):
         self.variance = Param(variance, transforms.positive)
 
     def Kdiag(self, X):
-        zeros = X[:,0]*0
+        zeros = X[:, 0]*0
         return zeros + self.variance
 
 
@@ -134,7 +134,7 @@ class Stationary(Kern):
         return tf.sqrt(r2 + 1e-12)
 
     def Kdiag(self, X):
-        zeros = X[:,0]*0
+        zeros = X[:, 0]*0
         return zeros + self.variance
 
 
@@ -226,41 +226,43 @@ class Cosine(Stationary):
         r = self.euclid_dist(X, X2)
         return self.variance * tf.cos(r)
 
+
 class PeriodicKernel(Kern):
     """
-    The periodic kernel. Defined in  Equation (47) of 
-    D.J.C.MacKay. Introduction to Gaussian processes. In C.M.Bishop, editor, Neural Networks and Machine Learning, pages 133--165. Springer, 1998.
+    The periodic kernel. Defined in  Equation (47) of
+
+    D.J.C.MacKay. Introduction to Gaussian processes. In C.M.Bishop, editor,
+    Neural Networks and Machine Learning, pages 133--165. Springer, 1998.
+
     Derived using the mapping u=(cos(x), sin(x)) on the inputs.
-    """    
-    def __init__(self, input_dim, period=1.0, variance=1.0, lengthscales=None, active_dims=None):        
+    """
+    def __init__(self, input_dim, period=1.0, variance=1.0, lengthscales=1.0, active_dims=None):
         # No ARD support for lenghtscale or period yet
         Kern.__init__(self, input_dim, active_dims)
         self.variance = Param(variance, transforms.positive)
-        if lengthscales is None:
-            lengthscales = 1.0
         self.lengthscales = Param(lengthscales, transforms.positive)
         self.ARD = False
         self.period = Param(period, transforms.positive)
-        
+
     def Kdiag(self, X):
-        zeros = X[:,0]*0 
+        zeros = X[:, 0] * 0
         return zeros + self.variance
-    
+
     def K(self, X, X2=None):
         X, X2 = self._slice(X, X2)
-        if X2 is None: 
+        if X2 is None:
             X2 = X
-            
-        # Introduce dummy dimension so we can use broadcasting
-        f= tf.reshape(X, tf.pack([tf.shape(X)[0], 1, tf.shape(X)[1]]) )
-        f2= tf.reshape(X2, tf.pack([1, tf.shape(X2)[0], tf.shape(X2)[1]]) )
-        r = np.pi * (f-f2) / self.period
-        
-        dist = tf.exp(-0.5*tf.reduce_sum(tf.square(tf.sin(r)/self.lengthscales),tf.rank(r)-1))
 
-        return self.variance * dist
-    
-    
+        # Introduce dummy dimension so we can use broadcasting
+        f = tf.expand_dims(X, 1)  # now N x 1 x D
+        f2 = tf.expand_dims(X2, 0)  # now 1 x M x D
+
+        r = np.pi * (f-f2) / self.period
+        r = tf.reduce_sum(tf.square(tf.sin(r)/self.lengthscales), 2)
+
+        return self.variance * tf.exp(-0.5 * r)
+
+
 def make_kernel_names(kern_list):
     """
     Take a list of kernels and return a list of strings, giving each kernel a
