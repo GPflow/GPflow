d6d9e7a3054f3b0ee7fb49bcbeeb73e719a9f143
James Hensman <james.hensman@gmail.com>
2016-05-27 15:47:36 +0100

    simpification of test to ensure prediction works after rest_graph

--- testing/test_autoflow.py
+++ testing/test_autoflow.py
@@ -77,11 +77,8 @@ class TestResetGraph(unittest.TestCase):
         self.m = GPflow.gpr.GPR(X, Y, kern=k)
 
     def test(self):
-        self.m._compile()
         mu, var = self.m.predict_f(self.Xnew)
-        self.m._needs_recompile = True
         tf.reset_default_graph()
-        self.m._compile()
         mu1, var1 = self.m.predict_f(self.Xnew)
 
 
