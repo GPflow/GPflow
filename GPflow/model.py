ff3bad2972e658ff475aa3b6575b1430c7cb44c4
Mark van der Wilk <mv310@cam.ac.uk>
2016-06-07 16:02:39 +0100

    Fixed AutoFlow sharing `self.tf_args` between different instances.

--- GPflow/model.py
+++ GPflow/model.py
@@ -82,14 +82,15 @@ class AutoFlow:
         @wraps(tf_method)
         def runnable(instance, *np_args):
             graph_name = '_' + tf_method.__name__ + '_graph'
+            tf_args_name = '_%s_tf_args' % tf_method.__name__
             if not hasattr(instance, graph_name):
                 if instance._needs_recompile:
                     instance._compile()  # ensures free_vars is up-to-date.
-                self.tf_args = [tf.placeholder(*a) for a in self.tf_arg_tuples]
+                setattr(instance, tf_args_name, [tf.placeholder(*a) for a in self.tf_arg_tuples])
                 with instance.tf_mode():
-                    graph = tf_method(instance, *self.tf_args)
+                    graph = tf_method(instance, *getattr(instance, tf_args_name))
                 setattr(instance, graph_name, graph)
-            feed_dict = dict(zip(self.tf_args, np_args))
+            feed_dict = dict(zip(getattr(instance, tf_args_name), np_args))
             feed_dict[instance._free_vars] = instance.get_free_state()
             graph = getattr(instance, graph_name)
             return instance._session.run(graph, feed_dict=feed_dict)
