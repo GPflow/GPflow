ff557f17f37752f622394a9917e65c2b47874e9c
James Hensman <james.hensman@gmail.com>
2016-06-16 11:04:50 +0100

    adding a test to compare psi-statistics with quadrature

--- GPflow/__init__.py
+++ GPflow/__init__.py
@@ -1,3 +1,3 @@
 # flake8: noqa
-from . import likelihoods, kernels, param, model, gpmc, sgpmc, priors, gpr, svgp, vgp, sgpr
+from . import likelihoods, kernels, param, model, gpmc, sgpmc, priors, gpr, svgp, vgp, sgpr, kernel_expectations, gplvm
 from ._version import __version__
