f91ce4ad424c91665cda4376c26d6305b5f43626
mqbssaby <alexis.boukouvalas@gmail.com>
2016-06-16 09:44:45 +0100

    starting with tests

--- testing/test_gplvm.py
+++ testing/test_gplvm.py
@@ -6,7 +6,7 @@ import numpy as np
 import unittest
 
 
-class TestGPLVM(unittest.TestCase):
+class TestBayesianGPLVM(unittest.TestCase):
     def setUp(self):
         N = 10 # number of data points
         D = 1  # latent dimensions
@@ -20,7 +20,35 @@ class TestGPLVM(unittest.TestCase):
         self.m = GPflow.gplvm.BayesianGPLVM(X_mean = np.zeros((N,D)), 
                     X_var=np.ones((N,D)), Y=Y, kern=k, Z=Z)
 
+    def test_linearSolution(self):
+        # You could implement a standard GPLVM, and show that it recovers PCA when the kernel is linear -> 
+        # How to deal with rotations and linear rescalings.
+        pass
+
+    def test_GPLVM_BGPLVM_Equivalence(self):
+        # You could set the variance of the BGPLVM to zero and show that it's the same as the GPLVM
+        # BGPLVM with variance to 0 is same as GPLVM
+        N = 10  # number of data points
+        D = 1  # latent dimensions
+        M = 5  # inducings points
+        R = 2  # data dimension
+        k = GPflow.kernels.RBF(D)
+        Z = np.linspace(0, 1, M)
+        Z = np.expand_dims(Z, D)
+        rng = np.random.RandomState(1)
+        Y = rng.randn(N, R)
+        XInit = rng.rand(N, D)
+        # use 0 variance for BGPLVM
+        m = GPflow.gplvm.BayesianGPLVM(X_mean=XInit, X_var=np.zeros((N, D)), Y=Y, kern=k, Z=Z)
+        m.X_var.fixed = True
+        m.optimize()
+        mGPLVM = GPflow.gplvm.GPLVM(Y=Y, Q=D, kern=k, XInit=XInit)
+        mGPLVM.optimize()
+        assert np.allclose(m.X_mean.value, mGPLVM.X.value)
+        # this does not work - f=    +Infinity!
+
     def test_gplvmOptimization(self):
+        print('Run optimisation')
         self.m.optimize()
     
 
