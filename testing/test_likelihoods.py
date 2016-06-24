1c531c1aef09c8cf95d2db3720338df2ed833156
alexggmatthews <alexggmatthews@googlemail.com>
2016-06-24 17:43:07 +0100

    Building up likelihood tests.

--- testing/test_likelihoods.py
+++ testing/test_likelihoods.py
@@ -3,6 +3,19 @@ import tensorflow as tf
 import numpy as np
 import unittest
 
+def getLikelihoodsAndYs():
+    liks = []
+    Ys = []
+    rng = np.random.RandomState(1)
+    for likelihoodClass in GPflow.likelihoods.Likelihood.__subclasses__():
+        if likelihoodClass!=GPflow.likelihoods.MultiClass:
+            liks.append(likelihoodClass())   
+            Ys.append( rng.rand(10,2) )
+        else:
+            liks.append(likelihoodClass(2)) 
+            sample = rng.randn(10,2)
+            Ys.append( np.argmax(sample, 1).reshape(-1,1) )
+    return liks, Ys
 
 class TestPredictConditional(unittest.TestCase):
     """
@@ -12,8 +25,8 @@ class TestPredictConditional(unittest.TestCase):
     """
     def setUp(self):
         tf.reset_default_graph()
-        self.liks = [L() for L in GPflow.likelihoods.Likelihood.__subclasses__()]
-
+        self.liks, self.Ys = getLikelihoodsAndYs()
+        
         #some likelihoods are additionally tested with non-standard links
         self.liks.append(GPflow.likelihoods.Poisson(invlink=tf.square))
         self.liks.append(GPflow.likelihoods.Exponential(invlink=tf.square))
@@ -28,7 +41,6 @@ class TestPredictConditional(unittest.TestCase):
         self.F = tf.placeholder(tf.float64)
         rng = np.random.RandomState(0)
         self.F_data = rng.randn(10,2)
-        self.Y = rng.rand(10,2)
 
     def test_mean(self):
         for l in self.liks:
@@ -42,7 +54,7 @@ class TestPredictConditional(unittest.TestCase):
         for l in self.liks:
             with l.tf_mode():
                 v1 = tf.Session().run(l.conditional_variance(self.F), feed_dict={self.x: l.get_free_state(), self.F:self.F_data})
-                v2 = tf.Session().run(l.predict_mean_and_var(self.F, self.F * 0)[1], feed_dict={self.x: l.get_free_state(), self.F:self.F_data})
+                v2 = tf.Session().run(l.predict_mean_and_var(self.F, self.F * 0)[1], feed_dict={self.x: l.get_free_state(), self.F:self.F_data})   
             self.failUnless(np.allclose(v1, v2, 1e-6, 1e-6))
 
     def test_var_exp(self):
@@ -50,13 +62,11 @@ class TestPredictConditional(unittest.TestCase):
         Here we make sure that the variational_expectations gives the same result
         as logp if the latent function has no uncertainty.
         """
-        for l in self.liks:
+        for l, y in zip(self.liks,self.Ys):
             with l.tf_mode():
-                r1 = tf.Session().run(l.logp(self.F, self.Y), feed_dict={self.x: l.get_free_state(), self.F:self.F_data})
-                r2 = tf.Session().run(l.variational_expectations(self.F, self.F * 0, self.Y), feed_dict={self.x: l.get_free_state(), self.F:self.F_data})
+                r1 = tf.Session().run(l.logp(self.F, y), feed_dict={self.x: l.get_free_state(), self.F:self.F_data})
+                r2 = tf.Session().run(l.variational_expectations(self.F, self.F * 0, y), feed_dict={self.x: l.get_free_state(), self.F:self.F_data})   
             self.failUnless(np.allclose(r1, r2, 1e-6, 1e-6))
-
-
 
 class TestQuadrature(unittest.TestCase):
     """
@@ -68,20 +78,21 @@ class TestQuadrature(unittest.TestCase):
 
         self.rng = np.random.RandomState()
         self.Fmu, self.Fvar, self.Y = self.rng.randn(3, 10, 2)
-        self.Y = self.Y**2
         self.Fvar = 0.01 * self.Fvar **2
 
     def test_var_exp(self):
         #get all the likelihoods where variational expectations has been overwritten
-        liks = [l() for l in GPflow.likelihoods.Likelihood.__subclasses__()
-                if l.variational_expectations.__func__ is not GPflow.likelihoods.Likelihood.variational_expectations.__func__]
-        for l in liks:
+        unfiltered_liks, unfiltered_ys = getLikelihoodsAndYs()
+        liksAndYs = [(l,y) for l,y in zip(unfiltered_liks,unfiltered_ys)
+                if l.predict_density.__func__ is not GPflow.likelihoods.Likelihood.predict_density.__func__]
+                
+        for l,y in liksAndYs:
             x_data = l.get_free_state()
             x = tf.placeholder('float64')
             l.make_tf_array(x)
             #'build' the functions
             with l.tf_mode():
-                F1 = l.variational_expectations(self.Fmu, self.Fvar, self.Y)
+                F1 = l.variational_expectations(self.Fmu, self.Fvar, y)
                 F2 = GPflow.likelihoods.Likelihood.variational_expectations(l, self.Fmu, self.Fvar, self.Y)
             #compile and run the functions:
             F1 = tf.Session().run(F1, feed_dict={x: x_data})
@@ -90,23 +101,24 @@ class TestQuadrature(unittest.TestCase):
 
     def test_pred_density(self):
         #get all the likelihoods where predict_density  has been overwritten
-        liks = [l() for l in GPflow.likelihoods.Likelihood.__subclasses__()
+        unfiltered_liks, unfiltered_ys = getLikelihoodsAndYs()
+        liksAndYs = [(l,y) for l,y in zip(unfiltered_liks,unfiltered_ys)
                 if l.predict_density.__func__ is not GPflow.likelihoods.Likelihood.predict_density.__func__]
-        for l in liks:
+                
+        for l, y in liksAndYs:
             x_data = l.get_free_state()
             #make parameters if needed
             x = tf.placeholder('float64')
             l.make_tf_array(x)
             #'build' the functions
             with l.tf_mode():
-                F1 = l.predict_density(self.Fmu, self.Fvar, self.Y)
-                F2 = GPflow.likelihoods.Likelihood.predict_density(l, self.Fmu, self.Fvar, self.Y)
+                F1 = l.predict_density(self.Fmu, self.Fvar, y)
+                F2 = GPflow.likelihoods.Likelihood.predict_density(l, self.Fmu, self.Fvar, y)
             #compile and run the functions:
             F1 = tf.Session().run(F1, feed_dict={x: x_data})
             F2 = tf.Session().run(F2, feed_dict={x: x_data})
             self.failUnless(np.allclose(F1, F2, 1e-6, 1e-6))
-
-
+            
 if __name__ == "__main__":
     unittest.main()
 
