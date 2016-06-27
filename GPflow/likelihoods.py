050bf0d7c8f1cfc70f35efc6a48b96f8ff5d50b5
alexggmatthews <alexggmatthews@googlemail.com>
2016-06-27 12:16:48 +0100

    Correcting an added bug and reimplimenting clipping.

--- GPflow/likelihoods.py
+++ GPflow/likelihoods.py
@@ -362,8 +362,10 @@ class RobustMax(object):
         X = tf.reshape(mu_selected, (-1, 1)) + gh_x * tf.reshape(tf.sqrt(tf.clip_by_value(2. * var_selected, 1e-10, np.inf)), (-1, 1))
 
         # compute the CDF of the Gaussian between the latent functions and the grid (includeing the selected function)
-        dist = (tf.expand_dims(X, 1) - tf.expand_dims(mu, 2)) / tf.expand_dims(tf.clip_by_value(2. * var, 1e-10, np.inf), 2)
+        dist = (tf.expand_dims(X, 1) - tf.expand_dims(mu, 2)) / tf.expand_dims(tf.sqrt(tf.clip_by_value(2. * var, 1e-10, np.inf)), 2)
         cdfs = 0.5 * (1.0 + tf.erf(dist/np.sqrt(2.0)))
+
+        cdfs = cdfs * (1-2e-4) + 1e-4
 
         # blank out all the distances on the selected latent function
         oh_off = tf.cast(tf.one_hot(tf.reshape(Y, (-1,)), self.num_classes, 0., 1.), tf.float64)
