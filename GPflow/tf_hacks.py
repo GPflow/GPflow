570898c4bd8232bced9a850c61d742314842bf10
James Hensman <james.hensman@gmail.com>
2016-08-09 18:26:52 +0100

    better shape registration

--- GPflow/tf_hacks.py
+++ GPflow/tf_hacks.py
@@ -1,11 +1,11 @@
 # Copyright 2016 James Hensman, alexggmatthews
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 # http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -40,6 +40,10 @@ def _vec_to_tri_grad(op, grad):
 @tf.RegisterShape("VecToTri")
 def _vec_to_tri_shape(op):
     in_shape = op.inputs[0].get_shape().with_rank(2)
-    k = int((in_shape[1].value * 8 + 1) ** 0.5 / 2.0 - 0.5)
+    M = in_shape[1].value
+    if M is None:
+        k = None
+    else:
+        k = int((M * 8 + 1) ** 0.5 / 2.0 - 0.5)
     shape = tf.TensorShape([in_shape[0], k, k])
     return [shape]
