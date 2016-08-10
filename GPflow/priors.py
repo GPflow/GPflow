fixed sign bug

GPflow/priors.py
@@ -90,7 +90,7 @@ class Laplace(Prior):
 
 class Uniform(Prior):
     def __init__(self, lower=0, upper=1):
-        self.log_height = np.log(upper - lower)
+        self.log_height = - np.log(upper - lower)
         self.lower, self.upper = lower, upper
 
     def logp(self, x):