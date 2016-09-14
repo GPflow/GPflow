8e1ef3f76e99e3081d6e215ea9d7e4029716e4d8
Mark van der Wilk <mv310@cam.ac.uk>
2016-09-14 15:03:20 +0100

    Settings module now has ability to change during a run.

--- GPflow/_settings.py
+++ GPflow/_settings.py
@@ -1,7 +1,75 @@
 from six.moves import configparser
+import copy
 import os
 import collections
 import tensorflow as tf
+from collections import OrderedDict
+
+
+class SettingsContextManager(object):
+    def __init__(self, manager, tmp_settings):
+        self._manager = manager
+        self._tmp_settings = tmp_settings
+
+    def __enter__(self):
+        self._manager.push(self._tmp_settings)
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._manager.pop()
+
+
+class SettingsManager(object):
+    def __init__(self, set):
+        self._cur_settings = set
+        self._settings_stack = []
+
+    def __getattr__(self, name):
+        try:
+            return self._cur_settings[name]
+        except KeyError:
+            raise AttributeError
+
+    def push(self, settings):
+        self._settings_stack.append(self._cur_settings)
+        self._cur_settings = settings
+
+    def pop(self):
+        rem = self._cur_settings
+        self._cur_settings = self._settings_stack.pop()
+        return rem
+
+    def temp_settings(self, tmp_settings):
+        return SettingsContextManager(self, tmp_settings)
+
+    def get_settings(self):
+        c = copy.deepcopy(self._cur_settings)
+        c._mutable = True
+        return c
+
+
+class MutableNamedTuple(OrderedDict):
+    """
+    A class that doubles as a mutable named tuple, to allow settings to be re-set during
+    """
+
+    def __init__(self, *args, **kwargs):
+        super(MutableNamedTuple, self).__init__(*args, **kwargs)
+        self._mutable = True
+        self._settings_stack = []
+
+    def __getattr__(self, name):
+        try:
+            return self[name]
+        except KeyError:
+            raise AttributeError(name)
+
+    def __setattr__(self, name, value):
+        if name in ["_settings_stack", "_mutable"]:
+            super(MutableNamedTuple, self).__setattr__(name, value)
+        elif self._mutable is True:
+            super(MutableNamedTuple, self).__setitem__(name, value)
+        else:
+            raise AttributeError("Instance not mutable.")
 
 
 # a very simple parser
@@ -36,7 +104,8 @@ def namedtuplify(mapping):  # thank you https://gist.github.com/hangtwenty/59604
             mapping.pop('__name__')
         except:
             pass
-        return collections.namedtuple('settings', dict(**mapping))(**mapping)
+        # return collections.namedtuple('settingsa', dict(**mapping))(**mapping)
+        return MutableNamedTuple(mapping)
     return parse(mapping)
 
 
@@ -62,4 +131,5 @@ def read_config_file(path=None):
 
 
 c = read_config_file()
-settings = namedtuplify(c._sections)
+loaded_settings = namedtuplify(c._sections)
+settings = SettingsManager(loaded_settings)
