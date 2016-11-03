Fix small off-by-one-error
k.active_dims contain indices, and we want number of columns.

GPflow/kernels.py
@@ -449,7 +449,7 @@ class Combination(Kern):
         for k in kern_list:
             assert isinstance(k, Kern), "can only add Kern instances"
 
-        input_dim = np.max([k.input_dim if type(k.active_dims) is slice else np.max(k.active_dims) for k in kern_list])
+        input_dim = np.max([k.input_dim if type(k.active_dims) is slice else np.max(k.active_dims) + 1 for k in kern_list])
         Kern.__init__(self, input_dim=input_dim)
 
         # add kernels to a list, flattening out instances of this class therein