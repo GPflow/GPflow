d0b3a7e9092de5a023a22962b30f2f3d1b4f9dc3
James Hensman <james.hensman@gmail.com>
2016-11-04 14:51:45 +0000

    small update to meanfunction tests

--- testing/test_mean_functions.py
+++ testing/test_mean_functions.py
@@ -214,21 +214,22 @@ class TestSwitchedMeanFunction(unittest.TestCase):
 
     def test(self):
         rng = np.random.RandomState(0)
-        X = np.hstack([rng.randn(10,3), 1.0*rng.randint(0,2,10).reshape(-1,1)])
+        X = np.hstack([rng.randn(10, 3), 1.0*rng.randint(0, 2, 10).reshape(-1, 1)])
         switched_mean = GPflow.mean_functions.SwitchedMeanFunction(
                         [GPflow.mean_functions.Constant(np.zeros(1)),
-                         GPflow.mean_functions.Constant(np.ones( 1))] )
+                         GPflow.mean_functions.Constant(np.ones(1))])
 
         sess = tf.Session()
         tf_array = switched_mean.get_free_state()
         switched_mean.make_tf_array(tf_array)
         sess.run(tf.initialize_all_variables())
+        fd = {}
+        switched_mean.update_feed_dict(switched_mean.get_feed_dict_keys(), fd)
         with switched_mean.tf_mode():
-            result = sess.run(
-                    switched_mean(X), feed_dict=switched_mean.get_feed_dict())
+            result = sess.run(switched_mean(X), feed_dict=fd)
 
-        np_list=np.array([0.,1.])
-        result_ref = (np_list[X[:,3].astype(np.int)]).reshape(-1,1)
+        np_list = np.array([0., 1.])
+        result_ref = (np_list[X[:, 3].astype(np.int)]).reshape(-1, 1)
         self.assertTrue(np.allclose(result, result_ref))
 
 
