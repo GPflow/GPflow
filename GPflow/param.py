38813e5ab3f557a2507b7e96d894a0cb8ed2d9f4
Dan Marthaler <dan.marthaler@revionics.com>
2016-11-29 14:49:09 -0700

    changed model and param initialize_all_variables() to global_variables_initializer()

--- GPflow/param.py
+++ GPflow/param.py
@@ -529,7 +529,8 @@ class AutoFlow:
                     storage['feed_dict_keys'] = instance.get_feed_dict_keys()
                     feed_dict = {}
                     instance.update_feed_dict(storage['feed_dict_keys'], feed_dict)
-                    storage['session'].run(tf.initialize_all_variables(), feed_dict=feed_dict)
+                    #storage['session'].run(tf.initialize_all_variables(), feed_dict=feed_dict)
+                    storage['session'].run(tf.global_variables_initializer(), feed_dict=feed_dict)
             feed_dict = dict(zip(storage['tf_args'], np_args))
             feed_dict[storage['free_vars']] = instance.get_free_state()
             instance.update_feed_dict(storage['feed_dict_keys'], feed_dict)
