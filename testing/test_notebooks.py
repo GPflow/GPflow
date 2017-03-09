50431a0afb89e35b7551c8076622fb1d85085363
alexisboukouvalas <alexis.boukouvalas@gmail.com>
2017-03-09 22:36:34 +0000

    clean up

--- testing/test_notebooks.py
+++ testing/test_notebooks.py
@@ -5,21 +5,18 @@ import glob
 import time
 import sys
 
-#TODO: how to do black list?
 pythonkernel = 'python'+str(sys.version_info[0])
 nbpath = '../doc/source/notebooks/'
 # see http://nbconvert.readthedocs.io/en/stable/execute_api.html
 ep = ExecutePreprocessor(timeout=5, kernel_name=pythonkernel, interrupt_on_timeout=True)
-for file in glob.glob(nbpath+"*.ipynb"):
-    notebook_filename = file
-    print(file)
+for notebook_filename in glob.glob(nbpath+"*.ipynb"):
+    print(notebook_filename)
     with open(notebook_filename) as f:
         nb = nbformat.read(f, as_version=4)
         try:
             t = time.time()
             out = ep.preprocess(nb, {'metadata': {'path': nbpath}})
         except CellExecutionError:
-            msg = 'Error executing the notebook "%s".\n\n' % notebook_filename
-            print(msg)
+            print('Error executing the notebook "%s".\n\n' % notebook_filename)
             raise
         print('Processing %s took %g seconds.' % (notebook_filename, time.time()-t))
