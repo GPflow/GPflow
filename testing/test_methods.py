8143037d40dbacf06805afdebe34cd834aff0e82
Artem Artemev <art.art.v@gmail.com>
2017-07-18 23:33:51 +0100

    Revert back GPflow copyright headers

--- testing/test_methods.py
+++ testing/test_methods.py
@@ -1,4 +1,4 @@
-# Copyright 2016 the gpflow authors.
+# Copyright 2016 the GPflow authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
