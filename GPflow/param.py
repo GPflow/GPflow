2b6d7f381a80c73b8b59bedf3401bee464f00384
Alessandro Davide Ialongo <alex.ialongo@gmail.com>
2017-08-07 16:04:47 +0100

    undo changes and assume input to forward is always a vector

--- GPflow/param.py
+++ GPflow/param.py
@@ -204,11 +204,11 @@ class Param(Parentable):
         if self.fixed:
             return pd.Series([self.value for _ in range(samples.shape[0])], name=self.long_name)
         start, _ = self.highest_parent.get_param_index(self)
-        free_state_shape = self.transform.free_state_shape(self.shape)
-        end = start + np.prod(free_state_shape)
+        free_state_size = self.transform.free_state_size(self.shape)
+        end = start + free_state_size
         samples = samples[:, start:end]
-        samples = samples.reshape((samples.shape[0],) + free_state_shape)
-        samples = [self.transform.forward(s) for s in samples]
+        samples = [np.atleast_1d(self.transform.forward(s).reshape(self.shape))
+                   for s in samples]
         return pd.Series(samples, name=self.long_name)
 
     def make_tf_array(self, free_array):
