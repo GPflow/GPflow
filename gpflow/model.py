27502b0d319ac61491e18f1f86238c9c273450f1
John Bradshaw <jab@prowler.io>
2017-09-14 11:54:24 +0100

    RBF derivative kernel -- remove whitespace and scratch files added accidently.

--- gpflow/model.py
+++ gpflow/model.py
@@ -435,7 +435,6 @@ class GPModel(Model):
         pred_f_mean, pred_f_var = self.build_predict(Xnew)
         return self.likelihood.predict_mean_and_var(pred_f_mean, pred_f_var)
 
-
     @AutoFlow((float_type, [None, None]), (float_type, [None, None]))
     def predict_density(self, Xnew, Ynew):
         """
