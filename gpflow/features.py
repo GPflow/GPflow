8d9f04b870786b79b048b4c4e315bb9ad9153ef2
Mark van der Wilk <mv310@cam.ac.uk>
2017-09-22 12:25:17 +0100

    Added Multiscale feature. To show the generality of the inter-domain code.

--- gpflow/features.py
+++ gpflow/features.py
@@ -1,12 +1,12 @@
+from abc import ABCMeta, abstractmethod
+from functools import singledispatch
+
 import numpy as np
 import tensorflow as tf
-from functools import singledispatch
-from abc import ABCMeta, abstractmethod
 
-from .param import Param, Parameterized
-from . import conditionals
+from . import conditionals, transforms, kernels
 from ._settings import settings
-
+from .param import Param, Parameterized
 
 float_type = settings.dtypes.float_type
 
@@ -66,12 +66,63 @@ class InducingPoints(InducingFeature):
         return Kzx
 
 
+class Multiscale(InducingFeature):
+    """
+    Multi-scale inducing features
+    Originally proposed in
+
+    ::
+
+      @incollection{NIPS2009_3876,
+        title = {Inter-domain Gaussian Processes for Sparse Inference using Inducing Features},
+        author = {Miguel L\'{a}zaro-Gredilla and An\'{\i}bal Figueiras-Vidal},
+        booktitle = {Advances in Neural Information Processing Systems 22},
+        year = {2009},
+      }
+
+    """
+    def __init__(self, Z, scales):
+        super().__init__()
+        self.Z = Param(Z)  # Multi-scale feature centres
+        self.scales = Param(scales, transform=transforms.positive)  # Multi-scale feature widths (std. dev. of Gaussian)
+        self._M = len(Z)
+
+    def __len__(self):
+        return self._M
+
+    def _cust_square_dist(self, A, B, sc):
+        return tf.reduce_sum(tf.square((tf.expand_dims(A, 1) - tf.expand_dims(B, 0)) / sc), 2)
+
+    def Kuf(self, kern, Xnew):
+        if isinstance(kern, kernels.RBF):
+            Xnew, _ = kern._slice(Xnew, None)
+            Zmu, Zlen = kern._slice(self.Z, self.scales)
+            idlengthscales = kern.lengthscales + Zlen
+            d = self._cust_square_dist(Xnew, Zmu, idlengthscales)
+            return tf.transpose(self.variance * tf.exp(-d / 2) *
+                                tf.reshape(tf.reduce_prod(kern.lengthscales / idlengthscales, 1), (1, -1)))
+        else:
+            raise NotImplementedError("Multiscale features not implemented for `%s`." % str(type(kern)))
+
+    def Kuu(self, kern):
+        if isinstance(kern, kernels.RBF):
+            Zmu, Zlen = kern._slice(self.Z, self.scales)
+            idlengthscales2 = tf.square(kern.lengthscales + Zlen)
+            sc = tf.sqrt(
+                tf.expand_dims(idlengthscales2, 0) + tf.expand_dims(idlengthscales2, 1) - tf.square(kern.lengthscales))
+            d = self._cust_square_dist(Zmu, Zmu, sc)
+            return self.variance * tf.exp(-d / 2) * tf.reduce_prod(kern.lengthscales / sc, 2)
+        else:
+            raise NotImplementedError("Multiscale features not implemented for `%s`." % str(type(kern)))
+
+
 @singledispatch
 def conditional(feat, kern, Xnew, f, full_cov=False, q_sqrt=None, whiten=False):
     """
     Note the changed function signature compared to conditionals.conditional() to allow for dispatch.
     """
     raise NotImplementedError("No implementation for {} found".format(type(feat).__name__))
+
 
 @conditional.register(InducingPoints)
 def _(feat, kern, Xnew, f, full_cov=False, q_sqrt=None, whiten=False):
