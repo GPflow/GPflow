f9ffcac830b10904cf610cb7a1e67d2564ec352e
John Bradshaw <jab@prowler.io>
2017-09-26 10:32:39 +0100

    Putting random features into GPR model
    * random features for GPR model (untested)
    * small script demonstrating how it works
    * kernels back to numpy randomness as more controllable.

---
+++ doc/source/notebooks/random_features_approx_demo.py
@@ -0,0 +1,115 @@
+"""
+Demos using random features to approximate draws from a GP.
+"""
+
+import numpy as np
+import tensorflow as tf
+import matplotlib.pyplot as plt
+from scipy import linalg as sla
+import gpflow
+
+
+NUM_OBS = 30
+AXIS_EDGE = 0.5
+NOISE_STD = 0.05
+
+def func1(x):
+    f = np.sin(12 * x) + 0.66 * np.cos(25 * x)
+    deriv = 12 * np.cos(12 * x) - 0.66 * 25 * np.sin(25 * x)  # the analytical deriv
+    second_deriv = -144 * np.sin(12 * x) - 412.5 * np.cos(25 * x)
+    return f, deriv, second_deriv
+
+
+def create_random_x_data(rng):
+    x_data = rng.rand(NUM_OBS, 1)  # make it as a column vector
+    x_axis_indcs = np.linspace(x_data.min() - AXIS_EDGE, x_data.max() + AXIS_EDGE, 150)[:, None]
+    return x_data, x_axis_indcs
+
+
+def package_data(x_data, x_axis_idcs, rng):
+    f_data, derivs, second_derivs = func1(x_data)
+
+    data = dict(
+        x_axis=x_axis_idcs,
+        x_data=x_data,
+
+        f=f_data,
+        deriv=derivs,
+        second_deriv=second_derivs
+    )
+    data['y'] = data['f'] + NOISE_STD * rng.randn(*data['f'].shape)
+    return data
+
+
+
+def create_approx_features_func(model, data_dim):
+
+    tf_graph = tf.Graph()
+    tf_sess = tf.Session(graph=tf_graph)
+
+    with tf_graph.as_default():
+        x_ph = tf.placeholder(tf.float64, [None, data_dim])
+
+        with model.kern.tf_mode():
+            x_free = tf.placeholder('float64')
+            model.kern.make_tf_array(x_free)
+            feats_func = model.kern.create_feature_map_func(model.random_seed_for_random_features)
+            feats = feats_func(x_ph)
+
+
+    def approx_features_func(X):
+        feats_evald = tf_sess.run(feats, feed_dict={x_ph:X, x_free: model.kern.get_free_state()})
+        return feats_evald
+
+    return approx_features_func
+
+
+
+
+def main():
+
+    rng = np.random.RandomState(100)
+    x_data, x_axis_points = create_random_x_data(rng)
+    data = package_data(x_data, x_axis_points, rng)
+
+    kernel = gpflow.kernels.RBF(1)
+    model = gpflow.gpr.GPR(data['x_data'], data['y'], kern=kernel)
+
+    model.optimize()
+
+
+
+    mean, var = model.predict_y(x_axis_points)
+    f, ax = plt.subplots()
+    ax.plot(x_data, data['f'], 'kx', mew=2)
+    ax.plot(x_axis_points, mean, 'b', lw=2)
+    ax.fill_between(x_axis_points[:, 0], mean[:, 0] - 2 * np.sqrt(var[:, 0]),
+                    mean[:, 0] + 2 * np.sqrt(var[:, 0]), color='blue', alpha=0.2)
+
+    approx_features_func = create_approx_features_func(model, x_data.shape[1])
+    mean, L_precision = model.linear_weights_posterior()
+    for j in range(2):
+        sampled_var = sla.solve_triangular(L_precision.T, rng.randn(mean.shape[0]), lower=False)[:, None]
+
+        theta_sample = mean + sampled_var
+
+
+        phi_at_xindcs = approx_features_func(x_axis_points)
+        f_sample = phi_at_xindcs @ theta_sample
+        plt.plot(x_axis_points, f_sample, '--', color='#727882')
+
+
+    for m in model.predict_f_samples(x_axis_points, 2):
+        plt.plot(x_axis_points, m, 'gray')
+
+
+    plt.show()
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
--- gpflow/gpr.py
+++ gpflow/gpr.py
@@ -19,6 +19,7 @@ from .model import GPModel
 from .densities import multivariate_normal
 from .mean_functions import Zero
 from . import likelihoods
+from .model import AutoFlow
 from .param import DataHolder
 from ._settings import settings
 float_type = settings.dtypes.float_type
@@ -37,7 +38,8 @@ class GPR(GPModel):
 
        \\log p(\\mathbf y \\,|\\, \\mathbf f) = \\mathcal N\\left(\\mathbf y\,|\, 0, \\mathbf K + \\sigma_n \\mathbf I\\right)
     """
-    def __init__(self, X, Y, kern, mean_function=None, name='name'):
+    def __init__(self, X, Y, kern, mean_function=None, name='name',
+                 random_seed_for_random_features=897):
         """
         X is a data matrix, size N x D
         Y is a data matrix, size N x R
@@ -48,6 +50,7 @@ class GPR(GPModel):
         Y = DataHolder(Y, on_shape_change='pass')
         GPModel.__init__(self, X, Y, kern, likelihood, mean_function, name)
         self.num_latent = Y.shape[1]
+        self.random_seed_for_random_features = random_seed_for_random_features
 
     def build_likelihood(self):
         """
@@ -87,3 +90,64 @@ class GPR(GPModel):
             fvar = self.kern.Kdiag(Xnew) - tf.reduce_sum(tf.square(A), 0)
             fvar = tf.tile(tf.reshape(fvar, (-1, 1)), [1, tf.shape(self.Y)[1]])
         return fmean, fvar
+
+    @AutoFlow()
+    def linear_weights_posterior(self):
+        """
+        Some kernels have finite dimensional feature maps. Others although not having finite
+        feature maps can have approximated feature vectors see eg.
+
+        ::
+            @inproceedings{rahimi2008random,
+              title={Random features for large-scale kernel machines},
+              author={Rahimi, Ali and Recht, Benjamin},
+              booktitle={Advances in neural information processing systems},
+              pages={1177--1184},
+              year={2008}
+            }
+
+        With these features, GP regression can be seen as Bayesian linear regression with Gaussian
+        priors on the initial weights vector. See Section 2.1 of:
+        ::
+            @book{rasmussen2006gaussian,
+              title={Gaussian processes for machine learning},
+              author={Rasmussen, Carl Edward and Williams, Christopher KI},
+              volume={1},
+              year={2006},
+              publisher={MIT press Cambridge}
+            }
+
+
+        This method compute the posterior mean and the lower trainglular decomposition of the
+        precision matrix for the distribution over the
+        linear weights.
+        Note that this method may not always work. If the kernel does not have a feature mapping
+        (even a random approximation) then a NotImplementedError will be raised.
+        """
+        assert self.num_latent == 1, "Only yet implemented for one latent variable GP."
+        # Pretty sure that it should work fine for more dimensional latent GP but just need
+        # to check that TF's Cholesky solve can deal with this.
+
+        # This follows almost exactly the example 2.1 of GPML as we have a tractable likelihood
+        feat_map = self.kern.create_feature_map_func(self.random_seed_for_random_features)
+        feats = feat_map(self.X)
+        num_obs = tf.shape(feats)[0]
+        num_feats = tf.shape(feats)[1]
+        # NB we currently run a naive version. However, if number of data points is smaller than
+        # the feature dimnension then I think we can use the Matrix Inversion Lemma to cut down on
+        # computation
+        Sigma_obs_inversed = tf.eye(num_obs, num_obs, dtype=float_type) / self.likelihood.variance
+
+        A = tf.matmul(feats, tf.matmul(Sigma_obs_inversed, feats), transpose_a=True) + \
+            tf.eye(num_feats, num_feats, dtype=float_type)
+        L_A = tf.cholesky(A)
+
+        unscaled_mean = tf.matmul(feats, tf.matmul(Sigma_obs_inversed, (self.Y - self.mean_function(self.X))),
+                                  transpose_a=True)
+        mean = tf.cholesky_solve(L_A, unscaled_mean)
+
+        # we return the precision matrix Cholesky decomposed as this is useful representation when
+        # wanting to sample from this function.
+        cholesky_for_precision = L_A
+        return mean, cholesky_for_precision
+
