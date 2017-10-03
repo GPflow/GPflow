Model name option argument is returned. Constrained parameter for Bernoulli had wrong shape.

gpflow/models/gpmc.py
@@ -27,7 +27,9 @@ from gpflow.priors import Gaussian
 
 class GPMC(GPModel):
     def __init__(self, X, Y, kern, likelihood,
-                 mean_function=None, num_latent=None):
+                 mean_function=None,
+                 num_latent=None,
+                 **kwargs):
         """
         X is a data matrix, size N x D
         Y is a data matrix, size N x R
@@ -47,7 +49,7 @@ class GPMC(GPModel):
         """
         X = DataHolder(X)
         Y = DataHolder(Y)
-        GPModel.__init__(self, X, Y, kern, likelihood, mean_function)
+        GPModel.__init__(self, X, Y, kern, likelihood, mean_function, **kwargs)
         self.num_data = X.shape[0]
         self.num_latent = num_latent or Y.shape[1]
         self.V = Parameter(np.zeros((self.num_data, self.num_latent)))

gpflow/models/vgp.py
@@ -46,7 +46,9 @@ class VGP(GPModel):
 
     """
     def __init__(self, X, Y, kern, likelihood,
-                 mean_function=None, num_latent=None):
+                 mean_function=None,
+                 num_latent=None,
+                 **kwargs):
         """
         X is a data matrix, size N x D
         Y is a data matrix, size N x R
@@ -56,7 +58,7 @@ class VGP(GPModel):
 
         X = DataHolder(X)
         Y = DataHolder(Y)
-        GPModel.__init__(self, X, Y, kern, likelihood, mean_function)
+        GPModel.__init__(self, X, Y, kern, likelihood, mean_function, **kwargs)
         self.num_data = X.shape[0]
         self.num_latent = num_latent or Y.shape[1]
 
@@ -151,7 +153,9 @@ class VGP_opper_archambeau(GPModel):
 
     """
     def __init__(self, X, Y, kern, likelihood,
-                 mean_function=Zero(), num_latent=None):
+                 mean_function=Zero(),
+                 num_latent=None,
+                 **kwargs):
         """
         X is a data matrix, size N x D
         Y is a data matrix, size N x R
@@ -159,7 +163,7 @@ class VGP_opper_archambeau(GPModel):
         """
         X = DataHolder(X)
         Y = DataHolder(Y)
-        GPModel.__init__(self, X, Y, kern, likelihood, mean_function)
+        GPModel.__init__(self, X, Y, kern, likelihood, mean_function, **kargs)
         self.num_data = X.shape[0]
         self.num_latent = num_latent or Y.shape[1]
         self.q_alpha = Parameter(np.zeros((self.num_data, self.num_latent)))

gpflow/params/parameter.py
@@ -155,8 +155,8 @@ class Parameter(Node):
                 raise GPflowError('Value has different shape.')
             session = self.enquire_session(session)
             self.is_built_coherence(graph=session.graph)
-            value = self._apply_transform(value)
-            self.parameter_tensor.load(value, session=session)
+            transformed_value = self._apply_transform(value)
+            self.parameter_tensor.load(transformed_value, session=session)
         else:
             self._value[...] = value
 
@@ -180,9 +180,10 @@ class Parameter(Node):
         return value
 
     def _clear(self):
-        self._unconstrained_tensor = None  # pylint: disable=W0201
-        self._prior_tensor = None          # pylint: disable=W0201
         self._externally_defined = False   # pylint: disable=W0201
+        self._prior_tensor = None          # pylint: disable=W0201
+        self._unconstrained_tensor = None  # pylint: disable=W0201
+        self._constrained_tensor = None    # pylint: disable=W0201
 
     def _build(self):
         self._unconstrained_tensor = self._build_parameter()  # pylint: disable=W0201
@@ -204,7 +205,7 @@ class Parameter(Node):
         init = tf.constant_initializer(value, dtype=settings.tf_float)
         return tf.get_variable(
             name,
-            shape=self.shape,
+            shape=value.shape,
             initializer=init,
             dtype=settings.tf_float,
             trainable=self.trainable)

gpflow/transforms.py
@@ -309,12 +309,15 @@ class LowerTriangular(Transform):
         Returns:
             Free state.
         """
-        N = int((y.size / self.num_matrices) ** 0.5)
-        y = np.reshape(y, (N, N, self.num_matrices))
-        return y[np.tril_indices(len(y), 0)].T.flatten()
+        N = int(np.sqrt(y.size / self.num_matrices))
+        reshaped = np.reshape(y, (N, N, self.num_matrices))
+        size = len(reshaped)
+        triangular = reshaped[np.tril_indices(size, 0)].T
+        return triangular
 
     def forward_tensor(self, x):
-        fwd = tf.transpose(vec_to_tri(tf.reshape(x, (self.num_matrices, -1)), self.N), [1, 2, 0])
+        reshaped = tf.reshape(x, (self.num_matrices, -1))
+        fwd = tf.transpose(vec_to_tri(reshaped, self.N), [1, 2, 0])
         return tf.squeeze(fwd) if self.squeeze else fwd
 
     def log_jacobian_tensor(self, x):