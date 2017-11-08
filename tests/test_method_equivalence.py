8bd6434675cb615314717b593dbf97d407012134
Artem Artemev <art.art.v@gmail.com>
2017-11-08 20:31:31 +0000

    Add Licences to tests.

--- tests/test_method_equivalence.py
+++ tests/test_method_equivalence.py
@@ -1,4 +1,4 @@
-# Copyright 2016 the GPflow authors.
+# Copyright 2017 the GPflow authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
