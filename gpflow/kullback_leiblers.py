48a5576f385d03d335c491c8fea86f28ba723db0
Vincent Adam <vincent.adam87@gmail.com>
2018-03-19 11:51:09 +0100

    simplify q_diag case, simplify if .. then ordering

--- gpflow/kullback_leiblers.py
+++ gpflow/kullback_leiblers.py
@@ -20,6 +20,7 @@ import tensorflow as tf
 from . import settings
 from .decors import name_scope
 
+
 @name_scope()
 def gauss_kl(q_mu, q_sqrt, K=None):
     """
@@ -41,39 +42,37 @@ def gauss_kl(q_mu, q_sqrt, K=None):
 
     K is the covariance of p.
     It is a positive definite matrix (M x M) or a tensor of stacked such matrices (L x M x M)
-    
     If K is None, compute the KL divergence to p(x) = N(0, I) instead.
     """
 
     # TODO: why do I need num_latents if I already have L (object type problem)
-    M,L = q_mu.get_shape().as_list()
+    M, L = q_mu.get_shape().as_list()
+    white = True if K is None else False
+    diag = True if q_sqrt.get_shape().ndims == 2 else False
 
-    if K is None:
-        white = True
+    if white:
         alpha = q_mu  # M x L
     else:
-        white = False
+        batch = True if K.get_shape().ndims == 3 else False
         Lp = tf.cholesky(K)  # L x M x M or M x M
-        q_mu = tf.transpose(q_mu)[:,:,None] if K.shape.ndims ==3 else q_mu  # L x M x 1 or M x L
-        alpha = tf.matrix_triangular_solve(Lp, q_mu, lower=True)  #  L x M x 1 or M x L
+        q_mu = tf.transpose(q_mu)[:, :, None] if K.shape.ndims == 3 else q_mu  # L x M x 1 or M x L
+        alpha = tf.matrix_triangular_solve(Lp, q_mu, lower=True)  # L x M x 1 or M x L
 
-    if q_sqrt.get_shape().ndims == 2:
-        diag = True
+    if diag:
         num_latent = tf.shape(q_sqrt)[1]
-        Lq = Lq_diag = q_sqrt # M x L
-    elif q_sqrt.get_shape().ndims == 3:
-        diag = False
+        Lq_diag = q_sqrt  # M x L
+        # one has to invert Lp anyway so little is wasted in expanding q_sqrt
+        Lq = tf.matrix_diag(tf.transpose(q_sqrt)) # L x M x M
+    else:
         num_latent = tf.shape(q_sqrt)[0]
-        Lq = tf.matrix_band_part(q_sqrt, -1, 0)  # force lower triangle M x L
-        Lq_diag = tf.matrix_diag_part(Lq)
-    else:  # pragma: no cover
-        raise ValueError("Bad dimension for q_sqrt: {}".format(q_sqrt.get_shape().ndims))
+        Lq = tf.matrix_band_part(q_sqrt, -1, 0)  # force lower triangle # L x M x M
+        Lq_diag = tf.matrix_diag_part(Lq) # # M x L
 
     # Mahalanobis term: μqᵀ Σp⁻¹ μq
     mahalanobis = tf.reduce_sum(tf.square(alpha))
 
     # Constant term: - N x M
-    constant = - tf.size(q_mu,out_type=settings.float_type)  # ML
+    constant = - tf.size(q_mu, out_type=settings.float_type)  # ML
 
     # Log-determinant of the covariance of q(x):
     logdet_qcov = tf.reduce_sum(tf.log(tf.square(Lq_diag)))
@@ -82,40 +81,11 @@ def gauss_kl(q_mu, q_sqrt, K=None):
     if white:
         trace = tf.reduce_sum(tf.square(Lq))
     else:
-        if diag:
-
-            if K.get_shape().ndims == 3:
-                LpT = tf.transpose(Lp,(0,2,1)) # L x M x M
-                Lp_inv = tf.matrix_triangular_solve(Lp,
-                        tf.matrix_diag(tf.ones((L, M), dtype=settings.float_type)), # L x M x M
-                        lower=True) # L x M x M
-                K_inv = tf.transpose(
-                    tf.matrix_diag_part(
-                        tf.matrix_triangular_solve(LpT, Lp_inv, lower=False) # L x M x M
-                    ) # L x M
-                ) # M x L
-            else:
-                LpT = tf.transpose(Lp) # M x M
-                Lp_inv = tf.matrix_triangular_solve(Lp,
-                        tf.eye(M, dtype=settings.float_type), #  M x M
-                        lower=True) # M x M
-                K_inv = tf.matrix_diag_part(
-                    tf.matrix_triangular_solve(LpT, Lp_inv, lower=False)  # M x M
-                )[:,None] # M x 1 (implicit extension to broadcast)
-
-            trace = tf.reduce_sum(K_inv * tf.square(q_sqrt))
-
-        else:
-
-            # If K is L x M x M, no need to tile the cholesky anymore
-            if K.get_shape().ndims == 3:
-                LpiLq = tf.matrix_triangular_solve(Lp, Lq, lower=True)
-            else:
-                Lp_tiled = tf.tile(tf.expand_dims(Lp, 0), [num_latent, 1, 1])
-                LpiLq = tf.matrix_triangular_solve(Lp_tiled, Lq, lower=True)
-
-            trace = tf.reduce_sum(tf.square(LpiLq))
-
+        # If K is L x M x M, no need to tile the cholesky anymore
+        # TODO: broadcast instead of tile when tf allows (not implemented in tf <= 1.6.0)
+        Lp_tiled = Lp if batch else tf.tile(tf.expand_dims(Lp, 0), [num_latent, 1, 1])
+        LpiLq = tf.matrix_triangular_solve(Lp_tiled, Lq, lower=True)
+        trace = tf.reduce_sum(tf.square(LpiLq))
 
     twoKL = mahalanobis + constant - logdet_qcov + trace
 
