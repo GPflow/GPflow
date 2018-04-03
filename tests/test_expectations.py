GPflow/GPflow

commit 5f2a6d406184cfd64e4add305f03f2b3090234f4

Gradient of `sqrt(RBF(Z, Z))` no longer NaN (#615)
* Gradient of `sqrt(RBF(Z, Z))` no longer NaN

Before this commit, `Kmms = sqrt(exp(-1/x)) = exp(-1/(2x))`. These two
expressions are equivalent mathematically. However, they are not
equivalent when it comes to computing finite-precision gradients
(dKmms/dx) via backpropagation when `Kmms**2 == 0.0`. Thus, we change
sqrt(exp(-1/x)) to exp(-1/(2x)). This solves #595 in the expectations
framework.

* Removed test for NaN gradients of eKxzKz

gpflow/expectations.py
@@ -305,7 +305,8 @@ def _expectation(p, kern1, feat1, kern2, feat2):
         lengthscales = kern.lengthscales if kern.ARD \
                         else tf.zeros((D,), dtype=settings.tf_float) + kern.lengthscales
 
-        Kmms = tf.sqrt(kern.K(Z, presliced=True)) / kern.variance ** 0.5
+        Kmms = tf.exp(-kern.square_dist(Z, None) / 4)
+
         scalemat = (tf.expand_dims(tf.eye(D, dtype=settings.tf_float), 0)
                     + 2 * Xcov * tf.reshape(lengthscales ** -2.0, [1, 1, -1]))  # NxDxD
         det = tf.matrix_determinant(scalemat)

commit 6f4f164c25dfa32a47109d80ce01768204a89f7d

Fixed regression to bug in #615
Refactoring `expectations.py` re-introduced the bug fixed by #615, for which we
decided to not introduce a test. This contains the bug fix and the test.

gpflow/expectations.py
@@ -404,7 +404,10 @@ def _expectation(p, kern1, feat1, kern2, feat2):
                                tf.expand_dims(two_z_CC_inv_mu, 2) - tf.expand_dims(two_z_CC_inv_mu, 1)  # NxMxM
         exponent_mahalanobis = tf.exp(-0.5 * exponent_mahalanobis)  # NxMxM
 
-        return kern.variance ** 1.5 * tf.sqrt(kern.K(Z, presliced=True)) * \
+        # Compute sqrt(self.K(Z)) explicitly to prevent automatic gradient from
+        # being NaN sometimes, see pull request #615
+        kernel_sqrt = tf.exp(-0.25 * kern.square_dist(Z, None))
+        return kern.variance ** 2 * kernel_sqrt * \
                tf.reshape(dets, [N, 1, 1]) * exponent_mahalanobis
 
 

tests/test_expectations.py
@@ -255,6 +255,30 @@ def test_eKzxKxz_no_uncertainty(session_tf, kernel, feature):
     assert_allclose(eKzxKxz, KzxKxz, rtol=RTOL)
 
 
+@test_util.session_context()
+def test_RBF_eKzxKxz_notNaN():
+    """
+    Ensure that <K_{Z, x} K_{x, Z}>_p(x) is not NaN and correct, when
+    K_{Z, Z} is zero with finite precision. See pull request #595.
+    """
+    kern = gpflow.kernels.RBF(1, lengthscales=0.1)
+    kern.variance = 2.
+
+    p = gpflow.probability_distributions.Gaussian(
+        tf.constant([[10]], dtype=gpflow.settings.tf_float),
+        tf.constant([[[0.1]]], dtype=gpflow.settings.tf_float))
+    z = gpflow.features.InducingPoints([[-10.], [10.]])
+
+    ekz = expectation(p, (kern, z), (kern, z))
+
+    g, = tf.gradients(ekz, kern.lengthscales._unconstrained_tensor)
+
+    sess = tf.get_default_session()
+    z.initialize(sess)
+    kern.initialize(sess)
+    np.testing.assert_almost_equal(sess.run(g), 0.79108496814443863)
+
+
 @pytest.mark.parametrize("kernel1", [rbf_kern_act_dim_0, lin_kern_act_dim_0])
 @pytest.mark.parametrize("kernel2", [rbf_kern_act_dim_1, lin_kern_act_dim_1])
 def test_eKzxKxz_separate_dims_simplification(