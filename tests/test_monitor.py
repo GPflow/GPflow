d1fa240e1368e3a140e96fbbefd3f1a925486d2e
Mark van der Wilk <mv310@cam.ac.uk>
2018-05-09 13:19:47 +0100

    Add very simple test for monitor.

---
+++ tests/test_monitor.py
@@ -0,0 +1,40 @@
+import itertools
+
+import numpy as np
+import tensorflow as tf
+
+import gpflow
+import gpflow.actions
+import gpflow.training.monitor as mon
+
+
+def test_monitor():
+    np.random.seed(0)
+    X = np.random.rand(10000, 1) * 10
+    Y = np.sin(X) + np.random.randn(*X.shape)
+
+    with gpflow.defer_build():
+        m = gpflow.models.SVGP(X, Y, gpflow.kernels.RBF(1), gpflow.likelihoods.Gaussian(),
+                               Z=np.linspace(0, 10, 5)[:, None],
+                               minibatch_size=100, name="SVGP")
+        m.likelihood.variance = 0.01
+    m.compile()
+
+    global_step = tf.Variable(0, trainable=False, name="global_step")
+    m.enquire_session().run(global_step.initializer)
+
+    adam = gpflow.train.AdamOptimizer(0.01).make_optimize_action(m, global_step=global_step)
+
+    # create a filewriter for summaries
+    fw = tf.summary.FileWriter('./model_tensorboard', m.graph)
+
+    print_lml = mon.PrintTimings(itertools.count(), mon.Trigger.ITER, single_line=True, global_step=global_step)
+    sleep = mon.SleepAction(itertools.count(), mon.Trigger.ITER, 0.0)
+    saver = mon.StoreSession(itertools.count(step=3), mon.Trigger.ITER, m.enquire_session(),
+                             hist_path="./monitor-saves/checkpoint", global_step=global_step)
+    tensorboard = mon.ModelTensorBoard(itertools.count(step=3), mon.Trigger.ITER, m, fw, global_step=global_step)
+    lml_tensorboard = mon.LmlTensorBoard(itertools.count(step=5), mon.Trigger.ITER, m, fw, global_step=global_step)
+
+    actions = [adam, print_lml, tensorboard, lml_tensorboard, saver, sleep]
+
+    gpflow.actions.Loop(actions, stop=11)()
