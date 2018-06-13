ef176d6a799f46481c130a73c69dadda77674f4a
ST John <st@prowler.io>
2018-06-13 11:14:56 +0100

    fix FITCvsVFE

--- doc/source/notebooks/FITCvsVFE.py
+++ doc/source/notebooks/FITCvsVFE.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import gpflow
+from gpflow.test_util import nb_niter
 import tensorflow as tf
 import os
 import numpy as np
@@ -92,9 +93,10 @@ def trainSparseModel(xtrain,ytrain,exact_model,isFITC, xtest, ytest):
     sparse_model.kern.lengthscales = exact_model.kern.lengthscales.read_value().copy()
     sparse_model.kern.variance = exact_model.kern.variance.read_value().copy()
     callback = cb(sparse_model, xtest, ytest)
+    opt = gpflow.train.ScipyOptimizer(options=dict(tol=tol))
     for repeatIndex in range(nRepeats):
-        print("repeatIndex ",repeatIndex)
-        sparse_model.optimize(disp=False, maxiter= 2000, tol=tol, callback=callback)
+        print("repeatIndex ", repeatIndex)
+        opt.minimize(sparse_model, disp=False, maxiter=nb_niter(2000), step_callback=callback)
     return sparse_model, callback
 
 def plotComparisonFigure(xtrain, sparse_model,exact_model, ax_predictions, ax_inducing_points, ax_optimization, iterations, log_likelihoods,hold_out_likelihood, title):
@@ -149,7 +151,8 @@ def snelsonDemo():
 
     #run exact inference on training data.
     exact_model = getRegressionModel(xtrain,ytrain)
-    exact_model.optimize(maxiter= 2000000, tol=tol)
+    opt = gpflow.train.ScipyOptimizer(options=dict(tol=tol))
+    opt.minimize(exact_model, maxiter=nb_niter(2000000))
 
     figA, axes = plt.subplots(1,1)
     inds = np.argsort(xtrain.flatten())
