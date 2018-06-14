7ddcaf46708bae5cab7d3cbaadba148efa18042f
ST John <st@prowler.io>
2018-06-14 09:53:15 +0100

    speed up test_hmc

--- tests/test_hmc.py
+++ tests/test_hmc.py
@@ -179,7 +179,7 @@ class CheckTrainingVariableState(GPflowTestCase):
         with self.test_context():
             m = self.model()
             hmc = gpflow.train.HMC()
-            for n in range(1, 5):
+            for n in [1, 2]:
                 samples = hmc.sample(m, num_samples=n, lmax=10, epsilon=0.05)
                 self.check_last_variables_state(m, samples)
 
