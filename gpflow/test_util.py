fbfa5e3840f54110eef3ff5727fb37170cf36965
Artem Artemev <art.art.v@gmail.com>
2018-06-19 13:03:27 +0100

    Cleaning prints in notebooks.

--- gpflow/test_util.py
+++ gpflow/test_util.py
@@ -114,16 +114,16 @@ class GPflowTestCase(tf.test.TestCase):
             yield session
 
 
-def is_continuous_integration():  # pragma: no cover
+def is_continuous_integration():
     ci = os.environ.get('CI', '').lower()
     return (ci == 'true') or (ci == '1')
 
 
-def notebook_niter(n, test_n=2):  # pragma: no cover
+def notebook_niter(n, test_n=2):
     return test_n if is_continuous_integration() else n
 
-def notebook_range(n, test_n=2):  # pragma: no cover
+def notebook_range(n, test_n=2):
     return range(notebook_niter(n, test_n))
 
-def notebook_list(lst, test_n=2):  # pragma: no cover
+def notebook_list(lst, test_n=2):
     return lst[:test_n] if is_continuous_integration() else lst
