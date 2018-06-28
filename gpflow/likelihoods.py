Softmax fix (#804)
* PEP8.

* PEP8.

* Added `SoftMax.conditional_variance()`.

* SoftMax test now tests against `Bernoulli` likelihood, including `predict_mean_and_var`.

* super

* consistency

* test comment

* bugfix

* fix tests

gpflow/likelihoods.py
@@ -14,21 +14,20 @@
 # limitations under the License.
 
 
-import tensorflow as tf
 import numpy as np
+import tensorflow as tf
 
-from . import settings
 from . import logdensities
-from . import transforms
 from . import priors
-
+from . import settings
+from . import transforms
 from .decors import params_as_tensors
 from .decors import params_as_tensors_for
+from .params import ParamList
 from .params import Parameter
 from .params import Parameterized
-from .params import ParamList
-from .quadrature import ndiagquad, ndiag_mc
 from .quadrature import hermgauss
+from .quadrature import ndiagquad, ndiag_mc
 
 
 class Likelihood(Parameterized):
@@ -181,6 +180,7 @@ class Poisson(Likelihood):
                    - tf.lgamma(Y + 1) + Y * tf.log(self.binsize)
         return super(Poisson, self).variational_expectations(Fmu, Fvar, Y)
 
+
 class Exponential(Likelihood):
     def __init__(self, invlink=tf.exp, **kwargs):
         super().__init__(**kwargs)
@@ -210,7 +210,7 @@ class StudentT(Likelihood):
         super().__init__(**kwargs)
         self.df = df
         self.scale = Parameter(scale, transform=transforms.positive,
-            dtype=settings.float_type)
+                               dtype=settings.float_type)
 
     @params_as_tensors
     def logp(self, F, Y):
@@ -222,13 +222,13 @@ class StudentT(Likelihood):
 
     @params_as_tensors
     def conditional_variance(self, F):
-        var = self.scale**2 * (self.df / (self.df - 2.0))
+        var = self.scale ** 2 * (self.df / (self.df - 2.0))
         return tf.fill(tf.shape(F), tf.squeeze(var))
 
 
 def inv_probit(x):
     jitter = 1e-3  # ensures output is strictly between 0 and 1
-    return 0.5 * (1.0 + tf.erf(x / np.sqrt(2.0))) * (1 - 2*jitter) + jitter
+    return 0.5 * (1.0 + tf.erf(x / np.sqrt(2.0))) * (1 - 2 * jitter) + jitter
 
 
 class Bernoulli(Likelihood):
@@ -245,7 +245,7 @@ class Bernoulli(Likelihood):
             return p, p - tf.square(p)
         else:
             # for other invlink, use quadrature
-            return Likelihood.predict_mean_and_var(self, Fmu, Fvar)
+            return super().predict_mean_and_var(Fmu, Fvar)
 
     def predict_density(self, Fmu, Fvar, Y):
         p = self.predict_mean_and_var(Fmu, Fvar)[0]
@@ -255,7 +255,7 @@ class Bernoulli(Likelihood):
         return self.invlink(F)
 
     def conditional_variance(self, F):
-        p = self.invlink(F)
+        p = self.conditional_mean(F)
         return p - tf.square(p)
 
 
@@ -286,9 +286,9 @@ class Gamma(Likelihood):
     def variational_expectations(self, Fmu, Fvar, Y):
         if self.invlink is tf.exp:
             return -self.shape * Fmu - tf.lgamma(self.shape) \
-                + (self.shape - 1.) * tf.log(Y) - Y * tf.exp(-Fmu + Fvar / 2.)
+                   + (self.shape - 1.) * tf.log(Y) - Y * tf.exp(-Fmu + Fvar / 2.)
         else:
-            return Likelihood.variational_expectations(self, Fmu, Fvar, Y)
+            return super().variational_expectations(Fmu, Fvar, Y)
 
 
 class Beta(Likelihood):
@@ -358,7 +358,6 @@ class RobustMax(Parameterized):
     @params_as_tensors
     def _eps_K1(self):
         return self.epsilon / (self.num_classes - 1.)
-
 
     def prob_is_largest(self, Y, mu, var, gh_x, gh_w):
         Y = tf.cast(Y, tf.int64)
@@ -540,6 +539,7 @@ class Ordinal(Likelihood):
       year={2005}
     }
     """
+
     def __init__(self, bin_edges, **kwargs):
         """
         bin_edges is a numpy array specifying at which function value the
@@ -554,8 +554,8 @@ class Ordinal(Likelihood):
     @params_as_tensors
     def logp(self, F, Y):
         Y = tf.cast(Y, tf.int64)
-        scaled_bins_left = tf.concat([self.bin_edges/self.sigma, np.array([np.inf])], 0)
-        scaled_bins_right = tf.concat([np.array([-np.inf]), self.bin_edges/self.sigma], 0)
+        scaled_bins_left = tf.concat([self.bin_edges / self.sigma, np.array([np.inf])], 0)
+        scaled_bins_right = tf.concat([np.array([-np.inf]), self.bin_edges / self.sigma], 0)
         selected_bins_left = tf.gather(scaled_bins_left, Y)
         selected_bins_right = tf.gather(scaled_bins_right, Y)
 
@@ -572,9 +572,9 @@ class Ordinal(Likelihood):
         Note that a matrix of F values is flattened.
         """
         scaled_bins_left = tf.concat([self.bin_edges / self.sigma, np.array([np.inf])], 0)
-        scaled_bins_right = tf.concat([np.array([-np.inf]), self.bin_edges/self.sigma], 0)
+        scaled_bins_right = tf.concat([np.array([-np.inf]), self.bin_edges / self.sigma], 0)
         return inv_probit(scaled_bins_left - tf.reshape(F, (-1, 1)) / self.sigma) \
-            - inv_probit(scaled_bins_right - tf.reshape(F, (-1, 1)) / self.sigma)
+               - inv_probit(scaled_bins_right - tf.reshape(F, (-1, 1)) / self.sigma)
 
     def conditional_mean(self, F):
         phi = self._make_phi(F)
@@ -595,7 +595,7 @@ class MonteCarloLikelihood(Likelihood):
         self.num_monte_carlo_points = 100
         del self.num_gauss_hermite_points
 
-    def _mc_quadrature(self, funcs, Fmu, Fvar, logspace: bool=False, epsilon=None, **Ys):
+    def _mc_quadrature(self, funcs, Fmu, Fvar, logspace: bool = False, epsilon=None, **Ys):
         return ndiag_mc(funcs, self.num_monte_carlo_points, Fmu, Fvar, logspace, epsilon, **Ys)
 
     def predict_mean_and_var(self, Fmu, Fvar, epsilon=None):
@@ -691,3 +691,7 @@ class SoftMax(MonteCarloLikelihood):
 
     def conditional_mean(self, F):
         return tf.nn.softmax(F)
+
+    def conditional_variance(self, F):
+        p = self.conditional_mean(F)
+        return p - tf.square(p)

tests/test_likelihoods.py
@@ -12,24 +12,21 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import numpy as np
 import six
 import tensorflow as tf
-
-import numpy as np
 from numpy.testing import assert_allclose
 
 import gpflow
 from gpflow import settings
 from gpflow.test_util import GPflowTestCase
 
-import pytest
-
 
 class LikelihoodSetup(object):
     def __init__(self, likelihood, Y, tolerance):
         self.likelihood, self.Y, self.tolerance = likelihood, Y, tolerance
-        self.is_analytic = six.get_unbound_function(likelihood.predict_density) is not\
-            six.get_unbound_function(gpflow.likelihoods.Likelihood.predict_density)
+        self.is_analytic = six.get_unbound_function(likelihood.predict_density) is not \
+                           six.get_unbound_function(gpflow.likelihoods.Likelihood.predict_density)
 
 
 def getLikelihoodSetups(includeMultiClass=True, addNonStandardLinks=False):
@@ -41,7 +38,7 @@ def getLikelihoodSetups(includeMultiClass=True, addNonStandardLinks=False):
         if likelihoodClass == gpflow.likelihoods.Ordinal:
             test_setups.append(
                 LikelihoodSetup(likelihoodClass(np.array([-1, 1])),
-                          rng.randint(0, 3, (10, 2)), 1e-6))
+                                rng.randint(0, 3, (10, 2)), 1e-6))
         elif likelihoodClass == gpflow.likelihoods.SwitchedLikelihood:
             continue  # switched likelihood tested separately
         elif likelihoodClass == gpflow.likelihoods.MultiClass:
@@ -51,25 +48,26 @@ def getLikelihoodSetups(includeMultiClass=True, addNonStandardLinks=False):
                 tolerance = 1e-3
                 test_setups.append(
                     LikelihoodSetup(likelihoodClass(2),
-                              np.argmax(sample, 1).reshape(-1, 1), tolerance))
+                                    np.argmax(sample, 1).reshape(-1, 1), tolerance))
         else:
             # most likelihoods follow this standard:
             test_setups.append(
                 LikelihoodSetup(likelihoodClass(),
-                          rng.rand(10, 2).astype(settings.float_type), 1e-6))
+                                rng.rand(10, 2).astype(settings.float_type), 1e-6))
 
     if addNonStandardLinks:
         test_setups.append(LikelihoodSetup(gpflow.likelihoods.Poisson(invlink=tf.square),
-                                     rng.rand(10, 2).astype(settings.float_type), 1e-6))
+                                           rng.rand(10, 2).astype(settings.float_type), 1e-6))
         test_setups.append(LikelihoodSetup(gpflow.likelihoods.Exponential(invlink=tf.square),
-                                     rng.rand(10, 2).astype(settings.float_type), 1e-6))
+                                           rng.rand(10, 2).astype(settings.float_type), 1e-6))
         test_setups.append(LikelihoodSetup(gpflow.likelihoods.Gamma(invlink=tf.square),
-                                     rng.rand(10, 2).astype(settings.float_type), 1e-6))
+                                           rng.rand(10, 2).astype(settings.float_type), 1e-6))
 
         def sigmoid(x):
-            return 1./(1 + tf.exp(-x))
+            return 1. / (1 + tf.exp(-x))
+
         test_setups.append(LikelihoodSetup(gpflow.likelihoods.Bernoulli(invlink=sigmoid),
-                                     rng.rand(10, 2).astype(settings.float_type), 1e-6))
+                                           rng.rand(10, 2).astype(settings.float_type), 1e-6))
     return test_setups
 
 
@@ -79,6 +77,7 @@ class TestPredictConditional(GPflowTestCase):
     give the same result as the predict_mean_and_var function if the prediction
     has no uncertainty.
     """
+
     def setUp(self):
         self.test_graph = tf.Graph()
 
@@ -135,6 +134,7 @@ class TestQuadrature(GPflowTestCase):
     Where quadrature methods have been overwritten, make sure the new code
     does something close to the quadrature
     """
+
     def setUp(self):
         self.test_graph = tf.Graph()
         self.rng = np.random.RandomState()
@@ -261,11 +261,20 @@ class TestSoftMax(GPflowTestCase):
             feed[tensor] = data.astype(dtype)
             return tensor
 
-        F = make_tensor(self.rng.randn(num, dimF))
-        Y = make_tensor(self.rng.randn(num, dimY) > 0, settings.int_type)  # 0 or 1
+        dF = np.vstack((self.rng.randn(num - 3, dimF), np.array([[-3., 0.], [3, 0.], [0., 0.]]))) if dimF == 2 else \
+            self.rng.randn(num, dimF)
+        dY = np.vstack((self.rng.randn(num - 3, dimY), np.ones((3, dimY)))) > 0
+        F = make_tensor(dF)
+        Y = make_tensor(dY, settings.int_type)  # 0 or 1
         return F, Y, feed
 
     def test_y_shape_assert(self):
+        """
+        SoftMax assumes the class is given as a label (not, e.g., one-hot
+        encoded), and hence just uses the first column of Y. To prevent
+        silent errors, there is a tf assertion that ensures Y only has one
+        dimension. This test checks that this assert works as intended.
+        """
         with self.test_context() as sess:
             F, Y, feed = self.prepare(dimF=5, dimY=2)
             l = gpflow.likelihoods.SoftMax(5)
@@ -278,29 +287,48 @@ class TestSoftMax(GPflowTestCase):
     def test_bernoulli_equivalence(self):
         with self.test_context() as sess:
             F, Y, feed = self.prepare(dimF=2, dimY=1)
+            Fvar = tf.exp(tf.stack([F[:, 1], -10.0 + tf.zeros(tf.shape(F)[0], dtype=F.dtype)], axis=1))
+            F = tf.stack([F[:, 0], tf.zeros(tf.shape(F)[0], dtype=F.dtype)], axis=1)
+            Ylabel = 1 - Y  # We need the 1 - Y, as we need to pass the *label* to SoftMax
 
-            q = tf.exp(F[:,0] - F[:,1])[:,None]
-            p = 1. / (1. + q)
+            def logistic_link(x):
+                return 1.0 / (1.0 + tf.exp(-x))
 
-            l = gpflow.likelihoods.SoftMax(2)
-            l.compile()
+            ls = gpflow.likelihoods.SoftMax(2)
+            ls.num_monte_carlo_points = 10000000
+            ls.compile()
+            lb = gpflow.likelihoods.Bernoulli(invlink=logistic_link)
+            lb.num_gauss_hermite_points = 50
+            lb.compile()
 
-            logp_softmax = sess.run(l.logp(F, Y), feed_dict=feed)
-            logp_bernoulli = sess.run(gpflow.logdensities.bernoulli(Y, p), feed_dict=feed)
+            ls_cm = sess.run(ls.conditional_mean(F), feed_dict=feed)[:, :1]
+            lb_cm = sess.run(lb.conditional_mean(F[:, :1]), feed_dict=feed)
+            ls_cv = sess.run(ls.conditional_variance(F), feed_dict=feed)[:, :1]
+            lb_cv = sess.run(lb.conditional_variance(F[:, :1]), feed_dict=feed)
+            ls_lp = sess.run(ls.logp(F, Ylabel), feed_dict=feed)
+            lb_lp = sess.run(lb.logp(F[:, :1], Y), feed_dict=feed)
 
-            assert_allclose(logp_softmax, logp_bernoulli)
+            assert_allclose(ls_cm, lb_cm)
+            assert_allclose(ls_cv, lb_cv)
+            assert_allclose(ls_lp, lb_lp)
 
-            cm_softmax = sess.run(l.conditional_mean(F), feed_dict=feed)
-            p_np = sess.run(p, feed_dict=feed)
-            cm_bernoulli = np.c_[1 - p_np, p_np]
+            ls_pm, ls_pv = sess.run(ls.predict_mean_and_var(F, Fvar), feed_dict=feed)
+            lb_pm, lb_pv = sess.run(lb.predict_mean_and_var(F[:, :1], Fvar[:, :1]), feed_dict=feed)
 
-            assert_allclose(cm_softmax, cm_bernoulli)
+            assert_allclose(ls_pm[:, 0, None], lb_pm, rtol=1e-3)
+            assert_allclose(ls_pv[:, 0, None], lb_pv, rtol=1e-3)
+
+            ls_ve = sess.run(ls.variational_expectations(F, Fvar, Ylabel), feed_dict=feed)
+            lb_ve = sess.run(lb.variational_expectations(F[:, :1], Fvar[:, :1], Y), feed_dict=feed)
+
+            assert_allclose(ls_ve[:, 0, None], lb_ve, rtol=1e-3)
 
 
 class TestRobustMaxMulticlass(GPflowTestCase):
     """
     Some specialized tests to the multiclass likelihood with RobustMax inverse link function.
     """
+
     def setUp(self):
         self.test_graph = tf.Graph()
 
@@ -328,18 +356,19 @@ class TestRobustMaxMulticlass(GPflowTestCase):
             pred = session.run(l.predict_density(F, F, Y), feed_dict=feed)
             variational_expectations = session.run(
                 l.variational_expectations(F, F, Y), feed_dict=feed)
-            expected_mu = (1./nClasses * (1. - epsilon) + (1. - 1. / nClasses) *\
+            expected_mu = (1. / nClasses * (1. - epsilon) + (1. - 1. / nClasses) * \
                            epsilon / (nClasses - 1)) * np.ones((nPoints, 1))
 
-            self.assertTrue(np.allclose(mu, expected_mu, tolerance, tolerance))  # assert_allclose() would complain about shape mismatch
+            self.assertTrue(np.allclose(mu, expected_mu, tolerance,
+                                        tolerance))  # assert_allclose() would complain about shape mismatch
             expected_log_denisty = np.log(expected_mu)
             self.assertTrue(np.allclose(pred, expected_log_denisty, 1e-3, 1e-3))
-            validation_variational_expectation = 1./nClasses * np.log(1. - epsilon) + \
-                (1. - 1./nClasses) * np.log(epsilon / (nClasses - 1))
+            validation_variational_expectation = 1. / nClasses * np.log(1. - epsilon) + \
+                                                 (1. - 1. / nClasses) * np.log(epsilon / (nClasses - 1))
             assert_allclose(
-                    variational_expectations,
-                    np.ones((nPoints, 1)) * validation_variational_expectation,
-                    tolerance, tolerance)
+                variational_expectations,
+                np.ones((nPoints, 1)) * validation_variational_expectation,
+                tolerance, tolerance)
 
     def testPredictDensity(self):
         tol = 1e-4
@@ -391,12 +420,11 @@ class TestRobustMaxMulticlass(GPflowTestCase):
             self.assertAlmostEqual(expected_eps_k2, actual_eps_k2)
 
 
-
-
 class TestMulticlassIndexFix(GPflowTestCase):
     """
     A regression test for a bug in multiclass likelihood.
     """
+
     def testA(self):
         with self.test_context():
             mu = tf.placeholder(settings.float_type)
@@ -412,17 +440,18 @@ class TestSwitchedLikelihood(GPflowTestCase):
     SwitchedLikelihood is separately tested here.
     Here, we make sure the partition-stitch works fine.
     """
+
     def setUp(self):
         self.test_graph = tf.Graph()
 
         with self.test_context():
             rng = np.random.RandomState(1)
-            self.Y_list = [rng.randn(3, 2),  rng.randn(4, 2),  rng.randn(5, 2)]
-            self.F_list = [rng.randn(3, 2),  rng.randn(4, 2),  rng.randn(5, 2)]
-            self.Fvar_list = [np.exp(rng.randn(3, 2)),  np.exp(rng.randn(4, 2)),
+            self.Y_list = [rng.randn(3, 2), rng.randn(4, 2), rng.randn(5, 2)]
+            self.F_list = [rng.randn(3, 2), rng.randn(4, 2), rng.randn(5, 2)]
+            self.Fvar_list = [np.exp(rng.randn(3, 2)), np.exp(rng.randn(4, 2)),
                               np.exp(rng.randn(5, 2))]
-            self.Y_label = [np.ones((3, 1))*0, np.ones((4, 1))*1, np.ones((5, 1))*2]
-            self.Y_perm = list(range(3+4+5))
+            self.Y_label = [np.ones((3, 1)) * 0, np.ones((4, 1)) * 1, np.ones((5, 1)) * 2]
+            self.Y_perm = list(range(3 + 4 + 5))
             rng.shuffle(self.Y_perm)
 
             # shuffle the original data
@@ -487,6 +516,7 @@ class TestSwitchedLikelihoodRegression(GPflowTestCase):
     one. The final column of Y is used to index the switch. If the number of
     latent functions does not match, an exception will be raised.
     """
+
     def setUp(self):
         self.test_graph = tf.Graph()
 