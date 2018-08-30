4f77c8706e114e817fb73242f59305f9ce6e597c
ST John <st@prowler.io>
2018-08-30 10:30:08 +0100

    fix types

--- gpflow/types.py
+++ gpflow/types.py
@@ -2,5 +2,7 @@ from typing import Union
 import numpy as np
 import tensorflow as tf
 
-TensorArray = Union[np.ndarray, tf.Tensor]
+
+# this should be a Union[] but multipledispatch module requires unions as tuples
+TensorArray = (np.ndarray, tf.Tensor)
 
