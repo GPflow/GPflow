f31a914bd38b00affc978d7ef1834e1303af5b4c
Artem Artemev <art.art.v@gmail.com>
2018-09-20 11:34:08 +0100

    Remove unnecessary parts

--- gpflow/models/model.py
+++ gpflow/models/model.py
@@ -24,84 +24,32 @@ from ..decors import autoflow
 from ..mean_functions import Zero
 
 
-class Model(Parameterized):
-    def __init__(self, name=None):
-        """
-        Name is a string describing this model.
-        """
-        super(Model, self).__init__(name=name)
-        self._objective = None
-        self._likelihood_tensor = None
-
-    @property
-    def objective(self):
-        return self._objective
-
-    @property
-    def likelihood_tensor(self):
-        return self._likelihood_tensor
-
-    @autoflow()
-    def compute_log_prior(self):
-        """Compute the log prior of the model."""
-        return self.prior_tensor
-
-    @autoflow()
-    def compute_log_likelihood(self):
-        """Compute the log likelihood of the model."""
-        return self.likelihood_tensor
-
-    def is_built(self, graph):
-        is_built = super().is_built(graph)
-        if is_built is not Build.YES:
-            return is_built
-        if self._likelihood_tensor is None:
-            return Build.NO
-        return Build.YES
-
-    def build_objective(self):
-        likelihood = self._build_likelihood()
-        priors = []
-        for param in self.parameters:
-            unconstrained = param.unconstrained_tensor
-            constrained = param._build_constrained(unconstrained)
-            priors.append(param._build_prior(unconstrained, constrained))
-        prior = self._build_prior(priors)
-        return self._build_objective(likelihood, prior)
-
-    def _clear(self):
-        super(Model, self)._clear()
-        self._likelihood_tensor = None
-        self._objective = None
-
-    def _build(self):
-        super(Model, self)._build()
-        likelihood = self._build_likelihood()
-        prior = self.prior_tensor
-        objective = self._build_objective(likelihood, prior)
-        self._likelihood_tensor = likelihood
-        self._objective = objective
-
-    def sample_feed_dict(self, sample):
-        tensor_feed_dict = {}
-        for param in self.parameters:
-            if not param.trainable: continue
-            constrained_value = sample[param.pathname]
-            unconstrained_value = param.transform.backward(constrained_value)
-            tensor = param.unconstrained_tensor
-            tensor_feed_dict[tensor] = unconstrained_value
-        return tensor_feed_dict
-
-    def _build_objective(self, likelihood_tensor, prior_tensor):
-        func = tf.add(likelihood_tensor, prior_tensor, name='nonneg_objective')
-        return tf.negative(func, name='objective')
-
-    @abc.abstractmethod
-    def _build_likelihood(self):
-        raise NotImplementedError('')  # TODO(@awav): write error message
+MeanAndVariance = Tuple[tf.Tensor, tf.Tensor]
 
 
-class GPModel(Model):
+class covstruct(Enum):
+    none = 0
+    diag = 1
+    full = 2
+
+
+class BayesianModel(Module):
+    """ Bayesian model. """
+
+    def negative_log_marginal_likelihood(self, *args, **kwargs) -> tf.Tensor:
+        return - tf.add(self.log_likelihood(*args, **kwargs), self.log_prior())
+
+    def log_prior(self) -> tf.Tensor:
+        if len(self.parameters()) == 0:
+            return 0
+        return tf.add_n([p.log_prior() for p in self.parameters])
+
+    @abstractmethod
+    def log_likelihood(self, *args, **kwargs) -> tf.Tensor:
+        pass
+
+
+class GPModel(BaysianModel):
     """
     A base class for Gaussian process models, that is, those of the form
 
@@ -132,65 +80,47 @@ class GPModel(Model):
     >>> m.Y = Ynew
     """
 
-    def __init__(self, X, Y, kern, likelihood, mean_function,
-                 num_latent=None, name=None):
-        super(GPModel, self).__init__(name=name)
-        self.num_latent = num_latent or Y.shape[1]
-        self.mean_function = mean_function or Zero(output_dim=self.num_latent)
-        self.kern = kern
+    def __init__(self,
+                 kernel: Kernel,
+                 likelihood: Likelihood,
+                 mean_function: Optional[MeanFunction] = None,
+                 num_latent: int = 1,
+                 seed: Optional[int] = None):
+        super().__init__()
+        self.mean_function = mean_function
+        self.num_latent = num_latent
+        if mean_function is not None:
+            self.num_latent = len(mean_function)
+        self.kernel = kernel
         self.likelihood = likelihood
 
-        if isinstance(X, np.ndarray):
-            # X is a data matrix; each row represents one instance
-            X = DataHolder(X)
-        if isinstance(Y, np.ndarray):
-            # Y is a data matrix, rows correspond to the rows in X,
-            # columns are treated independently
-            Y = DataHolder(Y)
-        self.X, self.Y = X, Y
+    @abstractmethod
+    def predict_f(self, X: tf.Tensor, cov_struct=None) -> MeanAndVariance:
+        pass
 
-    @autoflow((settings.float_type, [None, None]))
-    def predict_f(self, Xnew):
-        """
-        Compute the mean and variance of the latent function(s) at the points
-        Xnew.
-        """
-        return self._build_predict(Xnew)
-
-    @autoflow((settings.float_type, [None, None]))
-    def predict_f_full_cov(self, Xnew):
-        """
-        Compute the mean and covariance matrix of the latent function(s) at the
-        points Xnew.
-        """
-        return self._build_predict(Xnew, full_cov=True)
-
-    @autoflow((settings.float_type, [None, None]), (tf.int32, []))
-    def predict_f_samples(self, Xnew, num_samples):
+    def predict_f_samples(self, X, num_samples):
         """
         Produce samples from the posterior latent function(s) at the points
         Xnew.
         """
-        mu, var = self._build_predict(Xnew, full_cov=True)  # N x P, # P x N x N
-        jitter = tf.eye(tf.shape(mu)[0], dtype=settings.float_type) * settings.numerics.jitter_level
+        mu, var = self.predict_f(X, cov_struct=covstruct.full)  # [N, P] or [P, N, N]
+        jitter = tf.eye(tf.shape(mu)[0], dtype=X.dtype) * settings.numerics.jitter_level
         samples = []
         for i in range(self.num_latent):
             L = tf.cholesky(var[i, :, :] + jitter)
             shape = tf.stack([tf.shape(L)[0], num_samples])
-            V = tf.random_normal(shape, dtype=settings.float_type)
-            samples.append(mu[:, i:i + 1] + tf.matmul(L, V))
-        return tf.transpose(tf.stack(samples))
+            V = tf.random_normal(shape, dtype=settings.float_type, seed=self.seed)
+            samples[i] = mu[:, i:(i+1)] + L @ V
+        return tf.matrix_transpose(tf.stack(samples))
 
-    @autoflow((settings.float_type, [None, None]))
-    def predict_y(self, Xnew):
+    def predict_y(self, X):
         """
-        Compute the mean and variance of held-out data at the points Xnew
+        Compute the mean and variance of held-out data at the points X
         """
-        pred_f_mean, pred_f_var = self._build_predict(Xnew)
-        return self.likelihood.predict_mean_and_var(pred_f_mean, pred_f_var)
+        f_mean, f_var = self.predict_f(X)
+        return self.likelihood.predict_mean_and_var(f_mean, f_var)
 
-    @autoflow((settings.float_type, [None, None]), (settings.float_type, [None, None]))
-    def predict_density(self, Xnew, Ynew):
+    def predict_log_density(self, X, Y):
         """
         Compute the (log) density of the data Ynew at the points Xnew
 
@@ -198,9 +128,5 @@ class GPModel(Model):
         ignoring correlations between them. The result is a matrix the same
         shape as Ynew containing the log densities.
         """
-        pred_f_mean, pred_f_var = self._build_predict(Xnew)
-        return self.likelihood.predict_density(pred_f_mean, pred_f_var, Ynew)
-
-    @abc.abstractmethod
-    def _build_predict(self, *args, **kwargs):
-        raise NotImplementedError('') # TODO(@awav): write error message
+        f_mean, f_var = self.predict_f(X)
+        return self.likelihood.predict_density(f_mean, f_var, Y)
