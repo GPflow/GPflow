d604390d7bca81bd35e90f7a30e5e3c3ae5944dc
Artem Artemev <art.art.v@gmail.com>
2018-11-24 12:25:47 +0000

    Debugging Travis

--- travis.sh
+++ travis.sh
@@ -4,6 +4,10 @@ set -ev
 
 TESTRUN="pytest -W ignore::UserWarning --durations=5 --cov=./gpflow -n auto"
 
+echo ${TRAVIS_BRANCH}
+echo ${TRAVIS_PULL_REQUEST}
+echo ${TRAVIS_PULL_REQUEST_BRANCH}
+
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "develop" ]; then
     ${TESTRUN} -k 'not notebooks' ./tests;
 else
