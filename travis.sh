66f52874091c9e28d84a795cce6ff6ca7ae3020b
Artem Artemev <art.art.v@gmail.com>
2018-11-24 12:12:08 +0000

    Remove printing in travis.sh

--- travis.sh
+++ travis.sh
@@ -2,11 +2,7 @@
 
 set -ex
 
-
 TESTRUN="pytest -W ignore::UserWarning --durations=5 --cov=./gpflow -n auto"
-
-echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
-echo "TRAVIS_PULL_REQUEST_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}"
 
 if [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "develop" ]; then
     ${TESTRUN} -k 'not notebooks' ./tests;
