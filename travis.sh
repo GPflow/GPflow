4f981c18a4273c42f849b913c334cd14fe4b2638
Artem Artemev <art.art.v@gmail.com>
2018-11-24 20:56:48 +0000

    Debugging

--- travis.sh
+++ travis.sh
@@ -5,13 +5,13 @@ set -ex
 
 TESTRUN="pytest -W ignore::UserWarning --durations=5 --cov=./gpflow -n auto"
 
-echo ${TRAVIS_BRANCH}
-echo ${TRAVIS_PULL_REQUEST}
-echo ${TRAVIS_PULL_REQUEST_BRANCH}
+echo "TRAVIS_COMMIT_MESSAGE=${TRAVIS_COMMIT_MESSAGE}"
+echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
+echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
+echo "TRAVIS_PULL_REQUEST_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}"
+echo $(git branch)
 
-if [[ "${TRAVIS_BRANCH}" =~ ^(master|develop)$ ]] || \
-   [ "${TRAVIS_PULL_REQUEST}" != "false" ] && \
-   [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "develop" ]; then
+if [[ "${TRAVIS_BRANCH}" =~ ^(master|develop)$ ]]; then
     ${TESTRUN} -k 'not notebooks' ./tests;
 else
     ${TESTRUN} ./tests;
