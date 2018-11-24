29081536189c9dde618033cfb4a914137547cb83
Artem Artemev <art.art.v@gmail.com>
2018-11-24 00:05:53 +0000

    Travis script

---
+++ travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -ev
+
+TESTRUN="pytest -W ignore::UserWarning --durations=5 --cov=./gpflow -n auto"
+
+if [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "develop" ]; then
+    ${TESTRUN} -k 'not notebooks' ./tests;
+else
+    ${TESTRUN} ./tests;
+fi
+
+codecov --token=2ae2a756-f39c-467c-bd9c-4bdb3dc439c8
