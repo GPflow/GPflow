fa939a4dcf73aaef589d4304832a8e3c628626e0
Artem Artemev <art.art.v@gmail.com>
2018-11-24 21:22:43 +0000

    Debugging

--- travis.sh
+++ travis.sh
@@ -11,10 +11,13 @@ echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
 echo "TRAVIS_PULL_REQUEST_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}"
 echo $(git status -s -b)
 
-if [[ ${TRAVIS_BRANCH} =~ ^(master|develop)$ ]]; then
-    ${TESTRUN} -k 'not notebooks' ./tests;
+if [[ ! ${TRAVIS_BRANCH} =~ ^(master|develop)$ ]]; then
+    ${TESTRUN} ./tests
+# Special case for PRs from develop to master
+elif [[ ${TRAVIS_PULL_REQUEST} != "false" ]] && [[ ${TRAVIS_PULL_REQUEST_BRANCH} != "master" ]]
+    ${TESTRUN} ./tests
 else
-    ${TESTRUN} ./tests;
+    ${TESTRUN} -k 'not notebooks' ./tests
 fi
 
 codecov --token=2ae2a756-f39c-467c-bd9c-4bdb3dc439c8
