0ab60c34477a5643bbcaee5b0a4850c77cc91256
Artem Artemev <art.art.v@gmail.com>
2018-11-24 22:42:05 +0000

    Debug Travis

--- travis.sh
+++ travis.sh
@@ -4,10 +4,19 @@ set -ex
 
 TESTRUN="pytest -W ignore::UserWarning --durations=5 --cov=./gpflow -n auto"
 
-if [ "${TRAVIS_PULL_REQUEST}" != "false" ] && [ "${TRAVIS_PULL_REQUEST_BRANCH}" != "develop" ]; then
-    ${TESTRUN} -k 'not notebooks' ./tests;
+echo "TRAVIS_COMMIT_MESSAGE=${TRAVIS_COMMIT_MESSAGE}"
+echo "TRAVIS_BRANCH=${TRAVIS_BRANCH}"
+echo "TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST}"
+echo "TRAVIS_PULL_REQUEST_BRANCH=${TRAVIS_PULL_REQUEST_BRANCH}"
+echo $(git status -s -b)
+
+if [[ ! ${TRAVIS_BRANCH} =~ ^(master|develop)$ ]]; then
+    ${TESTRUN} -k 'not notebooks' ./tests
+# Special case for PRs from develop to master
+elif [[ ${TRAVIS_PULL_REQUEST} != "false" ]] && [[ ${TRAVIS_PULL_REQUEST_BRANCH} != "master" ]]; then
+    ${TESTRUN} -k 'not notebooks' ./tests
 else
-    ${TESTRUN} ./tests;
+    ${TESTRUN} ./tests
 fi
 
 codecov --token=2ae2a756-f39c-467c-bd9c-4bdb3dc439c8
