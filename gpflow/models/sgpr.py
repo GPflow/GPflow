d2795f636d1935940185bf94a42c22548ac740ce
Artem Artemev <art.art.v@gmail.com>
2018-12-01 21:47:21 +0000

    Fix inducing point tests

--- gpflow/models/sgpr.py
+++ gpflow/models/sgpr.py
@@ -346,8 +346,8 @@ class GPRFITC(GPModel, SGPRUpperMixin):
 
     @property
     def Z(self):
-        raise NotImplementedError("Inducing points are now in `model.features.Z`.")
+        raise NotImplementedError("Inducing points are now in `model.feature.Z`.")
 
     @Z.setter
     def Z(self, _):
-        raise NotImplementedError("Inducing points are now in `model.features.Z`.")
+        raise NotImplementedError("Inducing points are now in `model.feature.Z`.")
