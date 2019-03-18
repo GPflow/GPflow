1bb5a047c726e00b41b9ab23ea59ad862681d680
Sergio Diaz <sergio.diaz@prowler.io>
2019-03-18 15:11:28 +0000

    Quadrature test with pytest

--- unsorted_tests/test_quadrature.py
+++ unsorted_tests/test_quadrature.py
@@ -18,74 +18,58 @@ import tensorflow as tf
 from numpy.testing import assert_allclose
 
 import gpflow.quadrature as quadrature
-from gpflow.util import default_float
-
-
-@pytest.fixture
-def mu1(): return np.array([1.0, 1.3])
-
-
-@pytest.fixture
-def mu2(): return np.array([-2.0, 0.3])
-
-
-@pytest.fixture
-def var1(): return np.array([3.0, 3.5])
-
-
-@pytest.fixture
-def var2(): return np.array([4.0, 4.2])
-
-
-def cast(x):
-    return x
-    # return tf.cast(np.asarray(x), dtype=default_float())
 
 
 @pytest.mark.parametrize('mu', [np.array([1.0, 1.3])])
 @pytest.mark.parametrize('var', [np.array([3.0, 3.5])])
 def test_diagquad_1d(mu, var):
+    num_gauss_hermite_points = 25
     quad = quadrature.ndiagquad(
-        [lambda *X: tf.exp(X[0])], 25,
-        [cast(mu)], [cast(var)])
+        [lambda *X: tf.exp(X[0])], num_gauss_hermite_points,
+        [mu], [var])
     expected = np.exp(mu + var / 2)
     assert_allclose(quad[0], expected)
 
 
 @pytest.mark.parametrize('mu1', [np.array([1.0, 1.3])])
 @pytest.mark.parametrize('var1', [np.array([3.0, 3.5])])
-@pytest.mark.parametrize('mu2', [np.array([1.0, 1.3])])
-@pytest.mark.parametrize('var2', [np.array([3.0, 3.5])])
+@pytest.mark.parametrize('mu2', [np.array([-2.0, 0.3])])
+@pytest.mark.parametrize('var2', [np.array([4.0, 4.2])])
 def test_diagquad_2d(mu1, var1, mu2, var2):
     alpha = 2.5
+    # using logspace=True we can reduce this, see test_diagquad_logspace
+    num_gauss_hermite_points = 35
     quad = quadrature.ndiagquad(
         lambda *X: tf.exp(X[0] + alpha * X[1]),
-        35,  # using logspace=True we can reduce this, see test_diagquad_logspace
-        [cast(mu1), cast(mu2)], [cast(var1), cast(var2)])
+        num_gauss_hermite_points,
+        [mu1, mu2], [var1, var2])
     expected = np.exp(mu1 + var1 / 2 + alpha * mu2 + alpha ** 2 * var2 / 2)
     assert_allclose(quad, expected)
 
 
 @pytest.mark.parametrize('mu1', [np.array([1.0, 1.3])])
 @pytest.mark.parametrize('var1', [np.array([3.0, 3.5])])
-@pytest.mark.parametrize('mu2', [np.array([1.0, 1.3])])
-@pytest.mark.parametrize('var2', [np.array([3.0, 3.5])])
+@pytest.mark.parametrize('mu2', [np.array([-2.0, 0.3])])
+@pytest.mark.parametrize('var2', [np.array([4.0, 4.2])])
 def test_diagquad_logspace(mu1, var1, mu2, var2):
     alpha = 2.5
+    num_gauss_hermite_points = 25
     quad = quadrature.ndiagquad(
         lambda *X: (X[0] + alpha * X[1]),
-        25,
-        [cast(mu1), cast(mu2)], [cast(var1), cast(var2)],
+        num_gauss_hermite_points,
+        [mu1, mu2], [var1, var2],
         logspace=True)
     expected = mu1 + var1 / 2 + alpha * mu2 + alpha ** 2 * var2 / 2
     assert_allclose(quad, expected)
 
 
-
-def test_diagquad_with_kwarg(mu2, var2):
+@pytest.mark.parametrize('mu1', [np.array([1.0, 1.3])])
+@pytest.mark.parametrize('var1', [np.array([3.0, 3.5])])
+def test_diagquad_with_kwarg(mu1, var1):
     alpha = np.array([2.5, -1.3])
+    num_gauss_hermite_points = 25
     quad = quadrature.ndiagquad(
-        lambda X, Y: tf.exp(X * Y), 25,
-        cast(mu2), cast(var2), Y=alpha)
-    expected = np.exp(alpha * mu2 + alpha ** 2 * var2 / 2)
+        lambda X, Y: tf.exp(X * Y), num_gauss_hermite_points,
+        mu1, var1, Y=alpha)
+    expected = np.exp(alpha * mu1 + alpha ** 2 * var1 / 2)
     assert_allclose(quad, expected)
