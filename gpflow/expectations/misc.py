Fixed numerical errors in Expectations tests

gpflow/expectations/misc.py
@@ -41,7 +41,8 @@ def _E(p, kern, feat, mean, _, nghp=None):
     return tf.linalg.transpose(expectation(p, mean, (kern, feat), nghp=nghp))
 
 
-@dispatch.expectation.register(Gaussian, mfn.Constant, NoneType, kernels.Kernel, InducingPoints)
+@dispatch.expectation.register(Gaussian, (mfn.Constant, mfn.Zero),
+                               NoneType, kernels.Kernel, InducingPoints)
 def _E(p, constant_mean, _, kern, feat, nghp=None):
     """
     Compute the expectation:
@@ -71,7 +72,8 @@ def _E(p, linear_mean, _, kern, feat, nghp=None):
     D = p.mu.shape[1]
     exKxz = expectation(p, mfn.Identity(D), (kern, feat), nghp=nghp)
     eKxz = expectation(p, (kern, feat), nghp=nghp)
-    eAxKxz = tf.linalg.matmul(tf.tile(linear_mean.A[None, :, :], (N, 1, 1)), exKxz, transpose_a=True)
+    eAxKxz = tf.linalg.matmul(tf.tile(linear_mean.A[None, :, :], (N, 1, 1)), exKxz,
+                              transpose_a=True)
     ebKxz = linear_mean.b[None, :, None] * eKxz[:, None, :]
     return eAxKxz + ebKxz
 

gpflow/kernels/base.py
@@ -61,7 +61,7 @@ class Kernel(tf.Module):
 
         this_dims = tf.reshape(self.active_dims, (-1, 1))
         other_dims = tf.reshape(other.active_dims, (1, -1))
-        return not np.any(this_dims == other_dims)
+        return not np.any(tf.equal(this_dims, other_dims))
 
     def slice(self, X: tf.Tensor, Y: Optional[tf.Tensor] = None):
         """

gpflow/mean_functions.py
@@ -112,10 +112,11 @@ class Constant(MeanFunction):
         return tf.tile(tf.reshape(self.c, (1, -1)), shape)
 
 
-class Zero(MeanFunction):
+class Zero(Constant):
     def __init__(self, output_dim=1):
-        super().__init__()
+        Constant.__init__(self)
         self.output_dim = output_dim
+        del self.c
 
     def __call__(self, X):
         return tf.zeros((X.shape[0], self.output_dim), dtype=X.dtype)

tests/test_expectations.py
@@ -64,17 +64,19 @@ _distrs = {
     'dirac_gauss': Gaussian(ctt(Xmu), ctt(np.zeros((num_data, D_in, D_in)))),
     'gauss_diag': DiagonalGaussian(ctt(Xmu), ctt(rng.rand(num_data, D_in))),
     'dirac_diag': DiagonalGaussian(ctt(Xmu), ctt(np.zeros((num_data, D_in)))),
-    'dirac_markov_gauss': MarkovGaussian(ctt(Xmu_markov), ctt(np.zeros((2, num_data + 1, D_in, D_in)))),
+    'dirac_markov_gauss': MarkovGaussian(ctt(Xmu_markov),
+                                         ctt(np.zeros((2, num_data + 1, D_in, D_in)))),
     'markov_gauss': markov_gauss()
 }
-
 
 _kerns = {
     'rbf': kernels.RBF(variance=rng.rand(), lengthscales=rng.rand() + 1.),
     'lin': kernels.Linear(variance=rng.rand()),
     'matern': kernels.Matern32(variance=rng.rand()),
-    'rbf_act_dim_0': kernels.RBF(variance=rng.rand(), lengthscales=rng.rand() + 1., active_dims=[0]),
-    'rbf_act_dim_1': kernels.RBF(variance=rng.rand(), lengthscales=rng.rand() + 1., active_dims=[1]),
+    'rbf_act_dim_0': kernels.RBF(variance=rng.rand(), lengthscales=rng.rand() + 1.,
+                                 active_dims=[0]),
+    'rbf_act_dim_1': kernels.RBF(variance=rng.rand(), lengthscales=rng.rand() + 1.,
+                                 active_dims=[1]),
     'lin_act_dim_0': kernels.Linear(variance=rng.rand(), active_dims=[0]),
     'lin_act_dim_1': kernels.Linear(variance=rng.rand(), active_dims=[1]),
     'rbf_lin_sum': kernels.Sum([
@@ -141,7 +143,8 @@ def test_mean_function_only_expectations(distribution, mean1, mean2, arg_filter)
 @pytest.mark.parametrize("arg_filter",
                          [lambda p, k, f: (p, k),
                           lambda p, k, f: (p, (k, f)),
-                          lambda p, k, f: (p, (k, f), (k, f))])
+                          lambda p, k, f: (p, (k, f), (k, f))
+                          ])
 def test_kernel_only_expectations(distribution, kernel, feature, arg_filter):
     params = arg_filter(distribution, kernel, feature)
     _check(params)
@@ -257,9 +260,3 @@ def test_cov_shape_inference(distribution, kernel, feature):
     _check((gauss_tuple, (kernel, feature)))
     if isinstance(distribution, MarkovGaussian):
         _check((gauss_tuple, None, (kernel, feature)))
-
-
-# if __name__ == "__main__":
-    # pytest.main()
-# test_cov_shape_inference(_distrs['gauss_diag'], _kerns['rbf'], features.InducingPoints(Z))
-# test_kernel_only_expectations(_distrs['gauss'], _kerns['lin'], features.InducingPoints(Z), lambda p, k, f: (p, k))