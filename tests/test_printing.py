e9b5da11cbad163a1123eaa7469a07a47e4b4dc7
Sergio Diaz <sergio.diaz@prowler.io>
2019-06-24 19:29:51 +0100

    Test for multiple assign

---
+++ tests/test_printing.py
@@ -0,0 +1,141 @@
+# Copyright 2018 the GPflow authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import numpy as np
+import tensorflow as tf
+
+import gpflow
+from gpflow.utilities.printing import get_component_variables
+
+import pytest
+
+rng = np.random.RandomState(0)
+
+
+class Data:
+    H0 = 5
+    H1 = 2
+    M = 10
+    D = 1
+    Z = rng.rand(M, 1)
+    ls = 2.0
+    var = 1.0
+
+
+# ------------------------------------------
+# Helpers
+# ------------------------------------------
+
+
+class A(tf.Module):
+    def __init__(self, name=None):
+        super().__init__(name)
+        self.var_trainable = tf.Variable(tf.zeros((2, 2, 1)), trainable=True)
+        self.var_fixed = tf.Variable(tf.ones((2, 2, 1)), trainable=False)
+
+
+class B(tf.Module):
+    def __init__(self, name=None):
+        super().__init__(name)
+        self.submodule_list = [A(), A()]
+        self.var_trainable = tf.Variable(tf.zeros((2, 2, 1)), trainable=True)
+        self.var_fixed = tf.Variable(tf.ones((2, 2, 1)), trainable=False)
+
+
+example_tf_module = A()
+example_tf_module_variable_dict = {
+    'A.var_trainable': {'value': np.zeros((2, 2, 1)), 'trainable': True, 'shape': (2, 2, 1)},
+    'A.var_fixed': {'value': np.ones((2, 2, 1)), 'trainable': False, 'shape': (2, 2, 1)},
+}
+
+kernel = gpflow.kernels.RBF(lengthscale=Data.ls, variance=Data.var)
+kernel.lengthscale.trainable = False
+kernel_param_dict = {
+    'RBF.lengthscale': {'value': Data.ls, 'trainable': False, 'shape': ()},
+    'RBF.variance': {'value': Data.var, 'trainable': True, 'shape': ()}
+}
+
+model_gp = gpflow.models.SVGP(
+    kernel=kernel,
+    likelihood=gpflow.likelihoods.Gaussian(),
+    feature=Data.Z,
+    q_diag=True
+)
+model_gp.q_mu.trainable = False
+model_gp_param_dict = {'kernel.lengthscale': kernel_param_dict['RBF.lengthscale'],
+                       'kernel.variance': kernel_param_dict['RBF.variance'],
+                       'likelihood.variance': {'value': 1.0, 'trainable': True, 'shape': ()},
+                       'feature.Z': {'value': Data.Z, 'trainable': True, 'shape': (Data.M, Data.D)},
+                       'SVGP.q_mu': {'value': np.zeros((Data.M, 1)), 'trainable': False,
+                                     'shape': (Data.M, 1)},
+                       'SVGP.q_sqrt': {'value': np.ones((Data.M, 1)), 'trainable': True,
+                                       'shape': (Data.M, 1)}}
+
+example_module_list = B()
+example_module_list_variable_dict = {
+    'A_0.var_trainable': example_tf_module_variable_dict['A.var_trainable'],
+    'A_0.var_fixed': example_tf_module_variable_dict['A.var_fixed'],
+    'A_1.var_trainable': example_tf_module_variable_dict['A.var_trainable'],
+    'A_1.var_fixed': example_tf_module_variable_dict['A.var_fixed'],
+    'B.var_trainable': example_tf_module_variable_dict['A.var_trainable'],
+    'B.var_fixed': example_tf_module_variable_dict['A.var_fixed'],
+}
+
+model_keras = tf.keras.Sequential([
+    tf.keras.layers.Dense(Data.H0, activation='relu', kernel_initializer='ones'),
+    tf.keras.layers.Dense(Data.H1, activation='relu', kernel_initializer='ones', use_bias=False,
+                          trainable=False)
+])
+model_keras.build(input_shape=(Data.M, Data.D))
+model_keras_variable_dict = {
+    'dense.kernel': {'value': np.ones((Data.D, Data.H0)), 'trainable': True,
+                     'shape': (Data.D, Data.H0)},
+    'dense.bias': {'value': np.zeros((Data.H0,)), 'trainable': True, 'shape': (Data.H0,)},
+    'dense_1.kernel': {'value': np.ones((Data.H0, Data.H1)), 'trainable': False,
+                       'shape': (Data.H0, Data.H1)}
+}
+
+
+@pytest.mark.parametrize('module', [A(), kernel, model_gp, B(), model_keras])
+def test_get_component_variables_only_returns_parameters_and_variables(module):
+    for path, variable in get_component_variables(module).items():
+        assert isinstance(variable, tf.Variable) or isinstance(variable, gpflow.Parameter)
+
+
+@pytest.mark.parametrize('module, expected_param_dicts', [
+    (kernel, kernel_param_dict),
+    (model_gp, model_gp_param_dict)
+])
+def test_get_component_variables_registers_variable_properties(module, expected_param_dicts):
+    for path, variable in get_component_variables(module).items():
+        param_name = path.split('.')[-2] + '.' + path.split('.')[-1]
+        assert isinstance(variable, gpflow.Parameter)
+        np.testing.assert_equal(variable.value().numpy(), expected_param_dicts[param_name]['value'])
+        assert variable.trainable == expected_param_dicts[param_name]['trainable']
+        assert variable.shape == expected_param_dicts[param_name]['shape']
+
+
+@pytest.mark.parametrize('module, expected_var_dicts', [
+    (example_tf_module, example_tf_module_variable_dict),
+    (example_module_list, example_module_list_variable_dict),
+    (model_keras, model_keras_variable_dict),
+])
+def test_get_component_variables_registers_param_properties(module, expected_var_dicts):
+    for path, variable in get_component_variables(module).items():
+        var_name = path.split('.')[-2] + '.' + path.split('.')[-1]
+        assert isinstance(variable, tf.Variable)
+        np.testing.assert_equal(variable.numpy(), expected_var_dicts[var_name]['value'])
+        assert variable.trainable == expected_var_dicts[var_name]['trainable']
+        assert variable.shape == expected_var_dicts[var_name]['shape']
---
+++ tests/test_training.py
@@ -0,0 +1,98 @@
+# Copyright 2018 the GPflow authors.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+import numpy as np
+import pytest
+
+import gpflow
+from gpflow.utilities.printing import get_component_variables
+from gpflow.utilities.training import multiple_assign
+
+rng = np.random.RandomState(0)
+
+
+class Data:
+    H0 = 5
+    H1 = 2
+    M = 10
+    D = 1
+    Z = rng.rand(M, 1)
+    ls = 2.0
+    ls_new = 1.5
+    var = 1.0
+    var_new = 10.
+
+
+# ------------------------------------------
+# Helpers
+# ------------------------------------------
+
+
+kernel = gpflow.kernels.RBF(lengthscale=Data.ls, variance=Data.var)
+kernel_param_updates = {'RBF.lengthscale': Data.ls_new}
+
+model = gpflow.models.SVGP(
+    kernel=kernel,
+    likelihood=gpflow.likelihoods.Gaussian(),
+    feature=Data.Z,
+    q_diag=True
+)
+model_param_updates = {
+    'SVGP.kernel.lengthscale': Data.ls_new,
+    'SVGP.likelihood.variance': Data.var_new,
+    'SVGP.feature.Z': np.zeros_like(Data.Z),
+    'SVGP.q_sqrt': 0.5 * np.ones((Data.M, 1))
+}
+model_wrong_path = [
+    {'kernel.lengthscale': Data.ls_new},
+    {'SVGP.Gaussian.variance': Data.var_new},
+    {'feature.Z': np.zeros_like(Data.Z)},
+    {'SVGP.q_std': 0.5 * np.ones((Data.M, 1))}
+]
+
+model_wrong_value = [
+    {'SVGP.likelihood.variance': np.ones((2, 1), dtype=np.int32)},
+    {'SVGP.feature.Z': [1, 2, 3]}
+]
+
+
+@pytest.mark.parametrize('module, var_update_dict', [
+    (kernel, kernel_param_updates),
+    (model, model_param_updates)
+])
+def test_multiple_assign_updates_correct_values(module, var_update_dict):
+    old_value_dict = get_component_variables(module).copy()
+    multiple_assign(module, var_update_dict)
+    for path, variable in get_component_variables(module).items():
+        if path in var_update_dict.keys():
+            np.testing.assert_almost_equal(
+                variable.value().numpy(), var_update_dict[path], decimal=7
+            )
+        else:
+            np.testing.assert_equal(variable.value().numpy(), old_value_dict[path].value().numpy())
+
+
+@pytest.mark.parametrize('module', [model])
+@pytest.mark.parametrize('wrong_var_update_dict', model_wrong_path)
+def test_multiple_assign_fails_with_invalid_path(module, wrong_var_update_dict):
+    with pytest.raises(KeyError):
+        multiple_assign(module, wrong_var_update_dict)
+
+
+@pytest.mark.parametrize('module', [model])
+@pytest.mark.parametrize('wrong_var_update_dict', model_wrong_value)
+def test_multiple_assign_fails_with_invalid_values(module, wrong_var_update_dict):
+    with pytest.raises(ValueError):
+        multiple_assign(module, wrong_var_update_dict)
