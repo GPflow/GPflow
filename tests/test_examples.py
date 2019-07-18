9cc302e8e7ef599e041adf740768e7dd438583a3
Mark van der Wilk <mark@prowler.io>
2019-07-18 11:38:04 +0100

    Tests.

--- tests/test_examples.py
+++ tests/test_examples.py
@@ -12,6 +12,8 @@ def test_train_mnist():
     NB: This script will fail if you test twice within the same minute, due to
     the name where output files are dumped being the same. Not a problem IRL.
     """
-    # ex.run("print_config", config_updates={"iterations": 3, "hz": {"short": 1}})
-    ex.run(None, config_updates={"iterations": 2, "hz": {"short": 1}, "float_type": "float32", "jitter_level": 1e-4,
-                                 "num_inducing_points": 100, "dataset": "mnist-small-subset-for-test"})
+    # TODO: Run this test! Currently this doesn't run on CI, due to a bug in some metadata that TF should be giving
+    #       as part of importing itself. This makes it not work with sacred. A PR has been merged into TF, but this
+    #       hasn't made it into a release yet. https://github.com/tensorflow/tensorflow/issues/30028
+    # ex.run(None, config_updates={"iterations": 2, "hz": {"short": 1}, "float_type": "float32", "jitter_level": 1e-4,
+    #                              "num_inducing_points": 100, "dataset": "mnist-small-subset-for-test"})
