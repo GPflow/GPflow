86bcdffd7da3d01ecb3e7a7fbdabb23d403d4f92
Artem Artemev <art.art.v@gmail.com>
2019-08-22 13:12:48 +0100

    Apply suggestions from code review

--- gpflow/conditionals/mo_conditionals.py
+++ gpflow/conditionals/mo_conditionals.py
@@ -194,7 +194,7 @@ def _conditional(Xnew, inducing_variable, kernel, f, *, full_cov=False, full_out
 def _conditional(Xnew, inducing_variable, kernel, f, *, full_cov=False, full_output_cov=False, q_sqrt=None,
                  white=False):
     """Most efficient routine to project L independent latent gps through a mixing matrix W.
-    The mixing matrix is a member of the `LinearCoregionalisation` and has shape [P, L].
+    The mixing matrix is a member of the `LinearCoregionalization` and has shape [P, L].
     The covariance matrices used to calculate the conditional have the following shape:
     - Kuu: [L, M, M]
     - Kuf: [L, M, N]
