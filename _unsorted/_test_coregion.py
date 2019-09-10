3b5c8c77c46e4397d75544af5372044777170c0b
Sergio Diaz <sergio.diaz@prowler.io>
2019-09-10 14:57:09 +0100

    Preparing Coregion tests for TF2.0 GPflow2.0 !!! Still don't pass (hence in _unsorted_tests folder), waiting for VGP to me merged

--- _unsorted/_test_coregion.py
+++ _unsorted/_test_coregion.py
@@ -11,161 +11,169 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
-import tensorflow as tf
 import numpy as np
 from numpy.testing import assert_allclose
 
 import gpflow
-from gpflow.test_util import GPflowTestCase
+from gpflow.mean_functions import Constant
+
+rng = np.random.RandomState(0)
 
 
-class TestEquivalence(GPflowTestCase):
+class Datum:
+    X = [rng.rand(10, 2) * 10, rng.rand(20, 2) * 10]
+    Y = [np.sin(x) + 0.9 * np.cos(x * 1.6) + rng.randn(*x.shape) * 0.8 for x in X]
+    label = [np.zeros((10, 1)), np.ones((20, 1))]
+    perm = list(range(30))
+    rng.shuffle(perm)
+    Xtest = rng.rand(10, 2) * 10
+    X_augumented = np.hstack([np.concatenate(X), np.concatenate(label)])
+    Y_augumented = np.hstack([np.concatenate(Y), np.concatenate(label)])
+    # For predict tests
+    X_augumented0 = np.hstack([Xtest, np.zeros((Xtest.shape[0], 1))])
+    X_augumented1 = np.hstack([Xtest, np.ones((Xtest.shape[0], 1))])
+    Ytest = [np.sin(x) + 0.9 * np.cos(x * 1.6) for x in Xtest]
+    Y_augumented0 = np.hstack([Ytest, np.zeros((Xtest.shape[0], 1))])
+    Y_augumented1 = np.hstack([Ytest, np.ones((Xtest.shape[0], 1))])
+
+
+def _prepare_models():
     """
-    Here we make sure the coregionalized model with diagonal coregion kernel and
+    Prepare models to make sure the coregionalized model with diagonal coregion kernel and
     with fixed lengthscale is equivalent with normal GP regression.
     """
-    def prepare(self):
-        rng = np.random.RandomState(0)
-        X = [rng.rand(10, 2) * 10, rng.rand(20, 2) * 10]
-        Y = [np.sin(x) + 0.9 * np.cos(x * 1.6) + rng.randn(*x.shape) * 0.8 for x in X]
-        label = [np.zeros((10, 1)), np.ones((20, 1))]
-        perm = list(range(30))
-        rng.shuffle(perm)
-        Xtest = rng.rand(10, 2) * 10
+    # 1. Two independent VGPs for two sets of data
+    k0 = gpflow.kernels.SquaredExponential()
+    k0.lengthscale.trainable = False
+    k1 = gpflow.kernels.SquaredExponential()
+    k1.lengthscale.trainable = False
+    vgp0 = gpflow.models.VGP(
+        Datum.X[0], Datum.Y[0], kernel=k0,
+        mean_function=Constant(),
+        likelihood=gpflow.likelihoods.Gaussian())
+    vgp1 = gpflow.models.VGP(
+        Datum.X[1], Datum.Y[1], kernel=k1,
+        mean_function=Constant(),
+        likelihood=gpflow.likelihoods.Gaussian())
+    # 2. Coregionalized GPR
+    kc = gpflow.kernels.SquaredExponential()
+    kc.trainable = False  # lengthscale and variance is fixed.
+    coreg = gpflow.kernels.Coregion(output_dim=2, rank=1, active_dims=[2])
+    coreg.W.trainable = False
+    lik = gpflow.likelihoods.SwitchedLikelihood([gpflow.likelihoods.Gaussian(),
+                                                 gpflow.likelihoods.Gaussian()]
+                                                )
+    mean_c = gpflow.mean_functions.SwitchedMeanFunction(
+        [gpflow.mean_functions.Constant(), gpflow.mean_functions.Constant()])
+    cvgp = gpflow.models.VGP(Datum.X_augumented, Datum.Y_augumented,
+                             kernel=kc * coreg,
+                             mean_function=mean_c,
+                             likelihood=lik,
+                             num_latent=2
+                             )
 
-        X_augumented = np.hstack([np.concatenate(X), np.concatenate(label)])
-        Y_augumented = np.hstack([np.concatenate(Y), np.concatenate(label)])
+    # Train them for a small number of iterations
 
-        # 1. Two independent VGPs for two sets of data
+    opt = gpflow.optimizers.Scipy()
 
-        k0 = gpflow.kernels.SquaredExponential(2)
-        k0.lengthscale.trainable = False
-        vgp0 = gpflow.models.VGP(
-            X[0], Y[0], kernel=k0,
-            mean_function=gpflow.mean_functions.Constant(),
-            likelihood=gpflow.likelihoods.Gaussian())
-        k1 = gpflow.kernels.SquaredExponential(2)
-        k1.lengthscale.trainable = False
-        vgp1 = gpflow.models.VGP(
-            X[1], Y[1], kernel=k1,
-            mean_function=gpflow.mean_functions.Constant(),
-            likelihood=gpflow.likelihoods.Gaussian())
+    def vgp0_closure():
+        return - vgp0.log_likelihood()
 
-        # 2. Coregionalized GPR
+    def vgp1_closure():
+        return - vgp1.log_likelihood()
 
-        lik = gpflow.likelihoods.SwitchedLikelihood(
-            [gpflow.likelihoods.Gaussian(), gpflow.likelihoods.Gaussian()])
+    def cvgp_closure():
+        return - cvgp.log_likelihood()
 
-        kc = gpflow.kernels.SquaredExponential(2)
-        kc.trainable = False  # lengthscale and variance is fixed.
-        coreg = gpflow.kernels.Coregion(1, output_dim=2, rank=1, active_dims=[2])
-        coreg.W.trainable = False
+    opt.minimize(vgp0_closure, variables=vgp0.trainable_variables, maxiter=50)
+    opt.minimize(vgp1_closure, variables=vgp1.trainable_variables, maxiter=50)
+    opt.minimize(cvgp_closure, variables=cvgp.trainable_variables, maxiter=50)
 
-        mean_c = gpflow.mean_functions.SwitchedMeanFunction(
-            [gpflow.mean_functions.Constant(), gpflow.mean_functions.Constant()])
-        cvgp = gpflow.models.VGP(
-            X_augumented, Y_augumented,
-            kernel=kc * coreg,
-            mean_function=mean_c,
-            likelihood=lik,
-            num_latent=2)
-        return vgp0, vgp1, cvgp, Xtest
-
-    def setup(self):
-        vgp0, vgp1, cvgp, Xtest = self.prepare()
-        opt1 = gpflow.train.ScipyOptimizer()
-        opt2 = gpflow.train.ScipyOptimizer()
-        opt3 = gpflow.train.ScipyOptimizer()
-        opt1.minimize(vgp0, maxiter=50)
-        opt2.minimize(vgp1, maxiter=50)
-        opt3.minimize(cvgp, maxiter=50)
-        self.Xtest = Xtest
-        self.vgp0 = vgp0
-        self.vgp1 = vgp1
-        self.cvgp = cvgp
-
-    def test_likelihood_variance(self):
-        with self.test_context():
-            self.setup()
-            assert_allclose(self.vgp0.likelihood.variance.read_value(),
-                            self.cvgp.likelihood.likelihoods[0].variance.read_value(),
-                            atol=1e-2)
-            assert_allclose(self.vgp1.likelihood.variance.read_value(),
-                            self.cvgp.likelihood.likelihoods[1].variance.read_value(),
-                            atol=1e-2)
-
-    def test_kernel_variance(self):
-        with self.test_context():
-            self.setup()
-            assert_allclose(self.vgp0.kernel.variance.read_value(),
-                            self.cvgp.kernel.kernels[1].kappa.read_value()[0],
-                            atol=1.0e-2)
-            assert_allclose(self.vgp1.kernel.variance.read_value(),
-                            self.cvgp.kernel.kernels[1].kappa.read_value()[1],
-                            atol=1.0e-2)
-
-    def test_mean_values(self):
-        with self.test_context():
-            self.setup()
-            assert_allclose(self.vgp0.mean_function.c.read_value(),
-                            self.cvgp.mean_function.meanfunctions[0].c.read_value(),
-                            atol=1.0e-2)
-            assert_allclose(self.vgp1.mean_function.c.read_value(),
-                            self.cvgp.mean_function.meanfunctions[1].c.read_value(),
-                            atol=1.0e-2)
-
-    def test_predicts(self):
-        with self.test_context():
-            self.setup()
-            X_augumented0 = np.hstack([self.Xtest, np.zeros((self.Xtest.shape[0], 1))])
-            X_augumented1 = np.hstack([self.Xtest, np.ones((self.Xtest.shape[0], 1))])
-            Ytest = [np.sin(x) + 0.9 * np.cos(x*1.6) for x in self.Xtest]
-            Y_augumented0 = np.hstack([Ytest, np.zeros((self.Xtest.shape[0], 1))])
-            Y_augumented1 = np.hstack([Ytest, np.ones((self.Xtest.shape[0], 1))])
-
-            # check predict_f
-            pred_f0 = self.vgp0.predict_f(self.Xtest)
-            pred_fc0 = self.cvgp.predict_f(X_augumented0)
-            assert_allclose(pred_f0, pred_fc0, atol=1.0e-2)
-            pred_f1 = self.vgp1.predict_f(self.Xtest)
-            pred_fc1 = self.cvgp.predict_f(X_augumented1)
-            assert_allclose(pred_f1, pred_fc1, atol=1.0e-2)
-
-            # check predict y
-            pred_y0 = self.vgp0.predict_y(self.Xtest)
-            pred_yc0 = self.cvgp.predict_y(
-                np.hstack([self.Xtest, np.zeros((self.Xtest.shape[0], 1))]))
-
-            # predict_y returns results for all the likelihodds in multi_likelihood
-            assert_allclose(pred_y0[0], pred_yc0[0][:, :np.array(Ytest).shape[1]], atol=1.0e-2)
-            assert_allclose(pred_y0[1], pred_yc0[1][:, :np.array(Ytest).shape[1]], atol=1.0e-2)
-            pred_y1 = self.vgp1.predict_y(self.Xtest)
-            pred_yc1 = self.cvgp.predict_y(
-                np.hstack([self.Xtest, np.ones((self.Xtest.shape[0], 1))]))
-
-            # predict_y returns results for all the likelihodds in multi_likelihood
-            assert_allclose(pred_y1[0], pred_yc1[0][:, np.array(Ytest).shape[1]:], atol=1.0e-2)
-            assert_allclose(pred_y1[1], pred_yc1[1][:, np.array(Ytest).shape[1]:], atol=1.0e-2)
-
-            # check predict_density
-            pred_ydensity0 = self.vgp0.predict_density(self.Xtest, Ytest)
-            pred_ydensity_c0 = self.cvgp.predict_density(X_augumented0, Y_augumented0)
-            self.assertTrue(np.allclose(pred_ydensity0, pred_ydensity_c0, atol=1e-2))
-            pred_ydensity1 = self.vgp1.predict_density(self.Xtest, Ytest)
-            pred_ydensity_c1 = self.cvgp.predict_density(X_augumented1, Y_augumented1)
-            np.testing.assert_allclose(pred_ydensity1, pred_ydensity_c1, atol=1e-2)
-
-            # just check predict_f_samples(self) works
-            self.cvgp.predict_f_samples(X_augumented0, 1)
-            self.cvgp.predict_f_samples(X_augumented1, 1)
-
-            # check predict_f_full_cov
-            self.vgp0.predict_f_full_cov(self.Xtest)
-            self.cvgp.predict_f_full_cov(X_augumented0)
-            self.vgp1.predict_f_full_cov(self.Xtest)
-            self.cvgp.predict_f_full_cov(X_augumented1)
+    return vgp0, vgp1, cvgp
 
 
-if __name__ == '__main__':
-    tf.test.main()
+# ------------------------------------------
+# Tests
+# ------------------------------------------
+
+def test_likelihood_variance():
+    vgp0, vgp1, cvgp = _prepare_models()
+    assert_allclose(vgp0.likelihood.variance.read_value(),
+                    cvgp.likelihood.likelihoods[0].variance.read_value(),
+                    atol=1e-2)
+    assert_allclose(vgp1.likelihood.variance.read_value(),
+                    cvgp.likelihood.likelihoods[1].variance.read_value(),
+                    atol=1e-2)
+
+
+def test_kernel_variance():
+    vgp0, vgp1, cvgp = _prepare_models()
+    assert_allclose(vgp0.kernel.variance.read_value(),
+                    cvgp.kernel.kernels[1].kappa.read_value()[0],
+                    atol=1.0e-2)
+    assert_allclose(vgp1.kernel.variance.read_value(),
+                    cvgp.kernel.kernels[1].kappa.read_value()[1],
+                    atol=1.0e-2)
+
+
+def test_mean_values():
+    vgp0, vgp1, cvgp = _prepare_models()
+    assert_allclose(vgp0.mean_function.c.read_value(),
+                    cvgp.mean_function.meanfunctions[0].c.read_value(),
+                    atol=1.0e-2)
+    assert_allclose(vgp1.mean_function.c.read_value(),
+                    cvgp.mean_function.meanfunctions[1].c.read_value(),
+                    atol=1.0e-2)
+
+
+def test_predict_f():
+    vgp0, vgp1, cvgp = _prepare_models()
+
+    pred_f0 = vgp0.predict_f(Datum.Xtest)
+    pred_fc0 = cvgp.predict_f(Datum.X_augumented0)
+    assert_allclose(pred_f0, pred_fc0, atol=1.0e-2)
+    pred_f1 = vgp1.predict_f(Datum.Xtest)
+    pred_fc1 = cvgp.predict_f(Datum.X_augumented1)
+    assert_allclose(pred_f1, pred_fc1, atol=1.0e-2)
+
+    # check predict_f_full_cov
+    vgp0.predict_f_full_cov(Datum.Xtest)
+    cvgp.predict_f_full_cov(Datum.X_augumented0)
+    vgp1.predict_f_full_cov(Datum.Xtest)
+    cvgp.predict_f_full_cov(Datum.X_augumented1)
+
+
+def test_predict_y():
+    vgp0, vgp1, cvgp = _prepare_models()
+    pred_y0 = vgp0.predict_y(Datum.Xtest)
+    pred_yc0 = cvgp.predict_y(
+        np.hstack([Datum.Xtest, np.zeros((Datum.Xtest.shape[0], 1))]))
+
+    # predict_y returns results for all the likelihodds in multi_likelihood
+    assert_allclose(pred_y0[0], pred_yc0[0][:, :np.array(Datum.Ytest).shape[1]], atol=1.0e-2)
+    assert_allclose(pred_y0[1], pred_yc0[1][:, :np.array(Datum.Ytest).shape[1]], atol=1.0e-2)
+    pred_y1 = vgp1.predict_y(Datum.Xtest)
+    pred_yc1 = cvgp.predict_y(
+        np.hstack([Datum.Xtest, np.ones((Datum.Xtest.shape[0], 1))]))
+
+    # predict_y returns results for all the likelihodds in multi_likelihood
+    assert_allclose(pred_y1[0], pred_yc1[0][:, np.array(Datum.Ytest).shape[1]:], atol=1.0e-2)
+    assert_allclose(pred_y1[1], pred_yc1[1][:, np.array(Datum.Ytest).shape[1]:], atol=1.0e-2)
+
+
+def test_predict_log_density():
+    vgp0, vgp1, cvgp = _prepare_models()
+
+    pred_ydensity0 = vgp0.predict_log_density((Datum.Xtest, Datum.Ytest))
+    pred_ydensity_c0 = cvgp.predict_log_density((Datum.X_augumented0, Datum.Y_augumented0))
+    assert_allclose(pred_ydensity0, pred_ydensity_c0, atol=1e-2)
+    pred_ydensity1 = vgp1.predict_log_density((Datum.Xtest, Datum.Ytest))
+    pred_ydensity_c1 = cvgp.predict_log_density((Datum.X_augumented1, Datum.Y_augumented1))
+    assert_allclose(pred_ydensity1, pred_ydensity_c1, atol=1e-2)
+
+
+def test_predict_f_samples():
+    vgp0, vgp1, cvgp = _prepare_models()
+    # just check predict_f_samples(self) works
+    cvgp.predict_f_samples(Datum.X_augumented0, 1)
+    cvgp.predict_f_samples(Datum.X_augumented1, 1)
