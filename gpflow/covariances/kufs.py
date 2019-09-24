b8184f77d313e78e0c6384d5cd389a9d305270eb
ST John <st@prowler.io>
2019-09-24 14:55:32 +0100

    move TensorLike into base and fallbacks into covariances/

--- gpflow/covariances/kufs.py
+++ gpflow/covariances/kufs.py
@@ -1,16 +1,26 @@
 import tensorflow as tf
-from ..inducing_variables import InducingPoints, Multiscale
+from ..base import TensorLike
+from ..inducing_variables import InducingVariables, InducingPoints, Multiscale
 from ..kernels import Kernel, SquaredExponential
 from .dispatch import Kuf
 
 
-@Kuf.register(InducingPoints, Kernel, object)
-def _Kuf(inducing_variable: InducingPoints, kernel: Kernel, Xnew: tf.Tensor):
+@Kuf.register(InducingVariables, Kernel, TensorLike)
+def Kuf_fallback(inducing_variable, kernel, X):
+    from warnings import warn
+    warn('Kuf(inducing_variable, kernel, X) is deprecated, please use '
+         'Kuf(kernel, inducing_variable, X) instead.',
+         DeprecationWarning)
+    return Kuf(kernel, inducing_variable, X)
+
+
+@Kuf.register(InducingPoints, Kernel, TensorLike)
+def Kuf_kernel_inducingpoints(inducing_variable: InducingPoints, kernel: Kernel, Xnew):
     return kernel(inducing_variable.Z, Xnew)
 
 
-@Kuf.register(Multiscale, SquaredExponential, object)
-def _Kuf(inducing_variable: Multiscale, kernel: SquaredExponential, Xnew):
+@Kuf.register(Multiscale, SquaredExponential, TensorLike)
+def Kuf_sqexp_multiscale(inducing_variable: Multiscale, kernel: SquaredExponential, Xnew):
     Xnew, _ = kernel.slice(Xnew, None)
     Zmu, Zlen = kernel.slice(inducing_variable.Z, inducing_variable.scales)
     idlengthscale = kernel.lengthscale + Zlen
--- gpflow/covariances/kuus.py
+++ gpflow/covariances/kuus.py
@@ -1,19 +1,28 @@
 import tensorflow as tf
 
-from ..inducing_variables import InducingPoints, Multiscale
+from ..inducing_variables import InducingVariables, InducingPoints, Multiscale
 from ..kernels import Kernel, SquaredExponential
 from .dispatch import Kuu
 
 
+@Kuu.register(InducingVariables, Kernel)
+def Kuu_fallback(inducing_variable, kernel, **kw):
+    from warnings import warn
+    warn('Kuu(inducing_variable, kernel) is deprecated, please use '
+         'Kuu(kernel, inducing_variable) instead.',
+         DeprecationWarning)
+    return Kuu(kernel, inducing_variable, **kw)
+
+
 @Kuu.register(InducingPoints, Kernel)
-def _Kuu(inducing_variable: InducingPoints, kernel: Kernel, *, jitter=0.0):
+def Kuu_kernel_inducingpoints(inducing_variable: InducingPoints, kernel: Kernel, *, jitter=0.0):
     Kzz = kernel(inducing_variable.Z)
     Kzz += jitter * tf.eye(len(inducing_variable), dtype=Kzz.dtype)
     return Kzz
 
 
 @Kuu.register(Multiscale, SquaredExponential)
-def _Kuu(inducing_variable: Multiscale, kernel: SquaredExponential, *, jitter=0.0):
+def Kuu_sqexp_multiscale(inducing_variable: Multiscale, kernel: SquaredExponential, *, jitter=0.0):
     Zmu, Zlen = kernel.slice(inducing_variable.Z, inducing_variable.scales)
     idlengthscale2 = tf.square(kernel.lengthscale + Zlen)
     sc = tf.sqrt(idlengthscale2[None, ...] + idlengthscale2[:, None, ...] -
