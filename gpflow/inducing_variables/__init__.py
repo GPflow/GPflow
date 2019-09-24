283bc7989cd96f6be83e366b2d8a077c717cb93a
Mark van der Wilk <mv310@cam.ac.uk>
2019-09-24 14:43:53 +0100

    Rename.

--- gpflow/inducing_variables/__init__.py
+++ gpflow/inducing_variables/__init__.py
@@ -1,4 +1,4 @@
 from .inducing_variables import InducingVariables, InducingPoints, Multiscale
-from .convolutional_patch import InducingPatches
+from .inducing_patch import InducingPatches
 from .mo_inducing_variables import (MultioutputInducingVariables, FallbackSharedIndependentInducingVariables, FallbackSeparateIndependentInducingVariables,
                                     SharedIndependentInducingVariables, SeparateIndependentInducingVariables)
