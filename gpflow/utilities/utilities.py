b6c3a78c988c8efcba7274519a1d00a1625a08e8
ST John <st@prowler.io>
2019-09-25 13:51:41 +0100

    remove default for flag argument to set_trainable

--- gpflow/utilities/utilities.py
+++ gpflow/utilities/utilities.py
@@ -18,7 +18,7 @@ __all__ = [
 ]
 
 
-def set_trainable(model: tf.Module, flag: bool = True):
+def set_trainable(model: tf.Module, flag: bool):
     """
     Set trainable flag for all `tf.Variable`s and `gpflow.Parameter`s in a module.
     """
