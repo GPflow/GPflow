a1e588bcf559eb277ee2af12e47f2fce24bb10b5
ST John <st@prowler.io>
2019-10-01 17:34:22 +0100

    Move ci_utils into gpflow package so it is easily importable in notebooks.

--- gpflow/ci_utils.py
+++ gpflow/ci_utils.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Artem Artemev @awav
+# Copyright 2017-2019 GPflow
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
