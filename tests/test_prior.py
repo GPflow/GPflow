ad3cdb3f21fea0af9ef327ab0c21b5637880fc61
ST John <st@prowler.io>
2019-10-14 22:27:34 +0100

    additional test to check that transform is added correctly in MAP case

--- tests/test_prior.py
+++ tests/test_prior.py
@@ -1,6 +1,7 @@
 import gpflow
 import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 import pytest
 
 
@@ -41,3 +42,36 @@ def test_log_prior_with_no_prior():
     """
     param = gpflow.Parameter(5.3, transform=gpflow.positive())
     assert param.log_prior().numpy() == 0.0
+
+
+
+def fix_dtype(x):
+    # TODO replace with generic function for handling tensorflow_probability ...
+    return tf.cast(x, gpflow.default_float())
+
+class DummyModel(gpflow.models.BayesianModel):
+    value = 3.3
+    log_scale = 0.4
+
+    def __init__(self, with_transform):
+        super().__init__()
+
+        prior = tfp.distributions.Normal(fix_dtype(1.0), fix_dtype(1.0))
+
+        scale = np.exp(self.log_scale)
+        if with_transform:
+            transform = tfp.bijectors.AffineScalar(scale=fix_dtype(scale))
+        else:
+            transform = None
+
+        self.theta = gpflow.Parameter(self.value, prior=prior, transform=transform)
+
+    def log_likelihood(self):
+        return (self.theta + 5) ** 2
+
+def test_map_contains_log_det_jacobian():
+    m1 = DummyModel(with_transform=True)
+    m2 = DummyModel(with_transform=False)
+    assert np.allclose(- m1.neg_log_marginal_likelihood().numpy(),
+                       - m2.neg_log_marginal_likelihood().numpy() + m1.log_scale), \
+            "MAP objective should differ by log|Jacobian| of the transform"
