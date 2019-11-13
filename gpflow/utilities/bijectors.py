98ab6e1c81a8259a807c0531e44f624e77cc86ce
joehall87 <joehall87@gmail.com>
2019-11-13 22:45:05 +0000

    First draft of introducing a different base positive transform

--- gpflow/utilities/bijectors.py
+++ gpflow/utilities/bijectors.py
@@ -5,17 +5,41 @@ import tensorflow_probability as tfp
 from .. import config
 from .utilities import to_default_float
 
+
 __all__ = ["positive", "triangular"]
 
 
-def positive(lower: Optional[float] = None):
-    lower_value = config.default_positive_minimum()
-    if lower_value is None:
-        return tfp.bijectors.Softplus()
+_POSITIVE_BIJECTOR_MAP = {
+    "exp": tfp.bijectors.Exp,
+    "softplus": tfp.bijectors.Softplus,
+}
 
-    shift = to_default_float(lower_value)
-    bijectors = [tfp.bijectors.AffineScalar(shift=shift), tfp.bijectors.Softplus()]
-    return tfp.bijectors.Chain(bijectors)
+
+def positive(lower: Optional[float] = None,
+             bijector: Optional[str] = None) -> tfp.bijectors.Bijector:
+    """
+    Returns a positive bijector (a reversible transformation from real to positive numbers).
+
+    :param lower: lower bound override (defaults to config.default_positive_minimum())
+    :param bijector: bijector method override (defaults to config.default_positive_bijector())
+    :returns: a bijector instance
+    """
+    if lower is None:
+        lower = config.default_positive_minimum()
+    if lower is None:
+        return _get_base_positive_bijector(bijector)
+
+    shift = to_default_float(lower)
+    return tfp.bijectors.Chain([
+        tfp.bijectors.AffineScalar(shift=shift),
+        _get_base_positive_bijector(bijector),
+    ])
+
+
+def _get_base_positive_bijector(bijector_name: Optional[str] = None):
+    if bijector_name is None:
+        bijector_name = config.default_positive_bijector()
+    return _POSITIVE_BIJECTOR_MAP[bijector_name]()
 
 
 triangular = tfp.bijectors.FillTriangular
