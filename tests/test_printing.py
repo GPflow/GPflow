b8ed8f6504dc363c5064dc2ce6012c39ad297565
joehall87 <joehall87@gmail.com>
2019-11-14 19:55:47 +0000

    Keep original printing tests, but add in explicit test for non-null positive min

--- tests/test_printing.py
+++ tests/test_printing.py
@@ -17,6 +17,7 @@ import pytest
 import tensorflow as tf
 
 import gpflow
+from gpflow.config import Config, as_context
 from gpflow.utilities.utilities import leaf_components, _merge_leaf_components, tabulate_module_summary
 
 rng = np.random.RandomState(0)
@@ -186,32 +187,38 @@ example_dag_module_param_dict = {
 }
 
 compose_kernel_param_print_string = """\
-name                                       class      transform         trainable    shape    dtype      value\n\
------------------------------------------  ---------  ----------------  -----------  -------  -------  -------\n\
-Product.kernels[0].kernels[0].variance     Parameter  Softplus + Shift  True         ()       float64        1\n\
-Product.kernels[0].kernels[0].lengthscale  Parameter  Softplus + Shift  False        ()       float64        2\n\
-Product.kernels[0].kernels[1].variance     Parameter  Softplus + Shift  True         ()       float64        1\n\
-Product.kernels[0].kernels[1].lengthscale  Parameter  Softplus + Shift  False        ()       float64        2\n\
-Product.kernels[1].kernels[0].variance     Parameter  Softplus + Shift  True         ()       float64        1\n\
-Product.kernels[1].kernels[0].lengthscale  Parameter  Softplus + Shift  False        ()       float64        2\n\
-Product.kernels[1].kernels[1].variance     Parameter  Softplus + Shift  True         ()       float64        1\n\
-Product.kernels[1].kernels[1].lengthscale  Parameter  Softplus + Shift  False        ()       float64        2"""
+name                                       class      transform    trainable    shape    dtype      value\n\
+-----------------------------------------  ---------  -----------  -----------  -------  -------  -------\n\
+Product.kernels[0].kernels[0].variance     Parameter  Softplus     True         ()       float64        1\n\
+Product.kernels[0].kernels[0].lengthscale  Parameter  Softplus     False        ()       float64        2\n\
+Product.kernels[0].kernels[1].variance     Parameter  Softplus     True         ()       float64        1\n\
+Product.kernels[0].kernels[1].lengthscale  Parameter  Softplus     False        ()       float64        2\n\
+Product.kernels[1].kernels[0].variance     Parameter  Softplus     True         ()       float64        1\n\
+Product.kernels[1].kernels[0].lengthscale  Parameter  Softplus     False        ()       float64        2\n\
+Product.kernels[1].kernels[1].variance     Parameter  Softplus     True         ()       float64        1\n\
+Product.kernels[1].kernels[1].lengthscale  Parameter  Softplus     False        ()       float64        2"""
 
 kernel_param_print_string = """\
+name                            class      transform    trainable    shape    dtype      value\n\
+------------------------------  ---------  -----------  -----------  -------  -------  -------\n\
+SquaredExponential.variance     Parameter  Softplus     True         ()       float64        1\n\
+SquaredExponential.lengthscale  Parameter  Softplus     False        ()       float64        2"""
+
+kernel_param_print_string_with_shift = """\
 name                            class      transform         trainable    shape    dtype      value\n\
 ------------------------------  ---------  ----------------  -----------  -------  -------  -------\n\
 SquaredExponential.variance     Parameter  Softplus + Shift  True         ()       float64        1\n\
 SquaredExponential.lengthscale  Parameter  Softplus + Shift  False        ()       float64        2"""
 
 model_gp_param_print_string = """\
-name                      class      transform         trainable    shape    dtype    value\n\
-------------------------  ---------  ----------------  -----------  -------  -------  --------\n\
-SVGP.kernel.variance      Parameter  Softplus + Shift  True         ()       float64  1.0\n\
-SVGP.kernel.lengthscale   Parameter  Softplus + Shift  False        ()       float64  2.0\n\
-SVGP.likelihood.variance  Parameter  Softplus + Shift  True         ()       float64  1.0\n\
-SVGP.inducing_variable.Z  Parameter                    True         (10, 1)  float64  [[0.5...\n\
-SVGP.q_mu                 Parameter                    False        (10, 1)  float64  [[0....\n\
-SVGP.q_sqrt               Parameter  Softplus + Shift  True         (10, 1)  float64  [[1...."""
+name                      class      transform    trainable    shape    dtype    value\n\
+------------------------  ---------  -----------  -----------  -------  -------  --------\n\
+SVGP.kernel.variance      Parameter  Softplus     True         ()       float64  1.0\n\
+SVGP.kernel.lengthscale   Parameter  Softplus     False        ()       float64  2.0\n\
+SVGP.likelihood.variance  Parameter  Softplus     True         ()       float64  1.0\n\
+SVGP.inducing_variable.Z  Parameter               True         (10, 1)  float64  [[0.5...\n\
+SVGP.q_mu                 Parameter               False        (10, 1)  float64  [[0....\n\
+SVGP.q_sqrt               Parameter  Softplus     True         (10, 1)  float64  [[1...."""
 
 example_tf_module_variable_print_string = """\
 name             class             transform    trainable    shape      dtype    value\n\
@@ -333,7 +340,13 @@ def test_merge_leaf_components_merges_keys_with_same_values(dag_module, expected
     (B, example_module_list_variable_print_string),
 ])
 def test_print_summary_output_string(module_callable, expected_param_print_string):
-    assert tabulate_module_summary(module_callable()) == expected_param_print_string
+    with as_context(Config(positive_minimum=None)):
+        assert tabulate_module_summary(module_callable()) == expected_param_print_string
+
+
+def test_print_summary_output_string_with_positive_minimum():
+    with as_context(Config(positive_minimum=1e-6)):
+        assert tabulate_module_summary(create_kernel()) == kernel_param_print_string_with_shift
 
 
 def test_print_summary_for_keras_model():
