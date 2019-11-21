9cdb2a20cb725f21589beda5fd45b1893b262fe4
Joe Hall <joehall87@gmail.com>
2019-11-21 14:33:38 +0000

    Add config option for positive bijector + re-introduce positive lower bound (#1148)

--- gpflow/config/__config__.py
+++ gpflow/config/__config__.py
@@ -2,17 +2,25 @@ import contextlib
 import enum
 import os
 from dataclasses import dataclass, field, replace
-from typing import Optional
+from typing import Dict, Optional, Union
 
 import numpy as np
 import tabulate
 import tensorflow as tf
+import tensorflow_probability as tfp
+
 
 __all__ = [
-    "Config", "default_summary_fmt", "default_float", "default_jitter", "default_int", "set_default_summary_fmt",
-    "set_default_float", "set_default_jitter", "set_default_int", "as_context", "config", "set_config",
-    "default_positive_minimum", "set_default_positive_minimum"
+    "Config", "as_context", "config", "set_config",
+    "default_float", "set_default_float",
+    "default_int", "set_default_int",
+    "default_jitter", "set_default_jitter",
+    "default_positive_bijector", "set_default_positive_bijector",
+    "default_positive_minimum", "set_default_positive_minimum",
+    "default_summary_fmt", "set_default_summary_fmt",
+    "positive_bijector_type_map"
 ]
+
 
 __config = None
 
@@ -23,6 +31,7 @@ class _Values(enum.Enum):
     will be `GPFLOW_SUMMARY_FMT`."""
     INT = np.int32
     FLOAT = np.float64
+    POSITIVE_BIJECTOR = "softplus"
     POSITIVE_MINIMUM = None
     SUMMARY_FMT = None
     JITTER = 1e-6
@@ -33,7 +42,7 @@ class _Values(enum.Enum):
 
 
 def default(value: _Values):
-    """Checks if """
+    """Checks if value is set in the environment."""
     return os.getenv(value.name, default=value.value)
 
 
@@ -47,6 +56,8 @@ class Config:
         float: Float data type, float32 or float64
         jitter: Jitter value. Mainly used for for making badly conditioned matrices more stable.
             Default value is `1e-6`.
+        positive_bijector: Method for positive bijector, either "softplus" or "exp".
+            Default is "softplus".
         positive_minimum: Lower level for the positive transformation.
         summary_fmt: Summary format for module printing.
     """
@@ -54,6 +65,7 @@ class Config:
     int: type = field(default_factory=lambda: default(_Values.INT))
     float: type = field(default_factory=lambda: default(_Values.FLOAT))
     jitter: float = field(default_factory=lambda: default(_Values.JITTER))
+    positive_bijector: str = field(default_factory=lambda: default(_Values.POSITIVE_BIJECTOR))
     positive_minimum: float = field(default_factory=lambda: default(_Values.POSITIVE_MINIMUM))
     summary_fmt: str = field(default_factory=lambda: default(_Values.SUMMARY_FMT))
 
@@ -61,10 +73,6 @@ class Config:
 def config() -> Config:
     """Returns current active config."""
     return __config
-
-
-def default_summary_fmt():
-    return config().summary_fmt
 
 
 def default_int():
@@ -79,8 +87,16 @@ def default_jitter():
     return config().jitter
 
 
+def default_positive_bijector():
+    return config().positive_bijector
+
+
 def default_positive_minimum():
     return config().positive_minimum
+
+
+def default_summary_fmt():
+    return config().summary_fmt
 
 
 def set_config(new_config: Config):
@@ -124,12 +140,14 @@ def set_default_jitter(value: float):
     set_config(replace(config(), jitter=value))
 
 
-def set_default_summary_fmt(value: str):
-    formats = tabulate.tabulate_formats + ['notebook', None]
-    if value not in formats:
-        raise ValueError(f"Summary does not support '{value}' format")
+def set_default_positive_bijector(value: str):
+    type_map = positive_bijector_type_map()
+    if isinstance(value, str):
+        value = value.lower()
+    if value not in type_map:
+        raise ValueError(f"`{value}` not in set of valid bijectors: {sorted(type_map)}")
 
-    set_config(replace(config(), summary_fmt=value))
+    set_config(replace(config(), positive_bijector=value))
 
 
 def set_default_positive_minimum(value: float):
@@ -143,10 +161,24 @@ def set_default_positive_minimum(value: float):
     set_config(replace(config(), positive_minimum=value))
 
 
+def set_default_summary_fmt(value: str):
+    formats = tabulate.tabulate_formats + ['notebook', None]
+    if value not in formats:
+        raise ValueError(f"Summary does not support '{value}' format")
+
+    set_config(replace(config(), summary_fmt=value))
+
+
+def positive_bijector_type_map() -> Dict[str, type]:
+    return {
+        "exp": tfp.bijectors.Exp,
+        "softplus": tfp.bijectors.Softplus,
+    }
+
+
 @contextlib.contextmanager
 def as_context(temporary_config: Optional[Config] = None):
-    '''Ensure that global configs defaults, with a context manager. Useful
-    for testing.'''
+    """Ensure that global configs defaults, with a context manager. Useful for testing."""
     current_config = config()
     temporary_config = replace(current_config) if temporary_config is None else temporary_config
     try:
--- gpflow/utilities/bijectors.py
+++ gpflow/utilities/bijectors.py
@@ -5,17 +5,33 @@ import tensorflow_probability as tfp
 from .. import config
 from .utilities import to_default_float
 
+
 __all__ = ["positive", "triangular"]
 
 
-def positive(lower: Optional[float] = None):
-    lower_value = config.default_positive_minimum()
-    if lower_value is None:
-        return tfp.bijectors.Softplus()
+def positive(lower: Optional[float] = None, base: Optional[str] = None) -> tfp.bijectors.Bijector:
+    """
+    Returns a positive bijector (a reversible transformation from real to positive numbers).
 
-    shift = to_default_float(lower_value)
-    bijectors = [tfp.bijectors.AffineScalar(shift=shift), tfp.bijectors.Softplus()]
-    return tfp.bijectors.Chain(bijectors)
+    :param lower: lower bound override (defaults to config.default_positive_minimum())
+    :param base: overrides base positive bijector (defaults to config.default_positive_bijector())
+    :returns: a bijector instance
+    """
+    if isinstance(base, str):
+        base = base.lower()
+    bijector = base if base is not None else config.default_positive_bijector()
+    bijector = config.positive_bijector_type_map()[bijector]()
+    if lower is None:
+        lower = config.default_positive_minimum()
+    if lower is not None:
+        # Chain applies transformations in reverse order, so shift will be applied last
+        shift = tfp.bijectors.AffineScalar(shift=to_default_float(lower))
+        bijector = tfp.bijectors.Chain([shift, bijector])
+    return bijector
 
 
-triangular = tfp.bijectors.FillTriangular
+def triangular() -> tfp.bijectors.Bijector:
+    """
+    Returns instance of a triangular bijector.
+    """
+    return tfp.bijectors.FillTriangular()
