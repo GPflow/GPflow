8b17f5606ae53f416168ad94bfeb0d06d26fdfc1
Vincent Dutordoir <dutordoirv@gmail.com>
2020-02-19 10:04:11 +0000

    Add docstring to get_first_occurrence

--- gpflow/utilities/multipledispatch.py
+++ gpflow/utilities/multipledispatch.py
@@ -26,7 +26,15 @@ class Dispatcher(GeneratorDispatcher):
         return self.get_first_occurrence(*types)
 
     def get_first_occurrence(self, *types):
-        """ Returns the first occurrence of a matching function """
+        """ 
+        Returns the first occurrence of a matching function 
+        
+        Based on `multipledispatch.Dispatcher.dispatch_iter`, which
+        returns an iterator of matching functions. This method uses
+        the same logic to select functions, but simply returns the first
+        element of the iterator. If no matching functions are found, 
+        `None` is returned.
+        """
         n = len(types)
         for signature in self.ordering:
             if len(signature) == n and all(map(issubclass, types, signature)):
@@ -36,5 +44,4 @@ class Dispatcher(GeneratorDispatcher):
                 if variadic_signature_matches(types, signature):
                     result = self.funcs[signature]
                     return result
-
         return None
