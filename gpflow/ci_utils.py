c603f5212ddf125e052833be4d5aacd47c077b3b
vdutor <dutordoirv@gmail.com>
2020-02-24 11:15:15 +0000

    Update circleci config

--- gpflow/ci_utils.py
+++ gpflow/ci_utils.py
@@ -18,7 +18,14 @@ import os
 
 
 def is_continuous_integration():
-    return os.environ.get('CI', None) is not None
+    """
+    If the environment variable `DOCS` is set, always returns False.
+    Otherwise, returns `True` if notebooks are executed on CICD, else `False`.
+    """
+    if 'DOCS' in os.environ:
+        return False
+
+    return 'CI' in os.environ
 
 
 def ci_niter(n: int, test_n: int = 2):
@@ -31,3 +38,4 @@ def ci_range(n: int, test_n: int = 2):
 
 def ci_list(lst: list, test_n=2):
     return lst[:test_n] if is_continuous_integration() else lst
+
