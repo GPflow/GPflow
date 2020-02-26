57d8ab787dbb1b7b555279d38641d8b4db5dda0e
st-- <st--@users.noreply.github.com>
2020-02-26 14:09:18 +0000

    improve organization of test modules (#1288)
    Historically, there was just a single flat directory full of test modules. This had several negative implications: a) some test files ended up very long, b) it was occasionally hard to find a good place for new tests, c) no separation between unit and integration tests.
    
    This PR gives the tests a directory structure that mirrors that of the gpflow package. Where possible, the code in gpflow/submodule/file.py is tested in tests/gpflow/submodule/test_file.py. Integration tests (e.g. notebooks, method equivalence) are now in tests/integration/.
    
    Most of this PR is simple file moves, plus some splitting up of tests (particularly for models) according to the new structure. The reference kernel implementations are now unified in tests/gpflow/kernels/reference.py.

--- tests/gpflow/models/test_svgp.py
+++ tests/gpflow/models/test_svgp.py
@@ -1,4 +1,4 @@
-# Copyright 2016 the GPflow authors.
+# Copyright 2016-2020 the GPflow authors.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,8 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from dataclasses import dataclass
 
+from dataclasses import dataclass
 import numpy as np
 import pytest
 import tensorflow as tf
@@ -20,22 +20,6 @@ from numpy.testing import assert_allclose, assert_array_equal, assert_array_less
 
 import gpflow
 from gpflow.config import default_float
-
-
-# ------------------------------------------
-# Data classes: storing constants
-# ------------------------------------------
-
-
-@dataclass(frozen=True)
-class Datum:
-    rng: np.random.RandomState = np.random.RandomState(0)
-    X: np.ndarray = rng.randn(100, 2)
-    Y: np.ndarray = rng.randn(100, 1)
-    Z: np.ndarray = rng.randn(10, 2)
-    Xs: np.ndarray = rng.randn(10, 2)
-    lik = gpflow.likelihoods.Gaussian()
-    kernel = gpflow.kernels.Matern32()
 
 
 @dataclass(frozen=True)
@@ -50,79 +34,23 @@ class DatumSVGP:
     data = (X, Y)
 
 
-default_datum = Datum()
 default_datum_svgp = DatumSVGP()
 
 
-def _check_models_close(m1, m2, tolerance=1e-2):
-    m1_params = {p.name: p for p in list(m1.trainable_parameters)}
-    m2_params = {p.name: p for p in list(m2.trainable_parameters)}
-    if set(m2_params.keys()) != set(m2_params.keys()):
-        return False
-    for key in m1_params:
-        p1 = m1_params[key]
-        p2 = m2_params[key]
-        if not np.allclose(p1.read_value(), p2.read_value(), rtol=tolerance, atol=tolerance):
-            return False
-    return True
-
-
-_gp_models = [
-    gpflow.models.VGP((default_datum.X, default_datum.Y), default_datum.kernel, default_datum.lik),
-    gpflow.models.GPMC((default_datum.X, default_datum.Y), default_datum.kernel, default_datum.lik),
-    gpflow.models.SGPMC((default_datum.X, default_datum.Y),
-                        default_datum.kernel,
-                        default_datum.lik,
-                        inducing_variable=default_datum.Z),
-    gpflow.models.SGPR((default_datum.X, default_datum.Y), default_datum.kernel, inducing_variable=default_datum.Z),
-    gpflow.models.GPR((default_datum.X, default_datum.Y), default_datum.kernel),
-    gpflow.models.GPRFITC((default_datum.X, default_datum.Y), default_datum.kernel, inducing_variable=default_datum.Z)
-]
-
-_state_less_gp_models = [gpflow.models.SVGP(default_datum.kernel, default_datum.lik, inducing_variable=default_datum.Z)]
-
-
-@pytest.mark.parametrize('model', _state_less_gp_models + _gp_models)
-def test_methods_predict_f(model):
-    mf, vf = model.predict_f(default_datum.Xs)
-    assert_array_equal(mf.shape, vf.shape)
-    assert_array_equal(mf.shape, (10, 1))
-    assert_array_less(np.full_like(vf, -1e-6), vf)
-
-
-@pytest.mark.parametrize('model', _state_less_gp_models + _gp_models)
-def test_methods_predict_y(model):
-    mf, vf = model.predict_y(default_datum.Xs)
-    assert_array_equal(mf.shape, vf.shape)
-    assert_array_equal(mf.shape, (10, 1))
-    assert_array_less(np.full_like(vf, -1e-6), vf)
-
-
-@pytest.mark.parametrize('model', _state_less_gp_models + _gp_models)
-def test_methods_predict_log_density(model):
-    rng = Datum().rng
-    Ys = rng.randn(10, 1)
-    d = model.predict_log_density((default_datum.Xs, Ys))
-    assert_array_equal(d.shape, (10, 1))
-
-
-def test_sgpr_qu():
-    rng = Datum().rng
-    X, Z = tf.cast(rng.randn(100, 2), default_float()), tf.cast(rng.randn(20, 2), default_float())
-    Y = tf.cast(np.sin(X @ np.array([[-1.4], [0.5]])) + 0.5 * np.random.randn(len(X), 1), default_float())
-    model = gpflow.models.SGPR((X, Y), kernel=gpflow.kernels.SquaredExponential(), inducing_variable=Z)
-
-    @tf.function
-    def closure():
-        return - model.log_marginal_likelihood()
-
-    gpflow.optimizers.Scipy().minimize(closure, variables=model.trainable_variables)
-
-    qu_mean, qu_cov = model.compute_qu()
-    f_at_Z_mean, f_at_Z_cov = model.predict_f(model.inducing_variable.Z, full_cov=True)
-
-    np.testing.assert_allclose(qu_mean, f_at_Z_mean, rtol=1e-5, atol=1e-5)
-    np.testing.assert_allclose(tf.reshape(qu_cov, (1, 20, 20)), f_at_Z_cov, rtol=1e-5, atol=1e-5)
+def test_svgp_fixing_q_sqrt():
+    """
+    In response to bug #46, we need to make sure that the q_sqrt matrix can be fixed
+    """
+    num_latent = default_datum_svgp.Y.shape[1]
+    model = gpflow.models.SVGP(kernel=gpflow.kernels.SquaredExponential(),
+                               likelihood=default_datum_svgp.lik,
+                               q_diag=True,
+                               num_latent=num_latent,
+                               inducing_variable=default_datum_svgp.Z,
+                               whiten=False)
+    default_num_trainable_variables = len(model.trainable_variables)
+    model.q_sqrt.trainable = False
+    assert len(model.trainable_variables) == default_num_trainable_variables - 1
 
 
 def test_svgp_white():
@@ -175,20 +103,17 @@ def test_svgp_non_white():
     assert_allclose(model_1.log_likelihood(default_datum_svgp.data), model_2.log_likelihood(default_datum_svgp.data))
 
 
-def test_svgp_fixing_q_sqrt():
-    """
-    In response to bug #46, we need to make sure that the q_sqrt matrix can be fixed
-    """
-    num_latent = default_datum_svgp.Y.shape[1]
-    model = gpflow.models.SVGP(kernel=gpflow.kernels.SquaredExponential(),
-                               likelihood=default_datum_svgp.lik,
-                               q_diag=True,
-                               num_latent=num_latent,
-                               inducing_variable=default_datum_svgp.Z,
-                               whiten=False)
-    default_num_trainable_variables = len(model.trainable_variables)
-    model.q_sqrt.trainable = False
-    assert len(model.trainable_variables) == default_num_trainable_variables - 1
+def _check_models_close(m1, m2, tolerance=1e-2):
+    m1_params = {p.name: p for p in list(m1.trainable_parameters)}
+    m2_params = {p.name: p for p in list(m2.trainable_parameters)}
+    if set(m2_params.keys()) != set(m2_params.keys()):
+        return False
+    for key in m1_params:
+        p1 = m1_params[key]
+        p2 = m2_params[key]
+        if not np.allclose(p1.read_value(), p2.read_value(), rtol=tolerance, atol=tolerance):
+            return False
+    return True
 
 
 @pytest.mark.parametrize('indices_1, indices_2, num_data1, num_data2, max_iter', [
@@ -233,28 +158,3 @@ def test_stochastic_gradients(indices_1, indices_2, num_data1, num_data2, max_it
     model_1 = training_loop(indices_1, num_data=num_data1, max_iter=max_iter)
     model_2 = training_loop(indices_2, num_data=num_data2, max_iter=max_iter)
     assert _check_models_close(model_1, model_2)
-
-
-def test_sparse_mcmc_likelihoods_and_gradients():
-    """
-    This test makes sure that when the inducing points are the same as the data
-    points, the sparse mcmc is the same as full mcmc
-    """
-    rng = Datum().rng
-    X, Y = rng.randn(10, 1), rng.randn(10, 1)
-    v_vals = rng.randn(10, 1)
-
-    likelihood = gpflow.likelihoods.StudentT()
-    model_1 = gpflow.models.GPMC(data=(X, Y), kernel=gpflow.kernels.Exponential(), likelihood=likelihood)
-    model_2 = gpflow.models.SGPMC(data=(X, Y),
-                                  kernel=gpflow.kernels.Exponential(),
-                                  inducing_variable=X.copy(),
-                                  likelihood=likelihood)
-    model_1.V = tf.convert_to_tensor(v_vals, dtype=default_float())
-    model_2.V = tf.convert_to_tensor(v_vals, dtype=default_float())
-    model_1.kernel.lengthscale.assign(0.8)
-    model_2.kernel.lengthscale.assign(0.8)
-    model_1.kernel.variance.assign(4.2)
-    model_2.kernel.variance.assign(4.2)
-
-    assert_allclose(model_1.log_likelihood(), model_2.log_likelihood(), rtol=1e-5, atol=1e-5)
