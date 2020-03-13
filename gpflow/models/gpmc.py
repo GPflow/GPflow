6643223bd01a3890373a146e6c079dc38908a04a
ST John <st@prowler.io>
2020-03-13 18:49:15 +0000

    fix (S)GPMC models as well

--- gpflow/models/gpmc.py
+++ gpflow/models/gpmc.py
@@ -34,7 +34,7 @@ class GPMC(GPModel):
         kernel: Kernel,
         likelihood: Likelihood,
         mean_function: Optional[MeanFunction] = None,
-        num_latent_gps: int = 1,
+        num_latent_gps: Optional[int] = None,
     ):
         """
         data is a tuple of X, Y with X, a data matrix, size [N, D] and Y, a data matrix, size [N, R]
@@ -52,6 +52,8 @@ class GPMC(GPModel):
             L L^T = K
 
         """
+        if num_latent_gps is None:
+            num_latent_gps = self.num_latent_gps_for_likelihood_and_data(likelihood, data)
         super().__init__(kernel, likelihood, mean_function, num_latent_gps)
         self.data = data
         self.num_data = data[0].shape[0]
--- gpflow/models/sgpmc.py
+++ gpflow/models/sgpmc.py
@@ -67,7 +67,7 @@ class SGPMC(GPModel):
         kernel: Kernel,
         likelihood: Likelihood,
         mean_function: Optional[MeanFunction] = None,
-        num_latent_gps: int = 1,
+        num_latent_gps: Optional[int] = None,
         inducing_variable: Optional[InducingPoints] = None,
     ):
         """
@@ -75,6 +75,8 @@ class SGPMC(GPModel):
         Z is a data matrix, of inducing inputs, size [M, D]
         kernel, likelihood, mean_function are appropriate GPflow objects
         """
+        if num_latent_gps is None:
+            num_latent_gps = self.num_latent_gps_for_likelihood_and_data(likelihood, data)
         super().__init__(kernel, likelihood, mean_function, num_latent_gps=num_latent_gps)
         self.data = data
         self.num_data = data[0].shape[0]
