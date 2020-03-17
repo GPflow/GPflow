Fix more potential tf.cast() loss-of-precision issues (#1213)

doc/source/notebooks/intro_to_gpflow2.pct.py
@@ -37,6 +37,7 @@ import tensorflow as tf
 import gpflow
 
 from gpflow.config import default_float
+from gpflow.utilities import to_default_float
 
 import warnings
 
@@ -222,7 +223,7 @@ simple_training_loop(model, epochs=10, logging_epoch_freq=2)
 # %%
 from intro_to_gpflow2_plotting import plotting_regression, summary_matplotlib_image
 
-samples_input = tf.cast(np.linspace(0, 10, 100).reshape(100, 1), default_float())
+samples_input = to_default_float(np.linspace(0, 10, 100).reshape(100, 1))
 
 def monitored_training_loop(model: gpflow.models.SVGP, logdir: str,
                             epochs: int = 1, logging_epoch_freq: int = 10,

gpflow/base.py
@@ -302,9 +302,12 @@ def _cast_to_dtype(value: VariableData, dtype: Optional[DType] = None) -> tf.Ten
     if dtype is None:
         dtype = default_float()
     if tf.is_tensor(value):
+        # TODO(awav) TF2.2 resolves issue with cast.
+        # From TF2.2, `tf.cast` can be used alone instead of this auxiliary function.
+        # workaround for https://github.com/tensorflow/tensorflow/issues/35938
         return tf.cast(value, dtype)
     else:
-        return tf.convert_to_tensor(value, dtype)
+        return tf.convert_to_tensor(value, dtype=dtype)
 
 
 def _to_constrained(value: VariableData, transform: Transform) -> tf.Tensor:

gpflow/models/sgpr.py
@@ -24,6 +24,7 @@ from ..config import default_float, default_jitter
 from ..covariances.dispatch import Kuf, Kuu
 from ..inducing_variables import InducingPoints
 from ..mean_functions import Zero, MeanFunction
+from ..utilities import to_default_float
 
 
 class SGPRBase(GPModel):
@@ -92,7 +93,7 @@ class SGPRBase(GPModel):
         which computes each individual element of the trace term.
         """
         X_data, Y_data = self.data
-        num_data = tf.cast(tf.shape(Y_data)[0], default_float())
+        num_data = to_default_float(tf.shape(Y_data)[0])
 
         Kdiag = self.kernel(X_data, full_cov=False)
         kuu = Kuu(self.inducing_variable, self.kernel, jitter=default_jitter())
@@ -155,8 +156,8 @@ class SGPR(SGPRBase):
         X_data, Y_data = self.data
 
         num_inducing = len(self.inducing_variable)
-        num_data = tf.cast(tf.shape(Y_data)[0], default_float())
-        output_dim = tf.cast(tf.shape(Y_data)[1], default_float())
+        num_data = to_default_float(tf.shape(Y_data)[0])
+        output_dim = to_default_float(tf.shape(Y_data)[1])
 
         err = Y_data - self.mean_function(X_data)
         Kdiag = self.kernel(X_data, full_cov=False)