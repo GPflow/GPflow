476b7d395928bfc5bcedf771a7eb66310b81cffc
Ilia Kats <ilia-kats@gmx.net>
2020-03-21 18:30:07 +0100

    add a comment explaining why we need the delattr in _set_by_name_index

--- gpflow/utilities/utilities.py
+++ gpflow/utilities/utilities.py
@@ -265,6 +265,12 @@ def _set_by_name_index(parent: object, value: Any, attr_str: str, index_str: Uni
         index = int(index_str)
         attr = getattr(parent, attr_str)
         attr[index] = value
+        # tensorflow's __setattr__ override does not contain a check for the case where
+        # an attribute holding a trackable object (e.g. a Variable) is being reassigned
+        # a non-trackable object (e.g. a constant). Therefore, tensorflow still stores
+        # internal references to the trackable object after reassignment, which can lead
+        # to various problems, for example https://github.com/GPflow/GPflow/pull/1338
+        # By calling delattr first we are forcing tensorflow to remote its internal reference.
         delattr(parent, attr_str)
         setattr(parent, attr_str, attr)
     else:
