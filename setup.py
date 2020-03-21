bf98637ea6217ba176a3190c6a81cb46eae78cb6
Artem Artemev <art.art.v@gmail.com>
2020-03-21 20:50:13 +0000

    Add setup.py

--- setup.py
+++ setup.py
@@ -11,15 +11,15 @@ from pkg_resources import parse_version
 from setuptools import find_packages, setup
 
 is_py37 = sys.version_info.major == 3 and sys.version_info.minor == 7
-on_rtd = os.environ.get('READTHEDOCS', None) == 'True'  # copied from the docs
+on_rtd = os.environ.get("READTHEDOCS", None) == "True"  # copied from the docs
 
 # Dependencies of GPflow
 requirements = [
-    'numpy>=1.10.0',
-    'scipy>=0.18.0',
-    'multipledispatch>=0.4.9',
-    'tabulate',
-    'gast==0.2.2',
+    "numpy>=1.10.0",
+    "scipy>=0.18.0",
+    "multipledispatch>=0.4.9",
+    "tabulate",
+    "gast==0.2.2",
 ]
 
 if not is_py37:
@@ -28,9 +28,9 @@ if not is_py37:
 if not on_rtd:
     requirements.append("tensorflow-probability>=0.9")
 
-min_tf_version = '2.1.0'
-tf_cpu = 'tensorflow'
-tf_gpu = 'tensorflow-gpu'
+min_tf_version = "2.1.0"
+tf_cpu = "tensorflow"
+tf_gpu = "tensorflow-gpu"
 
 # Only detect TF if not installed or outdated. If not, do not do not list as
 # requirement to avoid installing over e.g. tensorflow-gpu
@@ -39,6 +39,7 @@ tf_gpu = 'tensorflow-gpu'
 try:
     # If tf not installed, import raises ImportError
     import tensorflow as tf
+
     if parse_version(tf.__version__) < parse_version(min_tf_version):
         # TF pre-installed, but below the minimum required version
         raise DeprecationWarning("TensorFlow version below minimum requirement")
@@ -51,27 +52,29 @@ except (ImportError, DeprecationWarning):
 with open(str(Path(".", "VERSION").absolute())) as version_file:
     version = version_file.read().strip()
 
-packages = find_packages('.', exclude=["tests"])
+packages = find_packages(".", exclude=["tests"])
 
-setup(name='gpflow',
-      version=version,
-      author="James Hensman, Alex Matthews",
-      author_email="james.hensman@gmail.com",
-      description="Gaussian process methods in TensorFlow",
-      license="Apache License 2.0",
-      keywords="machine-learning gaussian-processes kernels tensorflow",
-      url="http://github.com/GPflow/GPflow",
-      packages=packages,
-      include_package_data=True,
-      install_requires=requirements,
-      extras_require={'Tensorflow with GPU': [tf_gpu]},
-      python_requires=">=3.6",
-      classifiers=[
-          'License :: OSI Approved :: Apache Software License',
-          'Natural Language :: English',
-          'Operating System :: MacOS :: MacOS X',
-          'Operating System :: Microsoft :: Windows',
-          'Operating System :: POSIX :: Linux',
-          'Programming Language :: Python :: 3.6',
-          'Topic :: Scientific/Engineering :: Artificial Intelligence'
-      ])
+setup(
+    name="gpflow",
+    version=version,
+    author="James Hensman, Alex Matthews",
+    author_email="james.hensman@gmail.com",
+    description="Gaussian process methods in TensorFlow",
+    license="Apache License 2.0",
+    keywords="machine-learning gaussian-processes kernels tensorflow",
+    url="http://github.com/GPflow/GPflow",
+    packages=packages,
+    include_package_data=True,
+    install_requires=requirements,
+    extras_require={"Tensorflow with GPU": [tf_gpu]},
+    python_requires=">=3.6",
+    classifiers=[
+        "License :: OSI Approved :: Apache Software License",
+        "Natural Language :: English",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: POSIX :: Linux",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
+)
