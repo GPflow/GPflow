326cd8ec52e573cf6899fc1df807daeee532202f
ST John <st@prowler.io>
2020-03-27 11:49:03 +0000

    Revert "update _compute_loss_and_gradients for Scipy optimizer to only compute gradients wrt passed-in variables"
    This reverts commit f17fc8591d5dcd8e6eaad97368426f5a562631c0.

--- gpflow/optimizers/scipy.py
+++ gpflow/optimizers/scipy.py
@@ -134,8 +134,7 @@ V = TypeVar("V", bound=Variables)
 
 
 def _compute_loss_and_gradients(loss_closure: LossClosure, variables: V) -> Tuple[tf.Tensor, V]:
-    with tf.GradientTape(watch_accessed_variables=False) as tape:
-        tape.watch(variables)
+    with tf.GradientTape() as tape:
         loss = loss_closure()
-        grads = tape.gradient(loss, variables)
+    grads = tape.gradient(loss, variables)
     return loss, grads
