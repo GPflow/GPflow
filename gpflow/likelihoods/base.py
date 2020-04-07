a243e7e9eea3f7971383a10b38b25851bbe7cdce
ST John <st@prowler.io>
2020-04-07 12:44:08 +0100

    move inv_probit into utils.py

--- gpflow/likelihoods/base.py
+++ gpflow/likelihoods/base.py
@@ -65,11 +65,6 @@ from ..utilities import positive, to_default_int
 from .robustmax import RobustMax
 
 
-def inv_probit(x):
-    jitter = 1e-3  # ensures output is strictly between 0 and 1
-    return 0.5 * (1.0 + tf.math.erf(x / np.sqrt(2.0))) * (1 - 2 * jitter) + jitter
-
-
 class Likelihood(Module, metaclass=abc.ABCMeta):
     def __init__(self, latent_dim: int, observation_dim: int):
         """
