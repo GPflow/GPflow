cd2fc4c3e0f3c8cb0bc600ac4e803569a7eb568b
Eric Hammy <6815729+condnsdmatters@users.noreply.github.com>
2020-04-24 16:35:34 +0100

    Apply suggestions from code review
    Co-Authored-By: st-- <st--@users.noreply.github.com>

--- tests/gpflow/kernels/test_changepoints.py
+++ tests/gpflow/kernels/test_changepoints.py
@@ -1,22 +1,17 @@
 import numpy as np
 import gpflow
-import numpy as np
-import gpflow
 
 
 def test_changepoint_with_X1_X2():
-    X = np.linspace(0, 100, 100).reshape(100, 1)
+    N = 100
+    X = np.linspace(0, 100, N).reshape(N, 1)
     base_k1 = gpflow.kernels.Matern32(lengthscales=0.2)
     base_k2 = gpflow.kernels.Matern32(lengthscales=2.0)
     k = gpflow.kernels.ChangePoints([base_k1, base_k2], [0.0], steepness=5.0)
     K = k(X)
-    assert K.shape == [100, 100]
+    assert K.shape == [N, N]
 
-    N = 25
-    X2 = np.linspace(0, 50, N).reshape(N, 1)
+    N2 = 25
+    X2 = np.linspace(0, 50, N2).reshape(N2, 1)
     K = k(X, X2)
-    assert K.shape == [100, 25]
-
-
-if __name__ == "__main__":
-    test_changepoint_with_X1_X2()
+    assert K.shape == [N, N2]
