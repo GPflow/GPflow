GPflow/GPflow

commit a198764d941687bea9a0efa74773bff804bfdb44

enable mypy on CI (#1471)

Makefile
@@ -23,5 +23,8 @@ package:
 format:
 	black -t py36 -l 100 gpflow tests doc setup.py
 
+type-check:
+    mypy .
+
 test:
 	pytest -v --durations=10 tests/

.circleci/config.yml
@@ -19,6 +19,19 @@ runtest: &runtest
 version: 2.1
 
 jobs:
+  type-check:
+    docker:
+      - image: awav/tensorflow:2.1
+
+    steps:
+      - checkout
+      - run:
+          name: Install type checker
+          command: pip install mypy
+      - run:
+          name: Run type checker
+          command: mypy .
+
   unit-test:
     <<: *runtest
     docker:
@@ -103,6 +116,10 @@ workflows:
   version: 2.1
   build_test_and_deploy:
     jobs:
+      - type-check:
+          filters:
+            tags:
+              only: /^v[0-9]+(\.[0-9]+)*(-rc[0-9]+)?/
       - format-checker:
           filters:
             tags:

gpflow/base.py
@@ -107,7 +107,7 @@ class Parameter(tf.Module):
 
         self._transform = transform
         self.prior = prior
-        self.prior_on = prior_on
+        self.prior_on = prior_on  # type: ignore  # see https://github.com/python/mypy/issues/3004
 
         if isinstance(value, tf.Variable):
             self._unconstrained = value

mypy.ini
@@ -1,2 +1,19 @@
 [mypy]
 ignore_missing_imports = True
+
+; the following modules are not passing mypy, so we're skipping them until they've been fixed up.
+; see github issue https://github.com/GPflow/GPflow/issues/1368 for more information. Note that some
+; modules that haven't had types added are passing anyway so may not appear below.
+;
+; remove modules from this skip list when types have been added
+[mypy-gpflow.conditionals.*,gpflow.config.*,gpflow.covariances.*,gpflow.expectations.*]
+ignore_errors = True
+
+[mypy-gpflow.kernels.*,gpflow.models.*,gpflow.monitor.*,gpflow.optimizers.*,gpflow.utilities.*]
+ignore_errors = True
+
+[mypy-gpflow.mean_functions]
+ignore_errors = True
+
+[mypy-tests.*]
+ignore_errors = True

commit 1929a362d206dd1e84ad375c751f141c1870b419

Makefile quickfix (#1482)
Fixes a syntax error in the Makefile introduced by #1471. Also adds a `format-check` target for black --check and a `check-all` target that runs format-check, type-check and the pytest tests.

Makefile
@@ -1,12 +1,19 @@
-.PHONY: help clean dev-install install package format test
+BLACK_CONFIG=-t py36 -l 100
+BLACK_TARGETS=gpflow tests doc setup.py
+
+.PHONY: help clean dev-install install package format format-check type-check test check-all
 
 help:
 	@echo "The following make targets are available:"
 	@echo "	dev-install		install all dependencies for dev environment and sets a egg link to the project sources"
 	@echo "	install			install all dependencies and the project in the current environment"
 	@echo "	package			build pip package"
-	@echo "	test			run all tests in parallel"
 	@echo "	clean			removes package, build files and egg info"
+	@echo "	format			auto-format code"
+	@echo "	format-check		check that code has been formatted correctly"
+	@echo "	type-check		check that mypy is happy with type annotations"
+	@echo "	test			run all tests"
+	@echo "	check-all		run format-check, type-check, and test (as run by the continuous integration system)"
 
 clean:
 	rm -rf dist *.egg-info build
@@ -21,10 +28,15 @@ package:
 	python setup.py bdist
 
 format:
-	black -t py36 -l 100 gpflow tests doc setup.py
+	black $(BLACK_CONFIG) $(BLACK_TARGETS)
+
+format-check:
+	black --check $(BLACK_CONFIG) $(BLACK_TARGETS)
 
 type-check:
-    mypy .
+	mypy .
 
 test:
 	pytest -v --durations=10 tests/
+
+check-all: format-check type-check test