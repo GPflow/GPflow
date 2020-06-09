41eac9b2a11f2d1b91ecf2e8c30444984747abec
Gustavo Carvalho <gustavo.carvalho@delfosim.com>
2020-06-09 15:26:06 -0300

    docs

--- gpflow/quadrature/base.py
+++ gpflow/quadrature/base.py
@@ -10,6 +10,14 @@ class GaussianQuadrature:
         raise NotImplementedError
 
     def logspace(self, fun, mean, var, *args, **kwargs):
+        r"""
+        Compute the Gaussian log-Expectation of a function f
+            q(x) = N(mean, var)
+            E_{X~q(x)}[f(X)] = log∫f(x)q(x)dx
+        Using the approximation
+            log \sum exp(f(x_i) + log w_i)
+        Where x_i, w_i must be provided by some inheriting class
+        """
         X, W = self._build_X_W(mean, var)
         logW = tf.math.log(W)
         if isinstance(fun, Iterable):
@@ -17,6 +25,14 @@ class GaussianQuadrature:
         return tf.reduce_logsumexp(fun(X, *args, **kwargs) + logW, axis=-2)
 
     def __call__(self, fun, mean, var, *args, **kwargs):
+        r"""
+        Compute the Gaussian Expectation of a function f
+            q(x) = N(mean, var)
+            E_{X~q(x)}[f(X)] = ∫f(x)q(x)dx
+        Using the approximation
+            \sum f(x_i)*w_i
+        Where x_i, w_i must be provided by some inheriting class
+        """
         X, W = self._build_X_W(mean, var)
         if isinstance(fun, Iterable):
             # Maybe this can be better implemented by stacking [f1(X), f2(X), ...]
