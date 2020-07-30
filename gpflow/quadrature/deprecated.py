d9951946d64542268b85610593495e187343d42e
gustavocmv <47801305+gustavocmv@users.noreply.github.com>
2020-07-30 08:58:40 -0300

    Quadrature Refactoring (#1505)
    * WIP: quadrature refactoring
    
    * Removing old ndiagquad code
    
    * deleted test code
    
    * formatting and type-hint
    
    * merge modules
    
    * black formatting
    
    * formatting
    
    * solving failing tests
    
    * fixing failing tests
    
    * fixes
    
    * adapting tests for new syntax, keeping numerical behavior
    
    * black formatting
    
    * remove printf
    
    * changed code for compiled tf compatibility
    
    * black
    
    * restored to original version
    
    * undoing changes
    
    * renaming
    
    * renaming
    
    * renaming
    
    * reshape kwargs
    
    * quadrature along axis=-2, simplified broadcasting
    
    * black
    
    * docs
    
    * docs
    
    * helper function
    
    * docstrings and typing
    
    * added new and old quadrature equivalence tests
    
    * black
    
    * Removing comments
    
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>
    
    * Typo
    
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>
    
    * notation
    
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>
    
    * reshape_Z_dZ return docstring fix
    
    * FIX: quad_old computed with the ndiagquad_old
    
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>
    
    * more readable implementation
    
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>
    
    * tf.ensure_shape added
    
    * removed ndiagquad
    
    * removed ndiagquad
    
    * Revert "removed ndiagquad"
    
    This reverts commit 7bb0e9f1e0f2b0e225a2b8a5b3092c4c2f24ba91.
    
    * FIX: shape checking of dZ
    
    * Revert "removed ndiagquad"
    
    This reverts commit 8e235241a697696e361158c30ff9aa9b4cc69f8a.
    
    Co-authored-by: Gustavo Carvalho <gustavo.carvalho@delfosim.com>
    Co-authored-by: ST John <st@prowler.io>
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>

--- gpflow/quadrature/deprecated.py
+++ gpflow/quadrature/deprecated.py
@@ -18,8 +18,10 @@ from collections.abc import Iterable
 import numpy as np
 import tensorflow as tf
 
-from .config import default_float
-from .utilities import to_default_float
+from ..config import default_float
+from ..utilities import to_default_float
+
+from .gauss_hermite import NDiagGHQuadrature
 
 
 def hermgauss(n: int):
@@ -117,51 +119,48 @@ def ndiagquad(funcs, H: int, Fmu, Fvar, logspace: bool = False, **Ys):
     Fmu, Fvar, Ys should all have same shape, with overall size `N`
     :return: shape is the same as that of the first Fmu
     """
+    n_gh = H
     if isinstance(Fmu, (tuple, list)):
-        Din = len(Fmu)
-
-        def unify(f_list):
-            """Stack a list of means/vars into a full block."""
-            return tf.reshape(
-                tensor=tf.concat([tf.reshape(f, shape=(-1, 1)) for f in f_list], axis=1),
-                shape=(-1, 1, Din),
-            )
-
+        dim = len(Fmu)
         shape = tf.shape(Fmu[0])
-        Fmu, Fvar = map(unify, [Fmu, Fvar])  # both [N, 1, Din]
+        Fmu = tf.stack(Fmu, axis=-1)
+        Fvar = tf.stack(Fvar, axis=-1)
     else:
-        Din = 1
+        dim = 1
         shape = tf.shape(Fmu)
-        Fmu, Fvar = [tf.reshape(f, (-1, 1, 1)) for f in [Fmu, Fvar]]
 
-    xn, wn = mvhermgauss(H, Din)
-    # xn: H**Din x Din, wn: H**Din
+    Fmu = tf.reshape(Fmu, (-1, dim))
+    Fvar = tf.reshape(Fvar, (-1, dim))
 
-    gh_x = xn.reshape(1, -1, Din)  # [1, H]**Din x Din
-    Xall = gh_x * tf.sqrt(2.0 * Fvar) + Fmu  # [N, H]**Din x Din
-    Xs = [Xall[:, :, i] for i in range(Din)]  # [N, H]**Din  each
+    Ys = {Yname: tf.reshape(Y, (-1, 1)) for Yname, Y in Ys.items()}
 
-    gh_w = wn * np.pi ** (-0.5 * Din)  # H**Din x 1
+    def wrapper(old_fun):
+        def new_fun(X, **Ys):
+            Xs = tf.unstack(X, axis=-1)
+            fun_eval = old_fun(*Xs, **Ys)
+            if tf.rank(fun_eval) < tf.rank(X):
+                fun_eval = tf.expand_dims(fun_eval, axis=-1)
+            return fun_eval
 
-    for name, Y in Ys.items():
-        Y = tf.reshape(Y, (-1, 1))
-        Y = tf.tile(Y, [1, H ** Din])  # broadcast Y to match X
-        # without the tiling, some calls such as tf.where() (in bernoulli) fail
-        Ys[name] = Y  # now [N, H]**Din
-
-    def eval_func(f):
-        feval = f(*Xs, **Ys)  # f should be elementwise: return shape [N, H]**Din
-        if logspace:
-            log_gh_w = np.log(gh_w.reshape(1, -1))
-            result = tf.reduce_logsumexp(feval + log_gh_w, axis=1)
-        else:
-            result = tf.linalg.matmul(feval, gh_w.reshape(-1, 1))
-        return tf.reshape(result, shape)
+        return new_fun
 
     if isinstance(funcs, Iterable):
-        return [eval_func(f) for f in funcs]
+        funcs = [wrapper(f) for f in funcs]
+    else:
+        funcs = wrapper(funcs)
 
-    return eval_func(funcs)
+    quadrature = NDiagGHQuadrature(dim, n_gh)
+    if logspace:
+        result = quadrature.logspace(funcs, Fmu, Fvar, **Ys)
+    else:
+        result = quadrature(funcs, Fmu, Fvar, **Ys)
+
+    if isinstance(result, list):
+        result = [tf.reshape(r, shape) for r in result]
+    else:
+        result = tf.reshape(result, shape)
+
+    return result
 
 
 def ndiag_mc(funcs, S: int, Fmu, Fvar, logspace: bool = False, epsilon=None, **Ys):
