8c65ef658930afecb7001ce671f15772a0a4b23b
Artem Artemev <art.art.v@gmail.com>
2020-08-17 14:26:52 +0100

    Update parameter to be savable (#1518)

--- doc/source/notebooks/understanding/models.pct.py
+++ doc/source/notebooks/understanding/models.pct.py
@@ -119,10 +119,11 @@ m.kernel.kernels[0].lengthscales = new_parameter
 p.transform.inverse(p)
 
 # %% [markdown]
-# You can also change the `transform` attribute in place:
+# To replace the `transform` of a parameter you need to recreate the parameter with updated transform:
 
 # %%
-m.kernel.kernels[0].variance.transform = tfp.bijectors.Exp()
+p = m.kernel.kernels[0].variance
+m.kernel.kernels[0].variance = gpflow.Parameter(p.numpy(), transform=tfp.bijectors.Exp())
 
 # %%
 print_summary(m, fmt="notebook")
@@ -281,7 +282,7 @@ opt.minimize(m.training_loss, variables=m.trainable_variables)
 # %%
 xx, yy = np.mgrid[-4:4:200j, -4:4:200j]
 X_test = np.vstack([xx.flatten(), yy.flatten()]).T
-f_test = np.dot(X_test, m.W.read_value()) + m.b.read_value()
+f_test = np.dot(X_test, m.W.numpy()) + m.b.numpy()
 p_test = np.exp(f_test)
 p_test /= p_test.sum(1)[:, None]
 
