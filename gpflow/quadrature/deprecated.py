d025f9603419c338606e671258833922fa75e662
gustavocmv <47801305+gustavocmv@users.noreply.github.com>
2020-08-24 15:54:38 -0300

    Change shape of quadrature tensors for better broadcasting (#1542)
    * using the first dimension to hold the quadrature summation
    
    * adapting ndiagquad wrapper
    
    * Changed bf for bX in docstrings
    
    Co-authored-by: Gustavo Carvalho <gustavo.carvalho@delfosim.com>
    Co-authored-by: st-- <st--@users.noreply.github.com>
    Co-authored-by: Vincent Dutordoir <dutordoirv@gmail.com>

--- gpflow/quadrature/deprecated.py
+++ gpflow/quadrature/deprecated.py
@@ -136,7 +136,7 @@ def ndiagquad(funcs, H: int, Fmu, Fvar, logspace: bool = False, **Ys):
 
     def wrapper(old_fun):
         def new_fun(X, **Ys):
-            Xs = tf.unstack(X, axis=-1)
+            Xs = tf.unstack(tf.expand_dims(X, axis=-2), axis=-1)
             fun_eval = old_fun(*Xs, **Ys)
             return tf.cond(
                 pred=tf.less(tf.rank(fun_eval), tf.rank(X)),
