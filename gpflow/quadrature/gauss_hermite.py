ced9428a59319c7456206f307ba0e7fd4a7dbc38
Gustavo Carvalho <gustavo.carvalho@delfosim.com>
2020-09-02 14:56:03 -0300

    removed unecessary tf.init_scope

--- gpflow/quadrature/gauss_hermite.py
+++ gpflow/quadrature/gauss_hermite.py
@@ -83,10 +83,9 @@ class NDiagGHQuadrature(GaussianQuadrature):
         self.dim = dim
         self.n_gh = n_gh
         self.n_gh_total = n_gh ** dim
-        with tf.init_scope():
-            Z, dZ = ndgh_points_and_weights(self.dim, self.n_gh)
-            self.Z = tf.ensure_shape(Z, (self.n_gh_total, self.dim))
-            self.dZ = tf.ensure_shape(dZ, (self.n_gh_total, 1))
+        Z, dZ = ndgh_points_and_weights(self.dim, self.n_gh)
+        self.Z = tf.ensure_shape(Z, (self.n_gh_total, self.dim))
+        self.dZ = tf.ensure_shape(dZ, (self.n_gh_total, 1))
 
     def _build_X_W(self, mean: TensorType, var: TensorType):
         """
