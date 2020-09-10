a0df00ca67f074b917f8d12fb3363f9433a20c1e
st-- <st--@users.noreply.github.com>
2020-09-10 12:45:58 +0100

    add license text to all gpflow submodules (#1480)
    Adds Copyright notice and Apache license text (short form) to the preamble of all python files underneath gpflow/.
    Also unifies the copyright notice to simply state "The GPflow Contributors" to simplify maintenance of the copyright notices (this does not affect who holds copyright).

--- gpflow/models/svgp.py
+++ gpflow/models/svgp.py
@@ -1,4 +1,4 @@
-# Copyright 2016 James Hensman, Valentine Svensson, alexggmatthews, Mark van der Wilk
+# Copyright 2016-2020 The GPflow Contributors. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 from typing import Tuple
 
 import numpy as np
