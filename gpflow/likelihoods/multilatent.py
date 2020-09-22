e746108e386bae64df1d5e38da06baf1831524c1
ST John <st@prowler.io>
2020-09-22 15:36:47 +0100

    more explicit argument name

--- gpflow/likelihoods/multilatent.py
+++ gpflow/likelihoods/multilatent.py
@@ -97,20 +97,21 @@ class HeteroskedasticTFPConditional(MultiLatentTFPConditional):
     def __init__(
         self,
         distribution_class: Type[tfp.distributions.Distribution] = tfp.distributions.Normal,
-        transform: tfp.bijectors.Bijector = positive(base="exp"),
+        scale_transform: tfp.bijectors.Bijector = positive(base="exp"),
         **kwargs,
     ):
         """
         :param distribution_class: distribution class parameterized by `loc` and `scale`
-            as first and second argument, respectivily.
-        :param transform: callable applied to the variance GP to make it positive.
+            as first and second argument, respectively.
+        :param scale_transform: callable/bijector applied to the latent
+            function modelling the scale to ensure its positivity.
             Typically, `tf.exp` or `tf.softplus`, but can be any function f: R -> R^+.
         """
 
         def conditional_distribution(Fs) -> tfp.distributions.Distribution:
             tf.debugging.assert_equal(tf.shape(Fs)[-1], 2)
             loc = Fs[..., :1]
-            scale = transform(Fs[..., 1:])
+            scale = scale_transform(Fs[..., 1:])
             return distribution_class(loc, scale)
 
         super().__init__(
