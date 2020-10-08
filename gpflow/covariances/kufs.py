8e81151ad51c647919ca8545bc37d346e09b873e
ST John <st--@users.noreply.github.com>
2020-10-08 12:06:42 +0100

    format

--- gpflow/covariances/kufs.py
+++ gpflow/covariances/kufs.py
@@ -39,6 +39,8 @@ def Kuf_sqexp_multiscale(inducing_variable: Multiscale, kernel: SquaredExponenti
 @Kuf.register(InducingPatches, Convolutional, object)
 def Kuf_conv_patch(inducing_variable, kernel, Xnew):
     Xp = kernel.get_patches(Xnew)  # [N, num_patches, patch_len]
-    bigKzx = kernel.base_kernel.K(inducing_variable.Z, Xp)  # [M, N, P] -- thanks to broadcasting of kernels
+    bigKzx = kernel.base_kernel.K(
+        inducing_variable.Z, Xp
+    )  # [M, N, P] -- thanks to broadcasting of kernels
     Kzx = tf.reduce_sum(bigKzx * kernel.weights if hasattr(kernel, "weights") else bigKzx, [2])
     return Kzx / kernel.num_patches
--- gpflow/covariances/kuus.py
+++ gpflow/covariances/kuus.py
@@ -42,4 +42,6 @@ def Kuu_sqexp_multiscale(inducing_variable: Multiscale, kernel: SquaredExponenti
 
 @Kuu.register(InducingPatches, Convolutional)
 def Kuu_conv_patch(inducing_variable, kernel, jitter=0.0):
-    return kernel.base_kernel.K(inducing_variable.Z) + jitter * tf.eye(inducing_variable.num_inducing, dtype=default_float())
+    return kernel.base_kernel.K(inducing_variable.Z) + jitter * tf.eye(
+        inducing_variable.num_inducing, dtype=default_float()
+    )
