Fixed broadcasting rules for gpflow.models.model.predict_y, partially resolves #1461.

gpflow/likelihoods/scalar_continuous.py
@@ -18,9 +18,9 @@ import tensorflow as tf
 from .. import logdensities
 from ..base import Parameter
 from ..utilities import positive
+from ..utilities.ops import eye
 from .base import ScalarLikelihood
 from .utils import inv_probit
-
 
 class Gaussian(ScalarLikelihood):
     r"""
@@ -61,7 +61,11 @@ class Gaussian(ScalarLikelihood):
         return tf.fill(tf.shape(F), tf.squeeze(self.variance))
 
     def _predict_mean_and_var(self, Fmu, Fvar):
-        return tf.identity(Fmu), Fvar + self.variance
+        rank = tf.rank(Fvar).numpy()
+        if rank == 2:
+            return tf.identity(Fmu), Fvar + self.variance
+        else:
+            return tf.identity(Fmu), Fvar + eye(Fvar.shape[-1], self.variance)
 
     def _predict_log_density(self, Fmu, Fvar, Y):
         return tf.reduce_sum(logdensities.gaussian(Y, Fmu, Fvar + self.variance), axis=-1)

gpflow/models/model.py
@@ -211,14 +211,14 @@ class GPModel(BayesianModel):
         """
         Compute the mean and variance of the held-out data at the input points.
         """
-        if full_cov or full_output_cov:
-            # See https://github.com/GPflow/GPflow/issues/1461
-            raise NotImplementedError(
-                "The predict_y method currently supports only the argument values full_cov=False and full_output_cov=False"
-            )
-
         f_mean, f_var = self.predict_f(Xnew, full_cov=full_cov, full_output_cov=full_output_cov)
-        return self.likelihood.predict_mean_and_var(f_mean, f_var)
+        
+        if full_cov and full_output_cov:
+            f_var_mat = tf.reshape(f_var, [1, f_var.shape[0]*f_var.shape[1], f_var.shape[2]*f_var.shape[3]])
+            f_mean_pred, f_var_pred = self.likelihood.predict_mean_and_var(f_mean, f_var_mat)
+            return f_mean_pred, tf.reshape(f_var_pred, f_var.shape)
+        else:
+            return self.likelihood.predict_mean_and_var(f_mean, f_var)
 
     def predict_log_density(
         self, data: RegressionData, full_cov: bool = False, full_output_cov: bool = False