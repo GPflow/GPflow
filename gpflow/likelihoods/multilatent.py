3c7d54a2461e7349a253e0a49b2ec145ecee7b7b
Pola Elisabeth Schwobel <posc@dtu.dk>
2020-10-13 11:34:12 +0200

    change HeteroskedasticTFPConditional so it only constructs the tensor at class-construction, not at module-import time

--- gpflow/likelihoods/multilatent.py
+++ gpflow/likelihoods/multilatent.py
@@ -109,11 +109,12 @@ class HeteroskedasticTFPConditional(MultiLatentTFPConditional):
         """
         if scale_transform is None:
             scale_transform = positive(base="exp")
+        self.scale_transform = scale_transform
 
         def conditional_distribution(Fs) -> tfp.distributions.Distribution:
             tf.debugging.assert_equal(tf.shape(Fs)[-1], 2)
             loc = Fs[..., :1]
-            scale = scale_transform(Fs[..., 1:])
+            scale = self.scale_transform(Fs[..., 1:])
             return distribution_class(loc, scale)
 
         super().__init__(
