3ff792327e382bff266ec2491173283f3860c332
ST John <st--@users.noreply.github.com>
2021-01-28 17:14:37 +0000

    incorporate code from posterior_conditional.py

--- gpflow/posterior.py
+++ gpflow/posterior.py
@@ -161,6 +161,29 @@ class BasePosterior(AbstractPosterior):
 
 
 class LinearOperatorBasePosterior(BasePosterior):
+    # WIP
+
+    # base Kuu [M, M]:
+    #  - LinearOperatorFullMatrix
+    #  - LinearOperatorBlockDiag
+    #  - LinearOperatorDiag
+
+    # Kernels / inducing variables:
+    #  - single-output = shared IV *and* kernel; Kuu [M, M], Kuf [M, ..., N] / Kfu [..., N, M]
+    #  - fully-correlated multi-output; Kuu [M, P, M, P], Kuf [M, P, ..., N, P] / Kfu [..., N, P, M, P]
+    #  - separate independent IV and/or kernel (mainly for DGPs); Kuu [L=P, M, M], Kuf [L=P, ..., N] / Kfu [..., N, L=P, M]
+    #  - LinearCoregionalization, IV in g-space; Kuu [L, M, M], Kuf [L, M, ..., N] / Kfu [..., N, L, M]
+    #  - LinearCoregionalization, IV in f-space (fallback); Kuu [L, M, M], Kuf [M, L, ..., N, P] / Kfu [..., N, P, M, L]
+
+    # q(u):
+    #  - fully-correlated, [M*L, M*L]
+    #    LinearOperatorFullMatrix(eye(M*L))
+    #  - block-diagonal (mean field across L), [L, M, M]
+    #    LinearOperatorBlockDiag([LinearOperatorFullMatrix(eye(M)) for _ in range(L)])
+    #  - diagonal (mean field across L), [M, L] --> [L, M]?
+    #    LinearOperatorDiag(ones(M*L))
+
+
     def _set_qdist(self, q_mu, q_sqrt):
         q_mu = tf.linalg.adjoint(q_mu)  # TODO redefine how q_mu is stored internally
         if len(q_sqrt.shape) == 2:  # q_diag
@@ -181,22 +204,25 @@ class LinearOperatorBasePosterior(BasePosterior):
         Kuu = covariances.Kuu(
             self.inducing_variable, self.kernel, jitter=default_jitter()
         )  # [(R), M, M]
+
         q_mu = self.q_dist.loc
+        q_sqrt = self.q_dist.scale  # type: tf.linalg.LinearOperator
 
         if Kuu.shape.ndims == 4:
-            # this branch only gets called  in fully-correlated case
+            # this branch only gets called in fully-correlated case
             # TODO this branch won't work; it's left-over from dense Kuu matrices
             ML = tf.reduce_prod(tf.shape(Kuu)[:2])
             Kuu = tf.reshape(Kuu, [ML, ML])
 
-        L = tf.linalg.cholesky(Kuu)
+        L = Kuu.cholesky()
 
         if not self.whiten:
             # alpha = Kuu⁻¹ q_mu
-            alpha = tf.linalg.cholesky_solve(L, q_mu)
+            alpha = L.solvevec(L.solvevec(q_mu), adjoint=True)  # type: tf.Tensor
         else:
             # alpha = L⁻ᵀ q_mu
-            alpha = tf.linalg.triangular_solve(L, q_mu, adjoint=True)
+            alpha = L.solvevec(q_mu, adjoint=True)  # type: tf.Tensor
+
         # predictive mean = Kfu alpha
         # predictive variance = Kff - Kfu Qinv Kuf
         # S = q_sqrt q_sqrtᵀ
@@ -205,26 +231,24 @@ class LinearOperatorBasePosterior(BasePosterior):
             #      = Kuu⁻¹ - L⁻ᵀ L⁻¹ S L⁻ᵀ L⁻¹
             #      = L⁻ᵀ (I - L⁻¹ S L⁻ᵀ) L⁻¹
             #      = L⁻ᵀ B L⁻¹
-            if isinstance(self.q_dist, DiagNormal):
-                q_sqrt = tf.linalg.diag(tf.linalg.adjoint(self.q_dist.q_sqrt))
-            else:
-                q_sqrt = self.q_dist.q_sqrt
-            Linv_qsqrt = tf.linalg.triangular_solve(L, q_sqrt)
-            Linv_cov_u_LinvT = tf.matmul(Linv_qsqrt, Linv_qsqrt, transpose_b=True)
+            # Linv_qsqrt = L.solve(q_sqrt)
+            # Linv_cov_u_LinvT = Linv_qsqrt.matmul(Linv_qsqrt, adjoint_arg=True)
+            KuuInv_qsqrt = L.solve(L.solve(q_sqrt), adjoint=True)
+            KuuInv_covu_KuuInv = KuuInv_qsqrt.matmul(KuuInv_qsqrt, adjoint_arg=True)
         else:
-            if isinstance(self.q_dist, DiagNormal):
-                Linv_cov_u_LinvT = tf.linalg.diag(tf.linalg.adjoint(self.q_dist.q_sqrt ** 2))
-            else:
-                q_sqrt = self.q_dist.q_sqrt
-                Linv_cov_u_LinvT = tf.matmul(q_sqrt, q_sqrt, transpose_b=True)
             # Qinv = Kuu⁻¹ - L⁻ᵀ S L⁻¹
             # Linv = (L⁻¹ I) = solve(L, I)
             # Kinv = Linvᵀ @ Linv
-        I = tf.eye(tf.shape(Linv_cov_u_LinvT)[-1], dtype=Linv_cov_u_LinvT.dtype)
-        B = I - Linv_cov_u_LinvT
-        LinvT_B = tf.linalg.triangular_solve(L, B, adjoint=True)
-        B_Linv = tf.linalg.adjoint(LinvT_B)
-        Qinv = tf.linalg.triangular_solve(L, B_Linv, adjoint=True)
+            # Linv_cov_u_LinvT = q_sqrt.matmul(q_sqrt, adjoint_arg=True)
+            LinvT_qsqrt = L.solve(q_sqrt, adjoint=True)
+            KuuInv_covu_KuuInv = LinvT_qsqrt.matmul(LinvT_qsqrt, adjoint_arg=True)
+        # I = tf.eye(tf.shape(Linv_cov_u_LinvT)[-1], dtype=Linv_cov_u_LinvT.dtype)
+        # B = I - Linv_cov_u_LinvT
+        # LinvT_B = tf.linalg.triangular_solve(L, B, adjoint=True)
+        # B_Linv = tf.linalg.adjoint(LinvT_B)
+        # Qinv = tf.linalg.triangular_solve(L, B_Linv, adjoint=True)
+
+        Qinv = Kuu.inverse() - KuuInv_covu_KuuInv  # XXX LinearOperator does not support `-`
 
         return alpha, Qinv
 
