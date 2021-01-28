b83864a9539764670fe1ff49c4876078df9d89f9
ST John <st--@users.noreply.github.com>
2021-01-28 12:35:20 +0000

    LinearOperatorBasePosterior -- WIP

--- gpflow/posterior.py
+++ gpflow/posterior.py
@@ -17,6 +17,7 @@ from typing import Tuple
 
 import numpy as np
 import tensorflow as tf
+import tensorflow_probability as tfp
 
 from . import covariances, kernels
 from .base import Module, Parameter
@@ -119,6 +120,75 @@ class BasePosterior(AbstractPosterior):
             Kuu = tf.reshape(Kuu, [ML, ML])
         if Kuu.shape.ndims == 3:
             q_mu = tf.linalg.adjoint(self.q_dist.q_mu)[..., None]  # [..., R, M, 1]
+        L = tf.linalg.cholesky(Kuu)
+
+        if not self.whiten:
+            # alpha = Kuu⁻¹ q_mu
+            alpha = tf.linalg.cholesky_solve(L, q_mu)
+        else:
+            # alpha = L⁻ᵀ q_mu
+            alpha = tf.linalg.triangular_solve(L, q_mu, adjoint=True)
+        # predictive mean = Kfu alpha
+        # predictive variance = Kff - Kfu Qinv Kuf
+        # S = q_sqrt q_sqrtᵀ
+        if not self.whiten:
+            # Qinv = Kuu⁻¹ - Kuu⁻¹ S Kuu⁻¹
+            #      = Kuu⁻¹ - L⁻ᵀ L⁻¹ S L⁻ᵀ L⁻¹
+            #      = L⁻ᵀ (I - L⁻¹ S L⁻ᵀ) L⁻¹
+            #      = L⁻ᵀ B L⁻¹
+            if isinstance(self.q_dist, DiagNormal):
+                q_sqrt = tf.linalg.diag(tf.linalg.adjoint(self.q_dist.q_sqrt))
+            else:
+                q_sqrt = self.q_dist.q_sqrt
+            Linv_qsqrt = tf.linalg.triangular_solve(L, q_sqrt)
+            Linv_cov_u_LinvT = tf.matmul(Linv_qsqrt, Linv_qsqrt, transpose_b=True)
+        else:
+            if isinstance(self.q_dist, DiagNormal):
+                Linv_cov_u_LinvT = tf.linalg.diag(tf.linalg.adjoint(self.q_dist.q_sqrt ** 2))
+            else:
+                q_sqrt = self.q_dist.q_sqrt
+                Linv_cov_u_LinvT = tf.matmul(q_sqrt, q_sqrt, transpose_b=True)
+            # Qinv = Kuu⁻¹ - L⁻ᵀ S L⁻¹
+            # Linv = (L⁻¹ I) = solve(L, I)
+            # Kinv = Linvᵀ @ Linv
+        I = tf.eye(tf.shape(Linv_cov_u_LinvT)[-1], dtype=Linv_cov_u_LinvT.dtype)
+        B = I - Linv_cov_u_LinvT
+        LinvT_B = tf.linalg.triangular_solve(L, B, adjoint=True)
+        B_Linv = tf.linalg.adjoint(LinvT_B)
+        Qinv = tf.linalg.triangular_solve(L, B_Linv, adjoint=True)
+
+        return alpha, Qinv
+
+
+class LinearOperatorBasePosterior(BasePosterior):
+    def _set_qdist(self, q_mu, q_sqrt):
+        q_mu = tf.linalg.adjoint(q_mu)  # TODO redefine how q_mu is stored internally
+        if len(q_sqrt.shape) == 2:  # q_diag
+            q_sqrt = tf.linalg.adjoint(q_sqrt)  # TODO redefine how q_sqrt is stored internally
+            tf.debugging.assert_shapes([
+                (q_mu, ["L", "M"]),
+                (q_sqrt, ["L", "M"]),
+            ])
+            self.q_dist = tfp.distributions.MultivariateNormalDiag(loc=q_mu, scale_diag=q_sqrt)
+        else:
+            tf.debugging.assert_shapes([
+                (q_mu, ["L", "M"]),
+                (q_sqrt, ["L", "M", "M"]),
+            ])
+            self.q_dist = tfp.distributions.MultivariateNormalTriL(loc=q_mu, scale_tril=q_sqrt)
+
+    def _precompute(self):
+        Kuu = covariances.Kuu(
+            self.inducing_variable, self.kernel, jitter=default_jitter()
+        )  # [(R), M, M]
+        q_mu = self.q_dist.loc
+
+        if Kuu.shape.ndims == 4:
+            # this branch only gets called  in fully-correlated case
+            # TODO this branch won't work; it's left-over from dense Kuu matrices
+            ML = tf.reduce_prod(tf.shape(Kuu)[:2])
+            Kuu = tf.reshape(Kuu, [ML, ML])
+
         L = tf.linalg.cholesky(Kuu)
 
         if not self.whiten:
