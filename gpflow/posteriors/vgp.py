c112abb29b330755b517aa8d3d35cf762bf8b4ee
johnamcleod <john.mcleod@prowler.io>
2021-05-17 10:21:37 +0100

    Create `posteriors` package to replace `posteriors.py`.

--- gpflow/posteriors/vgp.py
+++ gpflow/posteriors/vgp.py
@@ -11,27 +11,3 @@
 #  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #  See the License for the specific language governing permissions and
 #  limitations under the License.
-from typing import Tuple
-
-import tensorflow as tf
-
-from .posterior import Posterior, VariationalPosteriorMixin
-from ..conditionals import conditional
-from ..types import MeanAndVariance
-
-
-class VariationalPosterior(Posterior, VariationalPosteriorMixin):
-
-    def _conditional_fused(self, Xnew, full_cov: bool = False, full_output_cov: bool = False) -> MeanAndVariance:
-        X_data, _ = self.data
-        mu, var = conditional(
-            Xnew, X_data, self.kernel, self.q_mu, q_sqrt=self.q_sqrt, full_cov=full_cov, white=True,
-        )
-        return self._add_mean_function(Xnew, mu), var
-
-    def _precompute(self) -> Tuple[tf.Tensor, tf.Tensor]:
-        pass
-
-    def _conditional_with_precompute(self, Xnew, full_cov: bool = False,
-                                     full_output_cov: bool = False) -> MeanAndVariance:
-        pass
