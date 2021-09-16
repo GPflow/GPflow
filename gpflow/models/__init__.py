85a012c052b4d2c2b3540735a99a85fed0a4914d
Alessandro Vullo <alessandro.vullo@prowler.io>
2021-09-16 13:06:58 +0100

    Make new model visible.

--- gpflow/models/__init__.py
+++ gpflow/models/__init__.py
@@ -4,7 +4,7 @@ from .gpmc import GPMC
 from .gpr import GPR
 from .model import BayesianModel, GPModel
 from .sgpmc import SGPMC
-from .sgpr import GPRFITC, SGPR_deprecated
+from .sgpr import GPRFITC, SGPR
 from .svgp import SVGP
 from .training_mixins import ExternalDataTrainingLossMixin, InternalDataTrainingLossMixin
 from .util import maximum_log_likelihood_objective, training_loss, training_loss_closure
