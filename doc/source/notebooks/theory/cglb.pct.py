7477ed1fb13d9390d70c27fa7a0765b9b4006e49
Jesper Nielsen <jesper@prowler.io>
2021-11-29 16:09:57 +0000

    Set kernelspec on cplb.pct.py.

--- doc/source/notebooks/theory/cglb.pct.py
+++ doc/source/notebooks/theory/cglb.pct.py
@@ -1,4 +1,19 @@
 # -*- coding: utf-8 -*-
+# ---
+# jupyter:
+#   jupytext:
+#     formats: ipynb,.pct.py:percent
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.3.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
 # %%
 import numpy as np
 import tensorflow as tf
