c49d7010fc77d35c4071ce0a76b56329ff46ee17
Jesper Nielsen <44195043+jesnie@users.noreply.github.com>
2022-02-14 09:56:41 +0000

    Add more type hints. (#1777)

--- tests/gpflow/experimental/test_utils.py
+++ tests/gpflow/experimental/test_utils.py
@@ -11,10 +11,12 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+from _pytest.recwarn import WarningsRecorder
+
 from gpflow.experimental.utils import experimental
 
 
-def test_experimental(recwarn) -> None:
+def test_experimental(recwarn: WarningsRecorder) -> None:
     @experimental
     def f() -> None:
         pass
