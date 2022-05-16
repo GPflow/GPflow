cfbe1f754a3565b847ed8b3d187d453ada838353
Jesper Nielsen <44195043+jesnie@users.noreply.github.com>
2022-05-16 10:30:09 +0100

    Actually add_noise_cov supports leading batch shapes. (#1887)

--- tests/gpflow/utilities/test_model_utils.py
+++ tests/gpflow/utilities/test_model_utils.py
@@ -6,9 +6,51 @@ from gpflow.base import TensorType
 from gpflow.utilities import add_noise_cov
 
 
-@pytest.mark.parametrize("input_tensor", [tf.constant([[1.0, 0.5], [0.5, 1.0]])])
-@pytest.mark.parametrize("variance", [gpflow.Parameter(1.0, dtype=tf.float32)])
-@pytest.mark.parametrize("expected_tensor", [tf.constant([[2.0, 0.5], [0.5, 2.0]])])
+@pytest.mark.parametrize(
+    "input_tensor,variance,expected_tensor",
+    [
+        (
+            tf.constant([[1.0, 0.5], [0.5, 1.0]]),
+            gpflow.Parameter(1.0, dtype=tf.float32),
+            tf.constant([[2.0, 0.5], [0.5, 2.0]]),
+        ),
+        (
+            tf.constant(
+                [
+                    [
+                        [
+                            [0.0, 0.0],
+                            [0.0, 0.0],
+                        ],
+                    ],
+                    [
+                        [
+                            [1.0, 1.0],
+                            [1.0, 1.0],
+                        ],
+                    ],
+                ]
+            ),
+            gpflow.Parameter(0.5, dtype=tf.float32),
+            tf.constant(
+                [
+                    [
+                        [
+                            [0.5, 0.0],
+                            [0.0, 0.5],
+                        ],
+                    ],
+                    [
+                        [
+                            [1.5, 1.0],
+                            [1.0, 1.5],
+                        ],
+                    ],
+                ]
+            ),
+        ),
+    ],
+)
 def test_add_noise_cov(
     input_tensor: TensorType, variance: TensorType, expected_tensor: TensorType
 ) -> None:
