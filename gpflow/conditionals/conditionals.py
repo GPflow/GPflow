f365e433f9bd68dcf7266b33c0b62485f42f31d6
Simon Chiu <simon.chiu@secondmind.ai>
2022-12-05 15:31:45 +0000

    Added _sparse_orthogonal_conditional

--- gpflow/conditionals/conditionals.py
+++ gpflow/conditionals/conditionals.py
@@ -87,6 +87,72 @@ def _sparse_conditional(
     return posterior.fused_predict_f(Xnew, full_cov=full_cov, full_output_cov=full_output_cov)
 
 
+@conditional._gpflow_internal_register(
+    object, InducingVariables, InducingVariables, Kernel, object, object
+)
+def _sparse_orthogonal_conditional(
+    Xnew: tf.Tensor,
+    inducing_variable_u: InducingVariables,
+    inducing_variable_v: InducingVariables,
+    kernel: Kernel,
+    f_u: tf.Tensor,
+    f_v: tf.Tensor,
+    *,
+    full_cov: bool = False,
+    full_output_cov: bool = False,
+    q_sqrt_u: Optional[tf.Tensor] = None,
+    q_sqrt_v: Optional[tf.Tensor] = None,
+    white: bool = False
+) -> MeanAndVariance:
+    """
+    Single-output distributional orthogonal GP conditional.
+
+    The covariance matrices used to calculate the conditional have the following shape:
+    - Kuu: [M, M]
+    - Kuf: [M, N]
+    - Kff: [N, N]
+
+    Further reference
+    -----------------
+    - See `gpflow.conditionals._dense_conditional` (below) for a detailed explanation of
+      conditional in the single-output case.
+    - See the multiouput notebook for more information about the multiouput framework.
+
+    Parameters
+    ----------
+    :param Xnew: data matrix, size [N, D].
+    :param f: data matrix, [M, R]
+    :param full_cov: return the covariance between the datapoints
+    :param full_output_cov: return the covariance between the outputs.
+           NOTE: as we are using a single-output kernel with repetitions
+                 these covariances will be zero.
+    :param q_sqrt: matrix of standard-deviations or Cholesky matrices,
+        size [M, R] or [R, M, M].
+    :param white: boolean of whether to use the whitened representation
+    :return:
+        - mean:     [N, R]
+        - variance: [N, R], [R, N, N], [N, R, R] or [N, R, N, R]
+        Please see `gpflow.conditional._expand_independent_outputs` for more information
+        about the shape of the variance, depending on `full_cov` and `full_output_cov`.
+    """
+    assert(False, "Throwing an error just to check this thing gets run")
+    posterior_class = get_posterior_class(kernel, inducing_variable_u, inducing_variable_v)
+
+    posterior = posterior_class(
+        kernel,
+        inducing_variable_u,
+        inducing_variable_v,
+        f_u,
+        f_v,
+        q_sqrt_u,
+        q_sqrt_v,
+        whiten=white,
+        mean_function=None,
+    )
+
+    return posterior.fused_predict_f(Xnew, full_cov=full_cov, full_output_cov=full_output_cov)
+
+
 @conditional._gpflow_internal_register(object, object, Kernel, object)
 @check_shapes(
     "Xnew: [batch..., N, D]",
