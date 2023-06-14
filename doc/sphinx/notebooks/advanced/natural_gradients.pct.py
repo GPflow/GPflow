4fb57ca6c0f39c60ce286bed2f42463236906ac0
Uri Granta <uri.granta@secondmind.ai>
2023-06-14 14:36:38 +0100

    And again

--- doc/sphinx/notebooks/advanced/natural_gradients.pct.py
+++ doc/sphinx/notebooks/advanced/natural_gradients.pct.py
@@ -26,7 +26,7 @@ try:
     # use legacy Adam optimizer to support old TF versions
     from tensorflow.keras.optimizers.legacy import Adam
 except ImportError:
-    from keras.optimizers import Adam
+    from tensorflow.keras.optimizers import Adam
 
 import gpflow
 from gpflow import set_trainable
