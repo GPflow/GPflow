666d0f47d4f3b862ec9c5932c20b0f022cf69445
mawbray <maxmowbray@msn.com>
2026-05-12 14:29:17 +0100

    removing cast to MO

--- tests/gpflow/kernels/test_broadcasting.py
+++ tests/gpflow/kernels/test_broadcasting.py
@@ -151,7 +151,7 @@ def test_broadcasting(
     rank2 = len(batch2_shape) - 1
 
     if isinstance(kernel, kernels.MultioutputKernel):
-        mo_kernel = cast(kernels.MultioutputKernel, kernel)
+        mo_kernel = kernel
 
         loop = cs(
             unroll_batches(
