48c914f1f96cd4bff65a3196644bca7bc1d77e0c
Uri Granta <uri.granta@secondmind.ai>
2026-05-21 11:15:11 +0100

    try again

--- tests/gpflow/kernels/test_broadcasting.py
+++ tests/gpflow/kernels/test_broadcasting.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from typing import Any, Callable, List, Sequence, Tuple
+from typing import Any, Callable, List, Sequence, Tuple, cast
 
 import numpy as np
 import pytest
@@ -134,7 +134,7 @@ def test_broadcasting(
     rank2 = len(batch2_shape) - 1
 
     if isinstance(kernel, kernels.MultioutputKernel):
-        mo_kernel = kernel
+        mo_kernel = cast(kernels.MultioutputKernel, kernel)  # type: ignore[redundant-cast]
 
         loop = cs(
             unroll_batches(
